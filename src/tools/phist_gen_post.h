ddf3119a1ad76e3e9ec1aa1a2b5264948412d777
thie_jo <thie_jo@fa06282a-b0d2-4afa-91b7-075bac3d2625>
2013-06-03 10:27:23 +0000

    added two missing header files in tools/
    git-svn-id: https://svn-dmz.sistec.dlr.de/svn/dfg-essex/trunk@28 fa06282a-b0d2-4afa-91b7-075bac3d2625

---
+++ src/tools/phist_gen_post.h
@@ -0,0 +1,71 @@
+#ifdef _IS_COMPLEX_
+#define _CONJ_(x) std::conj(x)
+#else
+#define _CONJ_(x) x
+#define _Complex_I _ZERO_
+#endif
+
+#ifdef _IS_DOUBLE_
+#define _MT_ double
+#define ASSERT_REAL_EQ(expected,actual) ASSERT_DOUBLE_EQ(expected,actual)
+#else
+#define _MT_ float
+#define ASSERT_REAL_EQ(expected,actual) ASSERT_FLOAT_EQ(expected,actual)
+#endif
+
+// check wether all necessary macros are there:
+
+// type specifier
+#ifndef _TP_
+#error "macro _TP_ not defined"
+#endif
+
+// scalar type
+#ifndef _ST_
+#error "macro _ST_ not defined"
+#endif
+
+// imaginary unit
+#ifndef _Complex_I
+#warning "macro _Complex_I not defined"
+#endif
+
+// magnitude (or member) type
+#ifndef _MT_
+#error "macro _MT_ not defined"
+#endif
+
+// how to take the conjugate of a scalar
+#ifndef _CONJ_
+#error "macro _CONJ_ not defined"
+#endif
+
+// scalar 0 in the type _ST_
+#ifndef _ZERO_
+#error "macro _ZERO_ not defined"
+#endif
+
+// scalar 1 in the type _ST_
+#ifndef _ONE_
+#error "macro _ONE_ not defined"
+#endif
+
+// adds type prefix to a specifier, e.g. _PREF_(gemm) -> Dgemm
+#ifndef _PREF_
+#error "macro _PREF_ not defined"
+#endif
+
+// how to build up the name of a subroutine (void function)
+#ifndef _SUBR_
+#error "macro _SUBR_ not defined"
+#endif
+
+// how to build up the name of a type
+#ifndef _TYPE_
+#error "macro _TYPE_ not defined"
+#endif
+
+// how to build up the name of a test with two 'template' params
+#ifndef _TESTNAME2_
+#error "macro _TESTNAME2_ not defined"
+#endif
---
+++ src/tools/phist_gen_pre.h
@@ -0,0 +1,72 @@
+// this file undefs all the macros defined by the phist_gen_* 
+// headers
+
+// type specifier
+#ifdef _TP_
+#undef _TP_
+#endif
+
+// scalar type
+#ifdef _ST_
+#undef _ST_
+#endif
+
+// imaginary unit
+#ifdef _Complex_I
+#undef _Complex_I
+#endif
+
+// magnitude (or member) type
+#ifdef _MT_
+#undef _MT_
+#endif
+
+// how to take the conjugate of a scalar
+#ifdef _CONJ_
+#undef _CONJ_
+#endif
+
+// wether _ST_ is a real or complex type
+#ifdef _IS_COMPLEX_
+#undef _IS_COMPLEX_
+#endif
+
+// wether _ST_ is a single or double precision type
+#ifdef _IS_DOUBLE_
+#undef _IS_DOUBLE_
+#endif
+
+// scalar 0 in the type _ST_
+#ifdef _ZERO_
+#undef _ZERO_
+#endif
+
+// scalar 1 in the type _ST_
+#ifdef _ONE_
+#undef _ONE_
+#endif
+
+// adds type prefix to a specifier, e.g. _PREF_(gemm) -> Dgemm
+#ifdef _PREF_
+#undef _PREF_
+#endif
+
+// how to build up the name of a subroutine (void function)
+#ifdef _SUBR_
+#undef _SUBR_
+#endif
+
+// how to build up the name of a type
+#ifdef _TYPE_
+#undef _TYPE_
+#endif
+
+// how to build up the name of a test with two 'template' params
+#ifdef _TESTNAME2_
+#undef _TESTNAME2_
+#endif
+
+// macro used in googletest assertions
+#ifdef ASSERT_REAL_EQ
+#undef ASSERT_REAL_EQ
+#endif
