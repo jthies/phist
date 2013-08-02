f91700c4e22e1c061adffecaf646bf58cde766d1
thie_jo <thie_jo@fa06282a-b0d2-4afa-91b7-075bac3d2625>
2013-08-02 10:32:21 +0000

    fixed orthog test (slightly relaxed tolerance) and CMakeLists.txt (missing source file)
    git-svn-id: https://svn-dmz.sistec.dlr.de/svn/dfg-essex/trunk@89 fa06282a-b0d2-4afa-91b7-075bac3d2625

--- test/core/OrthogTest_def.hpp
+++ test/core/OrthogTest_def.hpp
@@ -127,14 +127,14 @@ public:
       ASSERT_REAL_EQ(mt::one(),WTest::ColsAreNormalized(Q_vp_,nloc_,ldaQ_,stride_,mpi_comm_));
       ASSERT_REAL_EQ(mt::one(),WTest::ColsAreOrthogonal(Q_vp_,nloc_,ldaQ_,stride_,mpi_comm_));
       
-      // check the decomposition: Q*R2 = W - V*R1 (compute W2=Q*R2-W+V*R1 and compare with 0)
+      // check the decomposition: Q*R2 = W - V*R1 (compute W2=Q*R2+V*R1-W and compare with 0)
       _SUBR_(mvec_times_sdMat)(st::one(),Q_,R1_,st::zero(),W2_,&ierr_);
-      ASSERT_EQ(0,ierr_);
-      _SUBR_(mvec_add_mvec)(-st::one(),W_,st::one(),W2_,&ierr_);
       ASSERT_EQ(0,ierr_);
       _SUBR_(mvec_times_sdMat)(st::one(),V_,R2_,st::one(),W2_,&ierr_);
       ASSERT_EQ(0,ierr_);
-      ASSERT_REAL_EQ(mt::one(),ArrayEqual(W2_vp_,nloc_,k_,ldaW2_,stride_,st::zero()));
+      _SUBR_(mvec_add_mvec)(-st::one(),W_,st::one(),W2_,&ierr_);
+      ASSERT_EQ(0,ierr_);
+      ASSERT_NEAR(mt::one(),ArrayEqual(W2_vp_,nloc_,k_,ldaW2_,stride_,st::zero()),6*mt::eps());
       }
     }
 
