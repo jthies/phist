529849faf1f7212141c7beef735ea516d8d5278c
thie_jo <thie_jo@fa06282a-b0d2-4afa-91b7-075bac3d2625>
2013-10-11 10:21:15 +0000

    test matrices in ghost binCRS format
    git-svn-id: https://svn-dmz.sistec.dlr.de/svn/dfg-essex/trunk@135 fa06282a-b0d2-4afa-91b7-075bac3d2625

---
+++ test/matrices/convert_to_ghost.sh
@@ -0,0 +1,38 @@
+SMATS=`ls s_*.mm`
+DMATS=`ls d_*.mm`
+CMATS=`ls c_*.mm`
+ZMATS=`ls z_*.mm`
+
+for mat in ${SMATS}
+        do
+                echo $mat
+                ./mm2binCRS.py ${mat} 5
+                mv out.crs $(basename "${mat}" .mm).bin
+        done
+
+for mat in ${DMATS}
+        do
+                echo $mat
+                ./mm2binCRS.py ${mat} 6
+                mv out.crs $(basename "${mat}" .mm).bin
+        done
+
+for mat in ${CMATS}
+        do
+                echo $mat
+                ./mm2binCRS.py ${mat} 9
+                mv out.crs $(basename "${mat}" .mm).bin
+        done
+
+for mat in ${ZMATS}
+        do
+                echo $mat
+                ./mm2binCRS.py ${mat} 10
+                mv out.crs $(basename "${mat}" .mm).bin
+        done
+
+./mm2binCRS.py mhd4800b.mm 6
+mv out.crs mhd4800b.bin
+
+./mm2binCRS.py mhd1280b.mm 10
+mv out.crs mhd1280b.bin
