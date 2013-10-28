54142b0b99c2751b795fd9a9e0e256f27829b843
thie_jo <thie_jo@fa06282a-b0d2-4afa-91b7-075bac3d2625>
2013-10-28 14:11:16 +0000

    missing file phist_enums.h
    git-svn-id: https://svn-dmz.sistec.dlr.de/svn/dfg-essex/trunk@164 fa06282a-b0d2-4afa-91b7-075bac3d2625

---
+++ src/tools/phist_enums.h
@@ -0,0 +1,14 @@
+#ifndef PHIST_ENUMS_H
+#define PHIST_ENUMS_H
+
+typedef enum eigSort_t {
+
+NONE=0,
+LM=1,   // largest magnitude
+SM=2,   // smallest magnitude
+LR=3,   // largest real part
+SR=4   // smallest real part
+
+} eigSort_t;
+
+#endif
