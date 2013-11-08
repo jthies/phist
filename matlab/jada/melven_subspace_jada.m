935199faea592cd8538f050b5a6eded8f29dd5de
zoel_ml <zoel_ml@fa06282a-b0d2-4afa-91b7-075bac3d2625>
2013-11-08 15:51:41 +0000

    fix my matlab block jada algorithm, add some arnoldi-iterations for start vectors etc, only the generalized case with more eigenvalues seems problematic
    git-svn-id: https://svn-dmz.sistec.dlr.de/svn/dfg-essex/trunk@210 fa06282a-b0d2-4afa-91b7-075bac3d2625

--- matlab/jada/melven_subspace_jada.m
+++ matlab/jada/melven_subspace_jada.m
@@ -1,4 +1,4 @@
-function [R,Q,resnorm,resnorm_history,m,restarts]=melven_subspace_jada(A,B,v0,nEig,maxIter,minBas,maxBas,res_eps)
+function [R,Q,resnorm,lambda_history,resnorm_history,m,restarts]=melven_subspace_jada(A,B,v0,nEig,arnoldiIter,maxIter,minBas,maxBas,res_eps)
 
 n = size(A,1);                                  % matrix dimension
 k = size(v0,2);                                 % blocksize, e.g. number of eigenvalues wanted
@@ -13,29 +13,32 @@ restarts = 0;
 
 %data for already converged eigenvalues
 resnorm = inf(nEig,1);
-resnorm_history = inf(nEig,maxIter);
+resnorm_history = inf(nEig,arnoldiIter+maxIter);
+lambda_history = nan(nEig,arnoldiIter+maxIter);
 nConv = 0;
 R = zeros(nEig,nEig);
 Q = zeros(n,nEig);
 BQ = zeros(n,nEig);
 nNewConv = 0;
 
-for m = 1:maxIter                               % main iteration loop
+for m = 1:maxIter+arnoldiIter                               % main iteration loop
     nV_old = nV;
     if( m > 1 )
         t = t - V(:,1:nV_old) * (BV(:,1:nV_old)' * t);     % orthogonalize t wrt V
     end
-    [t,rank_t] = orth(t,B,res_eps); % need B-orthogonality
-    fprintf(' rank of correction: %d (newly converged EV: %d)\n', rank_t, nNewConv);
-    if( rank(t) == 0 && nNewConv == 0 )
-        return
+    [t,rank_t] = orth(t,B,res_eps/100); % need B-orthogonality
+    fprintf(' rank of correction: %d (previously converged EV: %d)\n', rank_t, nNewConv);
+    if( rank_t == 0 && nNewConv == 0 )
+        warning('using new random vector!');
+        t = rand(n,1);
+        t = t - Q(:,1:nConv)*BQ(:,1:nConv)'*t;
+        t = t - V(:,1:nV)*BV(:,1:nV)'*t;
+        [t,rank_t] = orth(t,B,res_eps/100); % need B-orthogonality
+        fprintf(' rank of random correction: %d\n', rank_t);
     end
     if( nV_old + rank_t > maxBas )
       restarts = restarts + 1;
       fprintf('restarting with %d basis vectors after subspace dimension of %d was reached.\n', minBas, nV_old + rank_t);
-      % resort schur form with minBas biggest EW
-      select_ev(sort_index(1:minBas)) = true;
-      [Q_H,~] = ordschur(Q_H,R_H,select_ev);
       % we need to get the "best" vectors out of V
       V(:,1:minBas) = V(:,1:nV_old)*Q_H(:,1:minBas);
       % also update BV, W and H
@@ -47,37 +50,37 @@ for m = 1:maxIter                               % main iteration loop
     nV = nV_old + rank_t;
     V(:,nV_old+1:nV) = t;                     % update subspace V
     BV(:,nV_old+1:nV) = B*t;
-    V_orth = max(max(abs(eye(nV) - BV(:,1:nV)' * V(:,1:nV))));
+V_orth = max(max(abs(eye(nV) - BV(:,1:nV)' * V(:,1:nV))));
+if( V_orth > res_eps/100 )
+    %% reorthogonalize
+    warning('V not orthogonal (%g), reorthogonalizing', V_orth);
+    V(:,1:nV) = V(:,1:nV) - Q(:,1:nConv)*BQ(:,1:nConv)' * V(:,1:nV);
+    [t,nV] = orth(V(:,1:nV),B,res_eps);
+    nV_old = 0;
+    V(:,1:nV) = t;
+    BV(:,1:nV) = B*t;
+    V_orth = max(max(abs(eye(nV) - BV(:,1:nV)' * t)));
     if( V_orth > res_eps )
-        %% reorthogonalize
-        warning('V not orthogonal (%g), reorthogonalizing', V_orth);
-        [t,nV] = orth(V(:,1:nV),B,res_eps);
-        nV_old = 0;
-        BV(:,1:nV) = B*t;
-        V_orth = max(max(abs(eye(nV) - BV(:,1:nV)' * t)));
-        if( V_orth > res_eps )
-            error('could not reorthogonalize, error: %g', V_orth);
-        end
+        error('could not reorthogonalize, error: %g', V_orth);
     end
+end
     W(:,nV_old+1:nV) = A*t;                   % update W = A*V
 
-    % update H
-    H(1:nV_old,nV_old+1:nV)     = V(:,1:nV_old)'     * W(:,nV_old+1:nV);
-    H(nV_old+1:nV,1:nV_old)     = V(:,nV_old+1:nV)' * W(:,1:nV_old);
-    H(nV_old+1:nV,nV_old+1:nV) = V(:,nV_old+1:nV)' * W(:,nV_old+1:nV);
+    % update H = V' * W
+    H(1:nV_old,    nV_old+1:nV)  = V(:,1:nV_old)'    * W(:,nV_old+1:nV);
+    H(nV_old+1:nV, 1:nV_old)     = V(:,nV_old+1:nV)' * W(:,1:nV_old);
+    H(nV_old+1:nV, nV_old+1:nV)  = V(:,nV_old+1:nV)' * W(:,nV_old+1:nV);
 
     % compute the schur form of H
-    k = min(k,nEig-nConv);
-    [Q_H,R_H] = schur(H(1:nV,1:nV),'complex');
-    % extract largest eigenvalues (in modulus)
-    DH = ordeig(R_H);
-    [~,sort_index] = sort(abs(DH),'descend');
-    select_ev = false(nV,1);
-    select_ev(sort_index(1:k)) = true;
-    % sort schur form
-    [Q_H,R_H] = ordschur(Q_H,R_H,select_ev);
+    [Q_H,R_H] = SortSchur(H(1:nV,1:nV),'LM',1,nV);
 
-    % get our current approximation of  A*Q = Q*R
+%test_H = max(max(abs(H(1:nV,1:nV)*Q_H - Q_H*R_H)));
+%if( test_H > res_eps )
+%    error('test Q_H R_H wrong: %e', test_H);
+%end
+
+    
+    % check for converged eigenvalues
     r = R_H(1:k,1:k);
     q = V(:,1:nV) * Q_H(:,1:k);
     Bq = BV(:,1:nV) * Q_H(:,1:k);
@@ -89,13 +92,13 @@ for m = 1:maxIter                               % main iteration loop
     for i = 1:k
         resnorm(nConv+i) = norm(res(:,i),2);
     end
+    lambda_history(1:nConv,m) = diag(R(1:nConv,1:nConv));
+    lambda_history(nConv+1:nConv+k,m) = diag(r);
     resnorm_history(:,m) = resnorm;
-
-    % check for converged eigenvalues
     nNewConv = 0;
     for i = 1:k
         if( resnorm(nConv+i) < res_eps )
-            fprintf('\nconverged eigenvalue %d: %8.4g', nConv+i, r(i,i));
+            fprintf('\nconverged eigenvalue %d: %s\n', nConv+i, num2str(r(i,i),'%8.4g'));
             nNewConv = nNewConv + 1;
         else
             %% TODO: also handle converged eigenvalues in the middle of the block q
@@ -104,44 +107,90 @@ for m = 1:maxIter                               % main iteration loop
     end
     if( nNewConv > 0 )
         % add it to converged data for deflation
-        Q(:,nConv+1:nConv+nNewConv) = q(:,1:nNewConv);
-        R(1:nConv,nConv+1:nConv+nNewConv) = a_(1:nConv,1:nNewConv);
+        Q(:,                     nConv+1:nConv+nNewConv) = q(:,         1:nNewConv);
+        R(1:nConv,               nConv+1:nConv+nNewConv) = a_(1:nConv,  1:nNewConv);
         R(nConv+1:nConv+nNewConv,nConv+1:nConv+nNewConv) = r(1:nNewConv,1:nNewConv);
-        BQ(:,nConv+1:nConv+nNewConv) = Bq(:,1:nNewConv);
+        BQ(:,                    nConv+1:nConv+nNewConv) = Bq(:,        1:nNewConv);
         % remove it from V!
-        V(:,1:nV-nNewConv) = V(:,1:nV)*Q_H(:,nNewConv+1:nV);
-        BV(:,1:nV-nNewConv) = BV(:,1:nV)*Q_H(:,nNewConv+1:nV);
-        W(:,1:nV-nNewConv) = W(:,1:nV)*Q_H(:,nNewConv+1:nV);
+        V (:,1:nV-nNewConv) = V (:,1:nV) * Q_H(:,nNewConv+1:nV);
+        BV(:,1:nV-nNewConv) = BV(:,1:nV) * Q_H(:,nNewConv+1:nV);
+        W (:,1:nV-nNewConv) = W (:,1:nV) * Q_H(:,nNewConv+1:nV);
+        H (1:nV-nNewConv, 1:nV-nNewConv) = Q_H(:,nNewConv+1:nV)' * H(1:nV,1:nV) * Q_H(:,nNewConv+1:nV);
+        V_orth = max(max(abs(eye(nV-nNewConv) - BV(:,1:nV-nNewConv)' * V(:,1:nV-nNewConv))));
+        % remove it from Q_H, R_H
+        Q_H = eye(nV-nNewConv);
+        R_H = R_H(nNewConv+1:nV,nNewConv+1:nV);
         nV = nV - nNewConv;
+        nConv = nConv + nNewConv;
+        if( nConv == nEig )
+            return;
+        end
+        k = min(k,nEig-nConv);
+
+%test_H = V(:,1:nV)' * W(:,1:nV);
+%if( H(1:nV,1:nV) - test_H > res_eps )
+%    error('H wrong!');
+%end
+%test_H = max(max(abs(H(1:nV,1:nV)*Q_H - Q_H*R_H)));
+%if( test_H > res_eps )
+%    error('Q_H R_H wrong: %e', test_H);
+%end
+        
+        % recalculate r,q,...
+        r = R_H(1:k,1:k);
+        q = V(:,1:nV) * Q_H(:,1:k);
+        Bq = BV(:,1:nV) * Q_H(:,1:k);
+        % calculate residuum
+        Aq = W(:,1:nV) * Q_H(:,1:k);
+        res = Aq - Bq*r;
+        a_ = BQ(:,1:nConv)'*res;
+        res = res - Q(:,1:nConv)*a_;
+        for i = 1:k
+            resnorm(nConv+i) = norm(res(:,i),2);
+        end
+        lambda_history(1:nConv,m) = diag(R(1:nConv,1:nConv));
+        lambda_history(nConv+1:nConv+k,m) = diag(r);
+        resnorm_history(:,m) = resnorm;
     end
-
-
+    
     
     % display stuff
-    fprintf('\niteration %d:',m);
+    if( m > arnoldiIter )
+        fprintf('\nBlockJaDa-iteration %d:',m - arnoldiIter);
+    else
+        fprintf('\nBlockArnoldi-iteration %d:', m);
+    end
     fprintf('\nconverged eigenvalues: %d (of %d)', nConv, nEig);
     fprintf('\nsubspace dimension %d:',nV);
     fprintf('\n V-orthog. %e:',V_orth);
     fprintf('\n app. eigenvalues: %s', num2str([diag(R(1:nConv,1:nConv)); diag(r)]','%8.4g'));
     fprintf('\n   schur residuum:');  fprintf(' %6.2g', resnorm);
     fprintf('\n');
-    if( nConv+nNewConv == nEig )
-        return;
+
+    
+    if( m <= arnoldiIter )
+        t = res;
+        continue;
     end
 
     % construct q_ and Bq_ which deflate already converged eigenvalues
     q_ = [Q(:,1:nConv) q];
     Bq_ = [BQ(:,1:nConv) Bq];
+if( max(max(abs(q_' * Bq_ - eye(nConv+k)))) > res_eps )
+    error('q_Bq_')
+end
+%r_ = [R(1:nConv,1:nConv) a_(1:nConv,1:k); zeros(k,nConv) r];
+%q_Orth = max(max(abs(q_' * Bq_ - eye(nConv+k)))); 
+%if( q_Orth > res_eps )
+%    error('q_/Bq_ not orthogonal: %e', q_Orth);
+%end
     t = zeros(n,k);
     if( true )
         % solve approximately
         % (I-Bqq')A(I-qBq')*t - (I-Bqq')*B*t*r = -res
         % as r is an upper triangular matrix, we can do this column for column
         % (I-Bqq')(A-r(i,i)*B)(I-qBq')*t(:,i) = -res(:,i) + (I-Bqq')*B*t(:,1:i-1)*r(1:i-1,i)
-        for i = 1:nNewConv
-            t(:,i) = 0;
-        end
-        for i = nNewConv+1:k
+        for i = 1:k
             % i-th residuum
             res_i = -res(:,i);
             % analyse behaviour if we omit the coupling here...
@@ -182,10 +231,9 @@ for m = 1:maxIter                               % main iteration loop
             t(:,i) = t(:,i) - q_*(Bq_'*t(:,i));
         end
     end
-    
-
-    nConv = nConv + nNewConv;
 end
+warning('reached maxIter');
+
 
 function y = A_proj(x)
     y = x-q_*(Bq_'*x);
--- matlab/test_melven_subspace_jada.m
+++ matlab/test_melven_subspace_jada.m
@@ -8,18 +8,27 @@ nx=8;
 A=make_testmat(nx);
 n=size(A,1);
 
-k=1; % block size
-nEig = 4;
+
+k = 4;
+nEig = 20;
+res_eps = 1.e-8;
+generalized = false;
+
+%% for generalized
+%k=4; % block size
+%nEig = 10;
+%res_eps = 1.e-3;
+%generalized = true;
+%%
 
 % complex case
 if (complex)
   A=A+0.25*sprandn(A)*1i*complex;
 end
 
-generalized = false;
 if( generalized )
   % create spd. B
-  B = sprandsym(n, 0.001,0.1,2);
+  B = sprandsym(n, 0.001,0.5,2);
   % print cond of B
   fprintf('cond(B) = %f\n', condest(B));
 else
@@ -36,10 +45,10 @@ disp('subspace JaDa iteration for exterior eig(s).');
 
 minBas = 2*k;
 maxBas = 80;
+arnoldiIter = 2;
 maxIter = 400;
-res_eps = 1.e-4;
 resnorm_history_total = [];
-[r,q,resnorm,resnorm_history,m,restarts] = melven_subspace_jada(A,B,v0,nEig,maxIter,minBas,maxBas,res_eps);
+[r,q,resnorm,lambda_history,resnorm_history,m,restarts] = melven_subspace_jada(A,B,v0,nEig,arnoldiIter,maxIter,minBas,maxBas,res_eps);
 fprintf('total iterations: %d, restarts: %d',m,restarts);
 fprintf('\nEigenv. (MATLAB): %s', num2str(eigs(A,B,nEig)','%8.4g'));
 fprintf('\n      Difference: '); fprintf(' %6.2g',abs(sort(diag(r),'descend')-sort(eigs(A,B,nEig),'descend')));
