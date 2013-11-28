596de16c8f1bff1cad0f672c78886da7b4dd45f5
Jonas Thies <jonas.thies@dlr.de>
2013-11-28 15:21:32 +0100

    the number of threads in the ghost/tsqr interface is now not set to # phys. cores but to # omp threads so that the computation in mvec_QR uses the same number of threads as in the other basic operations.

--- src/kernels/ghost/Ghost_TsqrAdaptor.hpp
+++ src/kernels/ghost/Ghost_TsqrAdaptor.hpp
@@ -369,7 +369,7 @@ namespace ghost {
  static Teuchos::RCP<node_type> createNode()
     {
     Teuchos::ParameterList nodeParams(node_type::getDefaultParameters());
-    nodeParams.set("Num Threads",ghost_getNumberOfPhysicalCores());
+    nodeParams.set("Num Threads",ghost_ompGetNumThreads());
     Teuchos::RCP<node_type> node = Teuchos::rcp(new node_type(nodeParams));
     return node;
     }
