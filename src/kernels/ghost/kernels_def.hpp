2709bb6450f09b3548ac14c2cb4e45dc69c54645
Jonas Thies <jonas.thies@dlr.de>
2013-12-17 12:34:54 +0100

    implemented mvec_view_scattered for ghost

--- src/kernels/ghost/kernels_def.hpp
+++ src/kernels/ghost/kernels_def.hpp
@@ -325,10 +325,24 @@ void SUBR(mvec_view_block)(TYPE(mvec_ptr) vV,
 //! them, the stride may even be negative. Not all functions have to support this kind of
 //! data, in particular mvecT_times_mvec) will probably not. They obviously have to report
 //! the error appropriately.
-void SUBR(mvec_view_scattered)(TYPE(mvec_ptr) V, TYPE(mvec_ptr)* Vscat,
+void SUBR(mvec_view_scattered)(TYPE(mvec_ptr) vV, TYPE(mvec_ptr)* vVscat,
                              int* cols, int ncols, int* ierr)
   {
-  *ierr=-99;
+  ENTER_FCN(__FUNCTION__);
+  *ierr=0;
+  CAST_PTR_FROM_VOID(ghost_vec_t,V,vV,*ierr);
+
+  //TODO: cols is int, ghost_vidx_t may be int64_t...
+  ghost_vec_t *Vscat = V->viewScatteredVec
+        (V, (ghost_vidx_t)ncols,(ghost_vidx_t*)cols);
+
+  if (*vVscat!=NULL)
+    {
+    CAST_PTR_FROM_VOID(ghost_vec_t,tmp,*vVscat,*ierr);
+    PHIST_DEB("destroying previous vector (view)");
+    tmp->destroy(tmp);
+    }
+  *vVscat = (TYPE(mvec_ptr))Vscat;
   }                             
 
 //! get a new vector that is a copy of some columns of the original one,  
