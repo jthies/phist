5f237e3090c445f6d6592f3ea0c18aad5ccea973
Jonas Thies <jonas.thies@dlr.de>
2014-03-08 23:22:38 +0100

    started working on (hitherto unpipelined) CARP-CG

--- src/krylov/phist_carp_cg_decl.h
+++ src/krylov/phist_carp_cg_decl.h
@@ -1,8 +1,13 @@
-// Pipelined Conjugate Gradient solver for positive definite systems.
+// Pipelined CARP-CG solver for general shifted matrices A-sigma[j]B.
+// The algorithm is CG on the minimum norm problem AA'x=b with SSOR pre-
+// conditioning, implemented following the work of Bjoerck and Elfving (1979).
+// The parallelization of the Kaczmarz sweeps is left to the kernel library
+// (function carp_fb).
+//
 
-// the usage of pcg is very similar to that of pgmres, except that we don't
+// the usage of carp_cg is very similar to that of pgmres, except that we don't
 // need the complicated queuing of vectors in pgmres.
-typedef struct TYPE(cgState) {
+typedef struct TYPE(carp_cgState) {
   //! \name input and output args:
   //@{
   int id; //! can be used to identify the system solved here (the column to which this 
@@ -11,44 +16,48 @@ typedef struct TYPE(cgState) {
           //! to X(:,i) everywhere. But it could be useful when reordering the state array 
           //! or printing debug info in a function which gets just one state object.
   _MT_ tol; //! convergence tolerance for this system
+  
+  _ST_ sigma; //! we're solving (A-sigma*I)x=b in this state object
 
   int ierr; //! error code returned by this CARP-CG instance
 
   int totalIter; //! counts number of iterations (also over restarts)
   //@}
-  //! \name  internal data structures
+  //! \name CARP data structures
+  //@{
+  //! vector with the 2-norm of each row of A-sigma*I (squared)
+  TYPE(mvec_ptr) norms_ai2_;
+
+  //! \name  internal CG data structures
   //@{
   TYPE(mvec_ptr) q_, r_, p_; //! this instance operates on column 'id' of these vectors only.
   TYPE(mvec_ptr) x0_; //! starting vector to compute the first residual vector r0
   TYPE(mvec_ptr) b_; //! rhs to compute the first residual vector r0
-  _MT_ *beta_;         //! diagonal of the Lanczos matrix
-  _ST_ *alpha_;        //! sub-/superdiagonal
+  _MT_ alpha_, beta_; // scalars forming the Lanczos (cf. numerics textbook for CG algorithm)
   
   _MT_ normR0_; //! stores initial (explicit) residual norm
   _MT_ normR_; //! stores current (implicit) residual norm
   
-  int curDimV_; //! current size of the Krylov subspace
-
   int maxIters_; //! maximum number of iterations allowed
 
   //@}
-} TYPE(cgState);
+} TYPE(carp_cgState);
 
-typedef TYPE(cgState)* TYPE(cgState_ptr);
+typedef TYPE(carp_cgState)* TYPE(carp_cgState_ptr);
 
-typedef TYPE(cgState) const * TYPE(const_cgState_ptr);
+typedef TYPE(carp_cgState) const * TYPE(const_cgState_ptr);
 
 //!
-void SUBR(cgStates_iterate)(TYPE(const_op_ptr) Op,
-        TYPE(cgState_ptr) S_array[], TYPE(mvec_ptr) X,
+void SUBR(carp_cgStates_iterate)(TYPE(const_crsMat_ptr) A,
+        TYPE(carp_cgState_ptr) S_array[], TYPE(mvec_ptr) X,
         int* nIter, int* ierr);
 
 //!
-void SUBR(cgStates_create)(TYPE(cgState_ptr) S_array[], int numSys,
+void SUBR(carp_cgStates_create)(TYPE(carp_cgState_ptr) S_array[], int numSys,
         const_map_ptr_t map, int maxIters, int* ierr);
 
 //!
-void SUBR(cgStates_delete)(TYPE(cgState_ptr) S_array[], int numSys, int* ierr);
+void SUBR(carp_cgStates_delete)(TYPE(carp_cgState_ptr) S_array[], int numSys, int* ierr);
 
 //! this function can be used to force a clean restart of the associated CARP-CG
 //! solver. It is necessary to call this function before the first call to
@@ -60,7 +69,7 @@ void SUBR(cgStates_delete)(TYPE(cgState_ptr) S_array[], int numSys, int* ierr);
 //! Krylov sequence will result and the convergence criterion will not be consistent. If
 //! the matrix A changes, *all* states must be reset. If some the shift in JaDa changes,
 //! reset should be called with A=NULL (A does not change) for that particular system.
-void SUBR(cgState_reset)(TYPE(cgState_ptr) S,
-                TYPE(const_op_ptr) A_op, TYPE(const_mvec_ptr) b,
+void SUBR(carp_cgState_reset)(TYPE(carp_cgState_ptr) S,
+                TYPE(const_crsMat_ptr) A, TYPE(const_mvec_ptr) b,
                 TYPE(const_mvec_ptr) x0, int *ierr);
 
--- src/krylov/phist_carp_cg_def.hpp
+++ src/krylov/phist_carp_cg_def.hpp
@@ -1,5 +1,5 @@
 // create new state objects. We just get an array of (NULL-)pointers
-void SUBR(cgStates_create)(TYPE(cgState_ptr) state[], int numSys,
+void SUBR(carp_cgStates_create)(TYPE(carp_cgState_ptr) state[], int numSys,
         const_map_ptr_t map, int maxIters,int* ierr)
 {
 #include "phist_std_typedefs.hpp"
@@ -9,7 +9,7 @@ void SUBR(cgStates_create)(TYPE(cgState_ptr) state[], int numSys,
 
   for (int i=0;i<numSys;i++)
   {
-    state[i] = new TYPE(cgState);
+    state[i] = new TYPE(carp_cgState);
     state[i]->id=i;
     // set some default options
     state[i]->tol=0.5; // typical starting tol for JaDa inner iterations...
@@ -32,7 +32,7 @@ void SUBR(cgStates_create)(TYPE(cgState_ptr) state[], int numSys,
 
 
 //! delete cgState object
-void SUBR(cgStates_delete)(TYPE(cgState_ptr) state[], int numSys, int* ierr)
+void SUBR(carp_cgStates_delete)(TYPE(carp_cgState_ptr) state[], int numSys, int* ierr)
 {
   ENTER_FCN(__FUNCTION__);
   *ierr=0;
@@ -50,7 +50,8 @@ void SUBR(cgStates_delete)(TYPE(cgState_ptr) state[], int numSys, int* ierr)
 }
 
 // reset pcg state.
-void SUBR(cgState_reset)(TYPE(cgState_ptr) S, 
+void SUBR(carp_cgState_reset)(TYPE(carp_cgState_ptr) S, 
+        TYPE(const_crsMat_ptr) A,
         TYPE(const_mvec_ptr) b,
         TYPE(const_mvec_ptr) x0,int *ierr)
 {
@@ -73,18 +74,22 @@ void SUBR(cgState_reset)(TYPE(cgState_ptr) S,
     S->totalIter = 0;
   }
   
-  S->curDimV_=0;
   S->normR0_=-mt::one(); // needs to be computed in next iterate call
   // set V_0=X_0. iterate() will have to compute the residual and normalize it,
   // because the actual V_0 we want is r/||r||_2, but we can't easily apply the
   // operator to a single vector.
   PHIST_CHK_IERR(SUBR(mvec_add_mvec)(st::one(),x0,st::zero(), S->x0_, ierr),*ierr);
+
+  //TODO - recompute if shift or A changes
+  S->nrm_ai2=nrms_ai2(A,sigma);
+
+
 }
 
 
 // implementation of pcg on several systems simultaneously
-void SUBR(cgStates_iterate)(TYPE(const_op_ptr) op,
-        TYPE(cgState_ptr) S[], TYPE(mvec_ptr) X,
+void SUBR(carp_cgStates_iterate)(TYPE(const_crsMat_ptr) A,
+        TYPE(carp_cgState_ptr) S_array[], TYPE(mvec_ptr) X,
         int* nIter, int* ierr)
 {
 #include "phist_std_typedefs.hpp"
@@ -98,15 +103,89 @@ void SUBR(cgStates_iterate)(TYPE(const_op_ptr) op,
   PHIST_SOUT(PHIST_DEBUG,"starting function iterate() with %d systems\n curDimVs: ",numSys);
   for (int i=0;i<numSys;i++)
   {
-    PHIST_SOUT(PHIST_DEBUG,"%d ",S[i]->curDimV_);
+    PHIST_SOUT(PHIST_DEBUG,"%d ",S[i]->totalIter_);
   }
   PHIST_SOUT(PHIST_DEBUG,"\n");
 #endif
 
   int anyConverged=0;
   int anyFailed=0;
+
+  if (numSys>1)
+  { 
+    *ierr=-99; //TODO: implement pipelining
+  }
   
-  *ierr=-99; //TODO: our own CG implementation
+int itprint=1;
+int itcheck=1;
+
+*ierr=0;
+
+TYPE(carp_cgState_ptr) S = s_array[0];
+
+MT reltol2=S->tol*S->tol*S->normR0_*S->normR0_;
+
+r=dkswp(A,sigma,B,b,x,omega,nrm_ai2)-x;
+if (deflMethod==2)
+  Vtr=V'*r;
+  vr=E\Vtr;
+  rtil=r-AV*(vr);
+  z=apply_op(rtil,M);
+  z=z-AV*(E\(V'*z));
+  z=z+V*vr;
+else
+  z=apply_op(r,M);
+end
+p=z;
+
+if (deflMethod==1)
+  p = p - V*(AV'*p);
+end
+
+r2_new = r'*z;
+
+alpha_old=0;
+beta_old=0;
+
+disp(sprintf('%d\t%e\t%e',0,sqrt(r2_new),sqrt(r2_new)/nrm_b));
+for k=1:maxIter
+  q=p-dkswp(A,sigma,B,bnul,p,omega,nrm_ai2);
+  alpha = (r'*z)/(p'*q);
+  x=x+alpha*p;
+  if (mod(k-1,itcheck)==0)
+    nrm_r = norm(A*x-sigma*x-b);
+    if (mod(k-1,itprint)==0)
+      disp(sprintf('%d\t%e\t%e',k,nrm_r,nrm_r/nrm_b));
+    end
+    relres=nrm_r/nrm_b;
+    resvec=[resvec,nrm_r];
+    if (nrm_r<tol*nrm_r0)
+      break;
+    end
+  end
+  r=r-alpha*q;
+  if (deflMethod==2)
+    Vtr=V'*r;
+    vr=E\Vtr;
+    rtil=r-AV*(vr);
+    z=apply_op(rtil,M);
+    z=z-AV*(E\(V'*z));
+    z=z+V*vr;
+  else
+    z=apply_op(r,M);
+  end
+  r2_old=r2_new;
+  r2_new=r'*z;
+  beta=r2_new/r2_old;
+  p=z+beta*p;
+
+  relres=sqrt(r2_old)/nrm_b;
+  %fprintf('\t%d\t%e\n',k,relres);
+  %if (r2_old<reltol2) 
+  %  break;
+  %end
+  } // while
+
 
   if (anyConverged > 0)
     *ierr=0;
