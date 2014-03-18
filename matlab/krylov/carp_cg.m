7aa0803aba61d7c3ee4888b3b499af55ce45e28c
Jonas Thies <jonas.thies@dlr.de>
2014-03-18 10:45:31 +0100

    experimental stuff in carp_cg.m before branch switch/merge

--- matlab/krylov/carp_cg.m
+++ matlab/krylov/carp_cg.m
@@ -1,28 +1,24 @@
-function [x,flag,relres,iter,resvec]=carp_cg(A,b,x0,opts)
-%                                                        
-% function X=bgmres( A,B,X0,opts,M)                      
-%                                                        
-% Right-preconditioned block GMRES(m).                   
-% Implementation follows Saad (Alg. 9.5, p270),          
-% with single Block-CGS orthogonalization, adapted for   
-% multiple RHS.                                          
-%                                                        
-% args                                                   
-% ~~~~                                                   
-%                                                        
-% A should be a real sparse matrix for now.
+function [x,flag,relres,iter,resvec, Tlan]=carp_cg(A,b,x0,opts)
+%                                                               
+% [x,flag,relres,iter,resvec, Tlan]=carp_cg(A,b,x0,opts)        
+%                                                               
+% CGMN algorithm (Bjoerck & Elfving 1979). Returns the typical  
+% output of iterative methods in matlab (cf. 'help pcg'), and   
+% optionally computes the tridiagonal Lanczos matrix so that    
+% eigenvalue information can be obtained a posteriori.          
 %                                                        
 % options                                                
 % ~~~~~~~                                                
 %                                                        
 % opts.tol - convergence tolerance                       
-% opts.maxIter - max number of iterations
-% opts.omega - relaxation parameter
-% opts.Precond - preconditioning operator
-% opts.sigma - shift, solve (A-sigma*I)x=b instead.     
-%              in contrast to A, sigma may be complex.
+% opts.maxIter - max number of iterations                
+% opts.omega - relaxation parameter                      
+% opts.Precond - preconditioning operator                
+% opts.sigma - shift, solve (A-sigma*I)x=b instead.      
+%              in contrast to A, sigma may be complex.   
+% opts.seedSpace (experimental) - attempt to deflate a   
+% number of approximate eigenvectors.                      
 %
-
 verbose=getopt(opts,'verbose',true);
 
 itprint=1;
@@ -33,14 +29,43 @@ relres=1.0;
 iter = 0; % total iterations
 resvec=[];
 
+wantT=(nargout>=6);
 n=size(A,1);
 
 tol=getopt(opts,'tol',1e-8);
 maxIter=getopt(opts,'maxIter',300);
 omega=getopt(opts,'omega',1.7);
 M=getopt(opts,'Precond',speye(n));
+seedSpace=getopt(opts,'seedspace',[]);
 sigma=getopt(opts,'sigma',0.0);
+B=getopt(opts,'massmat',speye(n));
+deflMethod=0;
+if ~isempty(seedSpace)
+  deflMethod=1; % deflMethod 1: simple A-orthogonal deflation
+                %            2: BNN (Tang et al paper)
+                % so far nothing is working in terms of deflation.
+                % This method is equivalent to balancing Neumann-Neumann,
+                % and in the notation in Tang et. al. chooses as preconditioner
+                % M1=P'M\P+Q, with E=(V'AV), Q=V(E\V'), P=I-AQ.
+end
 
+% diagonal and sub/super diagonal of the Lanczos method
+lanD0=[];
+lanD1=[];
+
+%{
+idrS=4;
+idrsOpts=struct;
+if ~isempty(seedSpace)
+  idrsOpts.P=seedSpace;
+  idrS=size(seedSpace,2);
+end
+
+[x,flag,relres,iter,resvec]=idrs(A-sigma*B,b,idrS,tol,maxIter,[],[],x0,idrsOpts);
+return;
+%}
+%[x,flag,relres,iter,resvec]=gmres(A-sigma*B,b,50,tol,maxIter,@apply_op,[],x0,M);
+%return;
 %fprintf('CARP-CG tol: %4.2e\n',tol);
 
 debug=getopt(opts,'debug',false);
@@ -51,24 +76,51 @@ if (debug)
   maxIter
 end
 
+nrm_ai2=nrms_ai2(A',sigma);
+bnul=zeros(n,1);
+
+
+V=[];
+AV=[];
+E=[];
+if (deflMethod~=0)
+  V=seedSpace;
+  %AV= A*V - sigma*V;
+  AV=V-dkswp(A,sigma,B,bnul,V,omega,nrm_ai2);
+  E=V'*AV;
+end
+
+
 nrm_b=norm(b);
 nrm_r0=norm(A*x0-sigma*x0-b);
 reltol2=tol*tol*nrm_b*nrm_b;
 
-nrm_ai2=nrms_ai2(A,sigma);
-
 x=x0;
-r=dkswp(A,sigma,b,x,omega,nrm_ai2)-x;
-z=apply_op(r,M);
+r=dkswp(A,sigma,B,b,x,omega,nrm_ai2)-x;
+if (deflMethod==2)
+  Vtr=V'*r;
+  vr=E\Vtr;
+  rtil=r-AV*(vr);
+  z=apply_op(rtil,M);
+  z=z-AV*(E\(V'*z));
+  z=z+V*vr;
+else
+  z=apply_op(r,M);
+end
 p=z;
+
+if (deflMethod==1)
+  p = p - V*(AV'*p);
+end
 
 r2_new = r'*z;
 
-bnul=zeros(n,1);
+alpha_old=0;
+beta_old=0;
 
 disp(sprintf('%d\t%e\t%e',0,sqrt(r2_new),sqrt(r2_new)/nrm_b));
 for k=1:maxIter
-  q=p-dkswp(A,sigma,bnul,p,omega,nrm_ai2);
+  q=p-dkswp(A,sigma,B,bnul,p,omega,nrm_ai2);
   alpha = (r'*z)/(p'*q);
   x=x+alpha*p;
   if (mod(k-1,itcheck)==0)
@@ -83,16 +135,38 @@ for k=1:maxIter
     end
   end
   r=r-alpha*q;
-  z=apply_op(r,M);
+  if (deflMethod==2)
+    Vtr=V'*r;
+    vr=E\Vtr;
+    rtil=r-AV*(vr);
+    z=apply_op(rtil,M);
+    z=z-AV*(E\(V'*z));
+    z=z+V*vr;
+  else
+    z=apply_op(r,M);
+  end
   r2_old=r2_new;
   r2_new=r'*z;
   beta=r2_new/r2_old;
   p=z+beta*p;
+
+  if (deflMethod==1)
+    p = p - V*(AV'*z);
+  end
   relres=sqrt(r2_old)/nrm_b;
   %fprintf('\t%d\t%e\n',k,relres);
   %if (r2_old<reltol2) 
   %  break;
   %end
+  if (wantT)
+    lanD0=[lanD0;1/alpha];
+    lanD1=[lanD1;sqrt(beta)/alpha];
+    if (k>1)
+      lanD0(k) = lanD0(k)+beta_old/alpha_old;
+    end
+    alpha_old=alpha;
+    beta_old=beta;
+  end
 end
 iter=k;
 
@@ -100,6 +174,13 @@ if iter>=maxIter
   flag=1;
 else
   flag=0;
+end
+
+if (wantT)
+  Tlan = spdiags([lanD1,lanD0,[0;conj(lanD1(1:k-2))]],-1:1,k-1,k-1);
+  disp(['D0=',num2str(lanD0(1))]);
+  disp(['E0=',num2str(lanD1(1))]);
+%Tlan=[lanD1, lanD0,[0;lanD1(1:k-1)]];
 end
 
 end
