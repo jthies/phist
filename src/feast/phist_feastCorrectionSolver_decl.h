c1a8d36ff16aecc507fbf76510a258506ced1fe0
Jonas Thies <jonas.thies@dlr.de>
2014-04-04 16:16:35 +0200

    defined an interface for the FEAST correction solver

---
+++ src/feast/phist_feastCorrectionSolver_decl.h
@@ -0,0 +1,55 @@
+
+#ifdef IS_DOUBLE
+typedef Zmvec_t Dfeast_mvec_t;
+typedef d_complex_t feast_shift_t;
+#else
+typedef Zmvec_t Dfeast_mvec_t;
+typedef s_complex_t feast_shift_t;
+typedef 
+#endif
+
+//!
+//! The feastCorrectionSolver uses carp_cg to calculate approximate solutions to a set 
+//! of FEAST correction equations. It provides a simple interface and takes care of 
+//! optimally pipelining the operations.
+//! In each call to solve(), numShifts linear systems with numBlocks right-hand sides
+//! are solved simultaneously. Whenever a shift changes or the number of shifts/systems
+//! changes, the object must be destroyed and rebuilt (This is just a first guess at
+//! what may be a useful interface)
+typedef struct TYPE(feastCorrectionSolver)
+{
+// TODO
+} TYPE(feastCorrectionSolver);
+
+typedef TYPE(feastCorrectionSolver)* TYPE(feastCorrectionSolver_ptr);
+
+typedef TYPE(feastCorrectionSolver) const * TYPE(const_feastCorrectionSolver_ptr);
+
+
+//! create a feastCorrectionSolver object. You have to specify the number of rhs 
+//! per shift (blockSize), the number of shifts (numShifts), and the complex shifts
+
+void SUBR(feastCorrectionSolver_create)(TYPE(feastCorrectionSolver_ptr) *fCorrSolver, 
+        TYPE(const_crsMat_ptr) A, 
+        int blockSize, int numShifts,
+        feast_shift_t shifts[],
+        int *ierr);
+
+//! delete a feastCorrectionSolver object
+void SUBR(feastCorrectionSolver_delete)(TYPE(feastCorrectionSolver_ptr) fCorrSolver, int *ierr);
+
+
+//! calculate approximate solutions to given set of FEAST correction equations
+//!
+//! arguments:
+//! fCorrSolver    the feastCorrectionSolver object
+//! rhs             rhs of the correction equations
+//! tol             desired accuracy (gmres residual tolerance) of the individual systems
+//! maxIter         maximal number of iterations after which individial systems should be aborted
+//! sol             returns approximate solution vectors
+//! ierr            a value > 0 indicates the number of systems that have not converged to the desired tolerance
+void SUBR(feastCorrectionSolver_run)(TYPE(feastCorrectionSolver_ptr) fCorrSolver,
+                                    const feast_mvec_t* const rhs,
+                                    const _MT_ tol, int maxIter,
+                                    feast_mvec_t* sol,
+                                    int *ierr);
