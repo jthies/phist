043323c3cb5cda9f9c7a69c886d6591f7f7aa33b
Jonas Thies <jonas.thies@dlr.de>
2014-04-23 20:40:27 +0200

    various old updates concerning carp-cg, I did not try to compile/run this version so it may be wracked.

--- src/kernels/ghost/kernels.cpp
+++ src/kernels/ghost/kernels.cpp
@@ -237,7 +237,6 @@ void phist_map_get_iupper(const_map_ptr_t vmap, gidx_t* iupper, int* ierr)
 #include "kernels_def.hpp"
 #include "carp_def.hpp"
 
-
 #include "phist_gen_z.h"
 #include "kernels_def.hpp"
 #include "carp_def.hpp"
