ff4dc102f58a4ec769d2851345cd12f3a9d9926d
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2014-05-09 11:04:49 +0200

    circumvent possible problem in simple_gmres (H != A'*AV)
    I have no idea why, this seemed to work correctly in
    MastersThesisMelven, but there seem no big changes in phist since then

--- src/jada/phist_subspacejada_def.hpp
+++ src/jada/phist_subspacejada_def.hpp
@@ -194,6 +194,9 @@ void SUBR(subspacejada)( TYPE(const_op_ptr) A_op,  TYPE(const_op_ptr) B_op,
   PHIST_CHK_IERR(SUBR( mvec_view_block  ) (BV_, 		&BV,                      0,     nV-1,      ierr), *ierr);
   PHIST_CHK_IERR(SUBR( sdMat_view_block ) (H_,  		&H,     0,      nV-1,     0,     nV-1,      ierr), *ierr);
 
+  // TODO: strangely there seems to be a bug in simple_arnoldi, s.t. H != V'*AV
+  //       so recalculate it!
+  PHIST_CHK_IERR(SUBR( mvecT_times_mvec ) (st::one(), V, AV, st::zero(), H, ierr), *ierr);
 
 
 
