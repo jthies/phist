735976e26aafb99ffd767efbe6c5802ff299a7af
Jonas Thies <jonas.thies@dlr.de>
2014-06-11 17:02:07 +0200

    updated carp kernel interface for fortran/ghost noimpl

--- src/kernels/fortran/carp_def.hpp
+++ src/kernels/fortran/carp_def.hpp
@@ -13,7 +13,8 @@ void SUBR(carp_setup)(TYPE(const_crsMat_ptr) A, int numShifts,
 
 void SUBR(dkswp)(TYPE(const_crsMat_ptr) A, int numShifts, 
         _MT_ const sigma_r[], _MT_ const sigma_i[],
-        TYPE(const_mvec_ptr) Rhs, TYPE(mvec_ptr) X[],
+        TYPE(const_mvec_ptr) Rhs, 
+        TYPE(mvec_ptr) X_r[], TYPE(mvec_ptr) X_i[],
         _MT_ const* nrm_ai2i, void* const work,
         _MT_ const * omega, int* ierr)
 {
@@ -21,7 +22,7 @@ void SUBR(dkswp)(TYPE(const_crsMat_ptr) A, int numShifts,
   return;
 }
 
-void SUBR(carp_destroy(TYPE(const_crsMat_ptr) A, int numShifts,
+void SUBR(carp_destroy)(TYPE(const_crsMat_ptr) A, int numShifts,
 _MT_* nrms_ai2i, void* work, int *ierr)
 {
   *ierr=-99;
