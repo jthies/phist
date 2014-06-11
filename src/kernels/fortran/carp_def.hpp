3337383b24bfa534dbc6024837c980b77b40f82e
Jonas Thies <jonas.thies@dlr.de>
2014-06-11 17:16:32 +0200

    fortran carp kernel C interface

--- src/kernels/fortran/carp_def.hpp
+++ src/kernels/fortran/carp_def.hpp
@@ -6,7 +6,12 @@ void SUBR(carp_setup)(TYPE(const_crsMat_ptr) A, int numShifts,
         _MT_ const sigma_r[], _MT_ const sigma_i[],
         _MT_ **nrms_ai2i, void** work, int* ierr)
 {
-  *ierr=-99;
+  void SUBR(carp_setup_f)(TYPE(const_crsMat_ptr) A, int numShifts,
+        _MT_ const sigma_r[], _MT_ const sigma_i[],
+        _MT_ **nrms_ai2i, void** work, int* ierr);
+  *ierr=0;
+  PHIST_CHK_IERR(SUBR(carp_setup_f)
+        (A,numShifts,sigma_r,sigma_i,nrms_ai2i,work,ierr),*ierr);
   return;
 }
 
@@ -18,20 +23,28 @@ void SUBR(dkswp)(TYPE(const_crsMat_ptr) A, int numShifts,
         _MT_ const* nrm_ai2i, void* const work,
         _MT_ const * omega, int* ierr)
 {
-  *ierr=-99;
+  void SUBR(dkswp_f)(TYPE(const_crsMat_ptr) A, int numShifts, 
+        _MT_ const sigma_r[], _MT_ const sigma_i[],
+        TYPE(const_mvec_ptr) Rhs, 
+        TYPE(mvec_ptr) X_r[], TYPE(mvec_ptr) X_i[],
+        _MT_ const* nrm_ai2i, void* const work,
+        _MT_ const * omega, int* ierr);
+  *ierr=0;
+  PHIST_CHK_IERR(SUBR(dkswp_f)(A, numShifts,
+        sigma_r, sigma_i, Rhs, X_r, X_i, nrm_ai2i, work,
+        omega, ierr),*ierr);
   return;
 }
 
 void SUBR(carp_destroy)(TYPE(const_crsMat_ptr) A, int numShifts,
 _MT_* nrms_ai2i, void* work, int *ierr)
 {
-  *ierr=-99;
+  void SUBR(carp_destroy_f)(TYPE(const_crsMat_ptr) A, int numShifts,
+        _MT_* nrms_ai2i, void* work, int *ierr);
+  *ierr=0;
+  PHIST_CHK_IERR(SUBR(carp_destroy_f)(A,numShifts,nrms_ai2i,work,ierr),*ierr);
   return;
 }
-
-
-
-
 
 
 } // extern "C"
