c2f62065453048cd2be9baab45bf02599f224867
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2014-06-25 13:46:57 +0200

    replace buggy handcoded mvec_times_sdMat_inplace by mvec_times_sdMat + mvec_set_block
    * needs to be implemented in GHOST

--- src/jada/phist_transform_searchspace_def.hpp
+++ src/jada/phist_transform_searchspace_def.hpp
@@ -66,7 +66,7 @@ void SUBR(mvec_times_sdMat_inplace)(TYPE(mvec_ptr) V_, TYPE(sdMat_ptr) M_, int* 
   // check dimensions
   PHIST_CHK_IERR(*ierr = (nvec == nM ? 0 : -1), *ierr);
   PHIST_CHK_IERR(*ierr = (nvec >= mM ? 0 : -1), *ierr);
-/*
+
   // get map
   const_map_ptr_t map = NULL;
   PHIST_CHK_IERR(SUBR(mvec_get_map)(V_, &map, ierr), *ierr);
@@ -81,76 +81,5 @@ void SUBR(mvec_times_sdMat_inplace)(TYPE(mvec_ptr) V_, TYPE(sdMat_ptr) M_, int* 
 
   // delete temp data
   PHIST_CHK_IERR(SUBR(mvec_delete)(Vtmp, ierr), *ierr);
-*/
-  // get raw view on V and M
-  ST *V = NULL;
-  ST *M = NULL;
-  lidx_t ldaV, ldaM;
-  PHIST_CHK_IERR( SUBR( mvec_extract_view  ) (V_, &V,  &ldaV, ierr), *ierr);
-  PHIST_CHK_IERR( SUBR( sdMat_extract_view ) (M_, &M,  &ldaM, ierr), *ierr);
-
-#ifdef IS_COMPLEX
-  typedef blas_cmplx_t BLAS_ST;
-#else
-  typedef ST BLAS_ST;
-#endif
-
-  int error = 0;
-#pragma omp parallel reduction(||:error)
-  {
-    // create a buffer for a block of the mvec
-    ST *work = (ST*)malloc(chunkSize*nvec*sizeof(ST));
-
-    // blas needs int instead of lidx_t
-    int i_chunkSize = chunkSize;
-    int i_nvec = nvec;
-    int i_ldaM = ldaM;
-    int i_ldaV = ldaV;
-    int info = 0;
-    ST blas_ONE = st::one();
-    ST blas_ZERO = st::zero();
-
-    lidx_t i, j, off;
-    lidx_t iChunk;
-    lidx_t nChunks = nV/chunkSize;
-#pragma omp for
-    for(iChunk = 0; iChunk < nChunks; iChunk++)
-    {
-      off = iChunk*chunkSize;
-      // copy block to work
-      for(j = 0; j < nvec; j++)
-        for(i = 0; i < chunkSize; i++)
-          work[chunkSize*j+i] = V[ldaV*j+off+i];
-
-      // call gemm with work buffer
-      PREFIX(GEMM) ("N", "N", &i_chunkSize, &i_nvec, &i_nvec, (BLAS_ST*)&blas_ONE, (BLAS_ST*)work, &i_chunkSize, (BLAS_ST*)M, &i_ldaM, (BLAS_ST*)&blas_ZERO, (BLAS_ST*)&(V[off]), &i_ldaV, &info);
-      error = error || (info != 0);
-    }
-
-
-#pragma omp single
-    {
-      // do the remaining part
-      off = nChunks*chunkSize;
-      chunkSize = nV-nChunks*chunkSize;
-      i_chunkSize = chunkSize;
-
-      if( chunkSize > 0 )
-      {
-        // copy block to work
-        for(j = 0; j < nvec; j++)
-          for(i = 0; i < chunkSize; i++)
-              work[chunkSize*j+i] = V[ldaV*j+off+i];
-
-        // call gemm with work buffer
-        PREFIX(GEMM) ("N", "N", &i_chunkSize, &i_nvec, &i_nvec, (BLAS_ST*)&blas_ONE, (BLAS_ST*)work, &i_chunkSize, (BLAS_ST*)M, &i_ldaM, (BLAS_ST*)&blas_ZERO, (BLAS_ST*)&(V[off]), &i_ldaV, &info);
-        error = error || (info != 0);
-      }
-    }
-
-    free(work);
-  }
-
-  PHIST_CHK_IERR(*ierr = error ? -1: 0, *ierr);
 }
 #endif
