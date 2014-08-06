db59e5f56fdd7e46541c5013996abdd30d613b8b
Jonas Thies <jonas.thies@dlr.de>
2014-08-06 13:26:50 +0200

    adjusted sample build script for stand-alone fortran compilation with ColPack from the DLR module system

--- buildScripts/script_feast.sh
+++ buildScripts/script_feast.sh
@@ -3,22 +3,22 @@
 # the FEAST/CARP-CG prototype. In this first step, we provide all
 # required core operations (CARP etc.) in the fortran kernel implementation.
 
-# use Fortran kernels for experimental CARP implementation
-PHIST_KERNEL_LIB=fortran
-# compile without Trilinios
+# compile without Trilinios and override env var for kernel lib
+unset PHIST_KERNEL_LIB
 unset TRILINOS_HOME
 unset Trilinos_HOME
 
 # use ParMETIS for global (re-)partitioning
-PARMETIS_DIR=/tools/modulesystem/tools/parmetis/parmetis-4.0.3/install/sled11.x86_64.gcc-4.8.2.release.int64
+PARMETIS_DIR=/tools/modulesystem/tools/parmetis/parmetis-4.0.3/install/sled11.x86_64.gcc-4.3.4.release.int64
 # use ColPack for node-local graph coloring
-COLPACK_DIR=/unsecured/thie_jo/essex/installs
+COLPACK_DIR=/tools/modulesystem/tools/ColPack/ColPack-1.0.8/install/sled11.x86_64.gcc-4.3.4.release
 # use MPI compilers
 CC=mpicc
 CXX=mpicxx
 FC=mpif90
 SRC_DIR=${HOME}/essex/phist
 cmake -DCMAKE_BUILD_TYPE=Release \
+      -DPHIST_KERNEL_LIB=fortran \
       -DPARMETIS_DIR=${PARMETIS_DIR} \
       -DCOLPACK_DIR=${COLPACK_DIR} \
       ${SRC_DIR}
