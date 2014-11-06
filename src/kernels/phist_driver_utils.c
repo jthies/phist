7d70c7ec0aa2cce90d9ad5e35b993315e97e42a5
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2014-11-06 10:30:53 +0100

    Put crsMat_read into libphist_kernels... instead of libphist_tools to avoid circular dependencies in the dynamic libraries
    Also added phist_sizeof_lidx and phist_sizeof_gidx to allow runtime
    detection of the correct integer type.

--- src/kernels/phist_driver_utils.c
+++ src/kernels/phist_driver_utils.c
@@ -7,6 +7,16 @@
 #include "phist_config.h"
 #include "phist_kernels.h"
 
+int phist_sizeof_lidx()
+{
+  return sizeof(lidx_t);
+}
+
+int phist_sizeof_gidx()
+{
+  return sizeof(gidx_t);
+}
+
 #ifdef PHIST_HAVE_SP
 #include "phist_gen_s.h"
 #include "phist_driver_utils_def.h"
