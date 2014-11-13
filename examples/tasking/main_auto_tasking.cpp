cd4aa19f64ed9330d11c7921e4b624e88d867521
Jonas Thies <jonas.thies@dlr.de>
2014-11-13 14:51:59 +0100

    added an example main program to debug the automatic ghost tasking mechanism by c++11 lambdas. Currently it seems that every function call creates a new set of threads, but I'm not sure if I interpret this correctly (pthread_create is called repeatedly, see issue #77)

---
+++ examples/tasking/main_auto_tasking.cpp
@@ -0,0 +1,40 @@
+#include "phist_config.h"
+#ifdef PHIST_HAVE_MPI
+#include <mpi.h>
+#endif
+
+#include <iostream>
+
+
+#ifdef PHIST_KERNEL_LIB_GHOST
+#include "phist_macros.h"
+#include "phist_ghost_macros.hpp"
+#include "phist_kernels.h"
+
+void printHello(int n)
+{
+PHIST_GHOST_TASK_BEGIN
+  std::cout << "Hello, World! ["<<n<<"]"<<std::endl;
+PHIST_GHOST_TASK_END
+}
+
+int main(int argc, char* argv[])
+{
+  int ierr;
+  PHIST_ICHK_IERR(phist_kernels_init(&argc,&argv,&ierr),ierr);
+
+  for (int i=1;i<=3;i++)
+    {
+    printHello(i);
+    }
+
+  PHIST_ICHK_IERR(phist_kernels_finalize(&ierr),ierr);
+}
+
+#else
+int main()
+{
+  std::cout << "this driver does nothing unless you use GHOST as kernel lib\n";
+  return;
+}
+#endif
