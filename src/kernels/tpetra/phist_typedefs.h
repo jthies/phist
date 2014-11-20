c99eeadd51dba7056d0f53d29c303df42c9a8b9a
Jonas Thies <jonas.thies@dlr.de>
2014-11-20 11:24:19 +0100

    Tpetra: do not include Kokkos OpenMPNode header, just use the default node defined at install time of Trilinos right now.

--- src/kernels/tpetra/phist_typedefs.h
+++ src/kernels/tpetra/phist_typedefs.h
@@ -13,7 +13,6 @@
 #include <complex>
 #include <cstddef>
 #include "Kokkos_DefaultNode.hpp"
-#include "Kokkos_OpenMPNode.hpp"
 #else
 //#include <inttypes.h>
 #include <complex.h>
@@ -25,7 +24,6 @@
 //! TODO - do we want that here or do we want to give
 //!        the user a choice?
 typedef Kokkos::DefaultNode::DefaultNodeType node_t;
-//typedef Kokkos::OpenMPNode node_t;
 //! single precision complex type
 typedef std::complex<float> s_complex_t;
 typedef std::complex<double> d_complex_t;
