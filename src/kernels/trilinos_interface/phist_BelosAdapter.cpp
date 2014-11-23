ff6a4d574fecefc3f792599e40dba02ead8e6012
Jonas Thies <jonas.thies@dlr.de>
2014-11-23 22:15:21 +0100

    two missing source files for the Belos interface

---
+++ src/kernels/trilinos_interface/phist_BelosAdapter.cpp
@@ -0,0 +1,15 @@
+#include "phist_config.h"
+#include "phist_typedefs.h"
+#include "phist_MultiVector.hpp"
+#include "phist_BelosAdapter.hpp"
+
+/* this file is just to put some static member variables somewhere */ 
+namespace Belos {
+
+#ifdef PHIST_HAVE_SP
+int MultiVecTraits<float,::phist::MultiVector<float> >::ierr_;
+int MultiVecTraits<s_complex_t, ::phist::MultiVector<s_complex_t> >::ierr_;
+#endif
+int MultiVecTraits<double, ::phist::MultiVector<double> >::ierr_;
+int MultiVecTraits<d_complex_t, ::phist::MultiVector<d_complex_t> >::ierr_;
+}
