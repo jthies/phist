52fd9a8db75cd549e2efde8db271d26774d22c58
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2014-12-15 19:52:28 +0100

    fix for ghost's mvec_QR

--- src/kernels/ghost/kernels_def.hpp
+++ src/kernels/ghost/kernels_def.hpp
@@ -1360,6 +1360,7 @@ extern "C" void SUBR(mvec_QR)(TYPE(mvec_ptr) vV, TYPE(sdMat_ptr) vR, int* ierr)
     MT nrm;
     PHIST_CHK_IERR(SUBR(mvec_normalize)(vV,&nrm,ierr),*ierr);
     PHIST_DEB("single vector QR, R=%8.4e\n",nrm);
+    PHIST_CHK_IERR(SUBR(sdMat_put_value)(R,(ST)nrm,ierr),*ierr);
     rank=1;
     if (nrm<rankTol)
     {
@@ -1368,8 +1369,8 @@ extern "C" void SUBR(mvec_QR)(TYPE(mvec_ptr) vV, TYPE(sdMat_ptr) vR, int* ierr)
       PHIST_CHK_IERR(SUBR(mvec_random)(vV,ierr),*ierr);
       PHIST_CHK_IERR(SUBR(mvec_normalize)(vV,&nrm,ierr),*ierr);
       rank=0;// dimension of null space
+      PHIST_CHK_IERR(SUBR(sdMat_put_value)(R,st::zero(),ierr),*ierr);
     }
-    PHIST_CHK_IERR(SUBR(sdMat_put_value)(R,(ST)nrm,ierr),*ierr);
     *ierr=1-rank;
     return;
   }// case ncols=1: normalize single vector
