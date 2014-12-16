e6ac6a7364421fcb3483496b0a0ec962a04f5ed6
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2014-12-16 23:52:01 +0100

    Try to adjust build script for Jenkins matrix job

--- buildScripts/ci-sc-dlr.sh
+++ buildScripts/ci-sc-dlr.sh
@@ -4,10 +4,10 @@ set -e
 ## default options and declarations
 # kernel lib
 KERNELS="builtin" # ghost epetra tpetra
-FLAGS="" # " optional-libs"
+PRGENV="gcc-4.9" # intel-13.0 gcc-4.8
+FLAGS="" # optional-libs
 
 # list of modules to load
-MODULES_PRGENV="PrgEnv/gcc-4.9.2-openmpi"
 MODULES_BASIC="cmake ccache cppcheck lapack"
 
 declare -A MODULES_KERNELS
@@ -17,16 +17,22 @@ MODULES_KERNELS=(
   ["epetra"]="trilinos"
   ["tpetra"]="trilinos" )
 
+declare -A MODULES_PRGENV
+MODULES_PRGENV=(
+  ["gcc-4.9"]="PrgEnv/gcc-4.9.2-openmpi"
+  ["intel-13.0"]="PrgEnv/intel-13.0.1-mpich"
+  ["gcc-4.8"]="PrgEnv/gcc-4.8.2-openmpi" )
+
 declare -A MODULES_KERNELS_OPTIONAL
 MODULES_KERNELS_OPTIONAL=(
   ["builtin"]="parmetis trilinos"
   ["ghost"]="trilinos"
   ["epetra"]=""
-  ["tpatra"]="" )
+  ["tpetra"]="" )
 
 
 ## parse command line arguments
-usage() { echo "Usage: $0 [-k <builtin|ghost|epetra|tpetra>] [-e <modules-string>] [-f <optional-libs>]" 1>&2; exit 1; }
+usage() { echo "Usage: $0 [-k <builtin|ghost|epetra|tpetra>] [-e <gcc-4.9|intel-13.0|gcc-4.8>] [-f <optional-libs>]" 1>&2; exit 1; }
 
 while getopts "k:e:f:h" o; do
     case "${o}" in
@@ -35,7 +41,8 @@ while getopts "k:e:f:h" o; do
             ((KERNELS == "builtin" || KERNELS == "ghost" || KERNELS == "tpetra" || KERNELS == "epetra")) || usage
             ;;
         e)
-            MODULES_PRGENV=${OPTARG}
+            PRGENV=${OPTARG}
+            ((PRGENV == "gcc-4.9" || PRGENV == "intel-13.0" || PRGENV == "gcc-4.8")) || usage
             ;;
         f)
             FLAGS=${OPTARG}
@@ -50,36 +57,26 @@ while getopts "k:e:f:h" o; do
 done
 shift $((OPTIND-1))
 
-echo "Options: KERNEL_LIB=${KERNELS}, MODULES_PRGENV=${MODULES_PRGENV}, FLAGS=${FLAGS}"
-exit 0
+echo "Options: KERNEL_LIB=${KERNELS}, PRGENV=${PRGENV}, FLAGS=${FLAGS}"
+
 ## prepare system for compilation
 # configure modulesystem
 export MODULEPATH=/tools/modulesystem/modulefiles
 module() { eval `/usr/bin/modulecmd bash $*`; }
 
 # load modules
-for m in $MODULES_PRGENV; do module load $m; done
-for m in $MODULES_BASIC; do module load $m; done
-for m in ${MODULES_KERNELS["$KERNELS"]}; do module load $m; done
+for m in ${MODULES_PRGENV["$PRGENV"]};    do module load $m; done
+for m in $MODULES_BASIC;                  do module load $m; done
+for m in ${MODULES_KERNELS["$KERNELS"]};  do module load $m; done
 if [[ "$FLAGS" = *"optional-libs"* ]]; then
   for m in ${MODULES_KERNELS_OPTIONAL["$KERNELS"]}; do module load $m; done
 fi
 module list
 
-# detect compiler
-if [[ "$MODULES_PRGENV" = *"gcc"* ]]; then
-  COMPILER=gcc-$(gcc -dumpversion)
-elif [[ "$MODULES_PRGENV" = *"intel"* ]]; then
-  COMPILER=intel-$(icc -dumpversion)
-else
-  echo "Unknown compiler!"
-  exit 1
-fi
-
 # use ccache to speed up build
-if [[ "$COMPILER" = "gcc-"* ]]; then
+if [[ "$PRGENV" = "gcc"* ]]; then
   export FC="ccache gfortran" CC="ccache gcc" CXX="ccache g++"
-elif [[ "$COMPILER" = "intel-"* ]]; then
+elif [[ "$PRGENV" = "intel"* ]]; then
   export FC="ccache ifort" CC="ccache icc" CXX="ccache icpc"
 fi
 
@@ -90,13 +87,8 @@ ulimit -v unlimited
 ## actually build and run tests
 error=0
 
-# nice _flags folder
-if [ "$FLAGS" != "" ]; then
-  FLAGS=" ${FLAGS}"
-fi
-
 # release build
-mkdir build_${KERNELS}_${COMPILER}_release${FLAGS// /_}; cd $_
+mkdir build_${KERNELS}_${PRGENV}_release_${FLAGS// /_}; cd $_
 cmake -DCMAKE_BUILD_TYPE=Release  \
       -DPHIST_KERNEL_LIB=$KERNELS \
       ..                            || error=1
@@ -106,7 +98,7 @@ make test                           || error=1
 cd ..
 
 # debug build
-mkdir build_${KERNELS}_${COMPILER}_debug${FLAGS// /_}; cd $_
+mkdir build_${KERNELS}_${PRGENV}_debug_${FLAGS// /_}; cd $_
 cmake -DCMAKE_BUILD_TYPE=Debug    \
       -DPHIST_KERNEL_LIB=$KERNELS \
       -DINTEGRATION_BUILD=On      \
