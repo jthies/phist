4c6dc8822a7527129e8ced934711047c739bc7a1
Jonas Thies <jonas.thies@dlr.de>
2014-12-19 14:11:13 +0100

    matfuncs "anderson" and "matpde" are currently implemented in the PHIST supplement to essex-physics, so if you link with essex-physics they are not available and cause dynamic link problems. This is now precluded and leads to a run time error (-99 return), which hopefully fixes #84.

--- src/kernels/phist_driver_utils_def.h
+++ src/kernels/phist_driver_utils_def.h
@@ -193,6 +193,11 @@ void SUBR(create_matrix)(TYPE(sparseMat_ptr)* mat, const_comm_ptr_t comm,
   }
   else if (mat_type==ANDERSON)
   {
+#ifdef PHIST_HAVE_ESSEX_PHYSICS
+PHIST_SOUT(PHIST_ERROR,"matrix row function \"anderson\" is only implemented in PHIST\n"
+                       "and not available if you link with the essex-physics library\n");
+*ierr=-99;
+#else
     PHIST_SOUT(PHIST_INFO,"problem type: Anderson %d x %d x %d\n",L,L,L);
     ghost_lidx_t LL=L;
   
@@ -203,6 +208,7 @@ void SUBR(create_matrix)(TYPE(sparseMat_ptr)* mat, const_comm_ptr_t comm,
     PHIST_CHK_IERR(SUBR(sparseMat_create_fromRowFunc)(mat,comm,
         (gidx_t)info.nrows, (gidx_t)info.ncols, (lidx_t)info.row_nnz,
         &anderson, iflag), *iflag);
+#endif
   }
   else if (mat_type==SPINSZ)
   {
@@ -222,6 +228,11 @@ void SUBR(create_matrix)(TYPE(sparseMat_ptr)* mat, const_comm_ptr_t comm,
   }
   else if (mat_type==MATPDE)
   {
+#ifdef PHIST_HAVE_ESSEX_PHYSICS
+PHIST_SOUT(PHIST_ERROR,"matrix row function \"matpde\" is only implemented in PHIST\n"
+                       "and not available if you link with the essex-physics library\n");
+  *ierr=-99;
+#else
     PHIST_SOUT(PHIST_INFO,"problem type: MATPDE %d x %d\n", L, L);
     gidx_t nrows = -1;
     gidx_t ncols = -1;
@@ -230,6 +241,7 @@ void SUBR(create_matrix)(TYPE(sparseMat_ptr)* mat, const_comm_ptr_t comm,
     ncols = nrows;
     PHIST_CHK_IERR(SUBR(sparseMat_create_fromRowFunc)(mat, comm, 
           nrows, ncols, row_nnz, &MATPDE_rowFunc, iflag), *iflag);
+#endif
   }
   else
   {
