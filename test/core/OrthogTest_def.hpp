06a12cc5a86147ce32ed339c1c21eaea395a9c55
Jonas Thies <jonas.thies@dlr.de>
2015-01-23 16:40:46 +0100

    all current orthog tests use doOrthogTest function

--- test/core/OrthogTest_def.hpp
+++ test/core/OrthogTest_def.hpp
@@ -265,12 +265,6 @@ public:
       SUBR(mvec_random)(W_,&iflag_);
       ASSERT_EQ(0,iflag_);
       
-      MT tolV=(MT)10.*VTest::releps(V_);
-      MT tolW=(MT)10.*WTest::releps(W_);
-
-      // copy Q=W because orthog() works in-place
-      SUBR(mvec_add_mvec)(st::one(),W_,st::zero(),Q_,&iflag_);
-      ASSERT_EQ(0,iflag_);
       lidx_t nR=_M_+_K_;
       TYPE(sdMat_ptr) R=NULL;
       SUBR(sdMat_create)(&R,nR,nR,comm_,&iflag_);
@@ -301,50 +295,10 @@ public:
       SUBR(sdMat_extract_view)(R2_,&R2_vp_,&this->ldaR2_,&this->iflag_);
       ASSERT_EQ(0,this->iflag_);
 
+      // test orthog routine, expect full rank of V and [V W]
+      doOrthogTests(V_, W_, Q_, R0_, R1_, R2_, 
+        0, 0, m_, m_+k_);
 
-      // orthogonalize the m columns of V
-      int rankVW=-42;
-      bool useSVQB=false;
-      SUBR(orthog)(NULL,V_,R0_,NULL,1,&rankVW,&iflag_);
-      if (iflag_!=+2)
-      {
-        ASSERT_EQ(0,iflag_);
-      }
-      else
-      {
-        useSVQB=true;
-      }
-      ASSERT_EQ(_M_,rankVW);
-      // check wether this worked out
-      ASSERT_NEAR(mt::one(),VTest::ColsAreNormalized(V_vp_,nloc_,ldaV_,stride_,mpi_comm_),tolV);
-      ASSERT_NEAR(mt::one(),VTest::ColsAreOrthogonal(V_vp_,nloc_,ldaV_,stride_,mpi_comm_),tolV);
-      
-      int nsteps=2;
-
-      // now orthogonalize W against V. The result should be such that Q*R1=W-V*R2, Q'*Q=I,V'*Q=0
-      rankVW=-42;
-      SUBR(orthog)(V_,Q_,R1_,R2_,nsteps,&rankVW,&iflag_);
-      if (iflag_!=+2)
-      {
-        ASSERT_EQ(0,iflag_);
-      }
-      ASSERT_EQ(m_+k_,rankVW);
-      
-      // check orthonormality of Q
-      ASSERT_NEAR(mt::one(),WTest::ColsAreNormalized(Q_vp_,nloc_,ldaQ_,stride_,mpi_comm_),tolW);
-      ASSERT_NEAR(mt::one(),WTest::ColsAreOrthogonal(Q_vp_,nloc_,ldaQ_,stride_,mpi_comm_),tolW);
-      
-      // check the decomposition: Q*R1 = W - V*R2 (compute W2=Q*R1+V*R2-W and compare with 0)
-      if (!useSVQB)
-      {
-        SUBR(mvec_times_sdMat)(st::one(),Q_,R1_,st::zero(),W2_,&iflag_);
-        ASSERT_EQ(0,iflag_);
-        SUBR(mvec_times_sdMat)(st::one(),V_,R2_,st::one(),W2_,&iflag_);
-        ASSERT_EQ(0,iflag_);
-        SUBR(mvec_add_mvec)(-st::one(),W_,st::one(),W2_,&iflag_);
-        ASSERT_EQ(0,iflag_);
-        ASSERT_NEAR(mt::one(),ArrayEqual(W2_vp_,nloc_,k_,ldaW2_,stride_,st::zero(),vflag_),tolW);
-      }
       // check the location of the resulting R parts
       _ST_ *R_vp;
       lidx_t ldaR;
@@ -354,6 +308,7 @@ public:
 
 #ifdef PHIST_SDMATS_ROW_MAJOR
 #warning "test not implemented for row-major sdMats"
+return;
 #endif
 
         _MT_ errR2=mt::one();
@@ -386,7 +341,7 @@ public:
 #if PHIST_OUTLEV>=PHIST_DEBUG
       PHIST_DEB("matrix H (last block-col should contain orthog-coefficients)");
       SUBR(sdMat_print)(R,&iflag_);
-#endif    
+#endif
       ASSERT_REAL_EQ(mt::one(),errR1);
       ASSERT_REAL_EQ(mt::one(),errR2);
     }
@@ -405,12 +360,6 @@ public:
       SUBR(mvec_put_value)(W_,st::one(),&iflag_);
       ASSERT_EQ(0,iflag_);
       
-      MT tolV=(MT)10.*VTest::releps(V_);
-      MT tolW=(MT)10.*WTest::releps(W_);
-
-      // copy Q=W because orthog() works in-place
-      SUBR(mvec_add_mvec)(st::one(),W_,st::zero(),Q_,&iflag_);
-      ASSERT_EQ(0,iflag_);
       lidx_t nR=_M_+_K_;
       TYPE(sdMat_ptr) R=NULL;
       SUBR(sdMat_create)(&R,nR,nR,comm_,&iflag_);
@@ -441,52 +390,10 @@ public:
       SUBR(sdMat_extract_view)(R2_,&R2_vp_,&this->ldaR2_,&this->iflag_);
       ASSERT_EQ(0,this->iflag_);
 
-
-      // orthogonalize the m columns of V
-      int rankVW=-42;
-      bool useSVQB=false;
-      SUBR(orthog)(NULL,V_,R0_,NULL,1,&rankVW,&iflag_);
-      if (iflag_!=+2)
-      {
-        ASSERT_EQ(_M_>1? 1:0,iflag_);
-      }
-      else
-      {
-        useSVQB=true;
-      }
-      ASSERT_EQ(1,rankVW);
-#if PHIST_OUTLEV>=PHIST_DEBUG
-      PHIST_DEB("coeffs for QR of V:\n");
-      SUBR(sdMat_print)(R0_,&iflag_);
-      PHIST_DEB("in correct position?\n");
-      SUBR(sdMat_print)(R,&iflag_);
-#endif      
-      // check wether this worked out
-      ASSERT_NEAR(mt::one(),VTest::ColsAreNormalized(V_vp_,nloc_,ldaV_,stride_,mpi_comm_),tolV);
-      ASSERT_NEAR(mt::one(),VTest::ColsAreOrthogonal(V_vp_,nloc_,ldaV_,stride_,mpi_comm_),tolV);
+      // test orthog routine, expect V and [V, W] to have rank 1
+      doOrthogTests(V_, W_, Q_, R0_, R1_, R2_, 
+        m_>1? 1:0, 1, 1, m_);
       
-      int nsteps=2;
-
-      // now orthogonalize W against V. The result should be such that Q*R1=W-V*R2, Q'*Q=I,V'*Q=0
-      rankVW=-42;
-      SUBR(orthog)(V_,Q_,R1_,R2_,nsteps,&rankVW,&iflag_);
-      ASSERT_EQ(_M_,rankVW);
-      
-      // check orthonormality of Q
-      ASSERT_NEAR(mt::one(),WTest::ColsAreNormalized(Q_vp_,nloc_,ldaQ_,stride_,mpi_comm_),tolW);
-      ASSERT_NEAR(mt::one(),WTest::ColsAreOrthogonal(Q_vp_,nloc_,ldaQ_,stride_,mpi_comm_),tolW);
-      
-      // check the decomposition: Q*R1 = W - V*R2 (compute W2=Q*R1+V*R2-W and compare with 0)
-      if (!useSVQB)
-      {
-        SUBR(mvec_times_sdMat)(st::one(),Q_,R1_,st::zero(),W2_,&iflag_);
-        ASSERT_EQ(0,iflag_);
-        SUBR(mvec_times_sdMat)(st::one(),V_,R2_,st::one(),W2_,&iflag_);
-        ASSERT_EQ(0,iflag_);
-        SUBR(mvec_add_mvec)(-st::one(),W_,st::one(),W2_,&iflag_);
-        ASSERT_EQ(0,iflag_);
-        ASSERT_NEAR(mt::one(),ArrayEqual(W2_vp_,nloc_,k_,ldaW2_,stride_,st::zero(),vflag_),tolW);
-      }
       // check the location of the resulting R parts
       _ST_ *R_vp;
       lidx_t ldaR;
@@ -496,6 +403,7 @@ public:
 
 #ifdef PHIST_SDMATS_ROW_MAJOR
 #warning "test not implemented for row-major sdMats"
+        return;
 #endif
 
         _MT_ errR2=mt::one();
