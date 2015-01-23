4e6e1511477a688fe687dffb77feed55ab69574e
Jonas Thies <jonas.thies@dlr.de>
2015-01-23 17:15:22 +0100

    new OrthogTests with [V,W] views in a single large block (as occurs in our Arnoldi implementation)

--- test/core/OrthogTest_def.hpp
+++ test/core/OrthogTest_def.hpp
@@ -443,279 +443,54 @@ return;
   }
 
   // check if we can orthogonalize W against V where V and W are views into Z=[V W].
-  TEST_F(CLASSNAME, DISABLED_random_vectors_viewing_same_block)
+  TEST_F(CLASSNAME, random_vectors_viewing_same_block)
   {
     if (typeImplemented_)
     {
-      // fill V and W with random numbers
-      SUBR(mvec_random)(V_,&iflag_);
+      TYPE(mvec_ptr) V_big=NULL, V=NULL, W=NULL;
+      int ncols = m_+k_+13;// some extra padding to make the test more interesting
+      SUBR(mvec_create)(&V_big,map_,ncols,&iflag_);
       ASSERT_EQ(0,iflag_);
-      SUBR(mvec_random)(W_,&iflag_);
+
+      // fill entire block with random numbers
+      SUBR(mvec_random)(V_big,&iflag_);
       ASSERT_EQ(0,iflag_);
       
-      MT tolV=(MT)10.*VTest::releps(V_);
-      MT tolW=(MT)10.*WTest::releps(W_);
-
-      // copy Q=W because orthog() works in-place
-      SUBR(mvec_add_mvec)(st::one(),W_,st::zero(),Q_,&iflag_);
+      // create views
+      SUBR(mvec_view_block)(V_big,&V,1,m_,&iflag_);
       ASSERT_EQ(0,iflag_);
-      lidx_t nR=_M_+_K_;
-      TYPE(sdMat_ptr) R=NULL;
-      SUBR(sdMat_create)(&R,nR,nR,comm_,&iflag_);
-      ASSERT_EQ(0,iflag_);
-      SUBR(sdMat_delete)(R0_,&iflag_);
-      ASSERT_EQ(0,iflag_);
-      R0_=NULL;
-      SUBR(sdMat_delete)(R1_,&iflag_);
-      ASSERT_EQ(0,iflag_);
-      R1_=NULL;
-      SUBR(sdMat_delete)(R2_,&iflag_);
-      ASSERT_EQ(0,iflag_);
-      R2_=NULL;
-      SUBR(sdMat_view_block)(R,&R0_,0,_M_-1,0,_M_-1,&iflag_);
-      ASSERT_EQ(0,iflag_);
-      // R1 is the k x k diagonal block:
-      SUBR(sdMat_view_block)(R,&R1_,_M_,_M_+_K_-1,_M_,_M_+_K_-1,&iflag_);
-      ASSERT_EQ(0,iflag_);
-      // R2 is the m'th block col with k cols and m rows
-      SUBR(sdMat_view_block)(R,&R2_,0,_M_-1,_M_,_M_+_K_-1,&iflag_);
+      SUBR(mvec_view_block)(V_big,&W,m_+1,m_+k_,&iflag_);
       ASSERT_EQ(0,iflag_);
 
-      // renew raw views
-      SUBR(sdMat_extract_view)(R0_,&R0_vp_,&this->ldaR0_,&this->iflag_);
-      ASSERT_EQ(0,this->iflag_);
-      SUBR(sdMat_extract_view)(R1_,&R1_vp_,&this->ldaR1_,&this->iflag_);
-      ASSERT_EQ(0,this->iflag_);
-      SUBR(sdMat_extract_view)(R2_,&R2_vp_,&this->ldaR2_,&this->iflag_);
-      ASSERT_EQ(0,this->iflag_);
-
-
-      // orthogonalize the m columns of V
-      int rankVW=-42;
-      bool useSVQB=false;
-      SUBR(orthog)(NULL,V_,R0_,NULL,1,&rankVW,&iflag_);
-      if (iflag_!=+2)
-      {
-        ASSERT_EQ(0,iflag_);
-      }
-      else
-      {
-        useSVQB=true;
-      }
-      ASSERT_EQ(_M_,rankVW);
-      // check wether this worked out
-      ASSERT_NEAR(mt::one(),VTest::ColsAreNormalized(V_vp_,nloc_,ldaV_,stride_,mpi_comm_),tolV);
-      ASSERT_NEAR(mt::one(),VTest::ColsAreOrthogonal(V_vp_,nloc_,ldaV_,stride_,mpi_comm_),tolV);
+      // test orthog routine, expect full rank of V and [V W]
+      doOrthogTests(V, W, Q_, R0_, R1_, R2_, 
+        0, 0, m_, m_+k_);
       
-      int nsteps=2;
-
-      // now orthogonalize W against V. The result should be such that Q*R1=W-V*R2, Q'*Q=I,V'*Q=0
-      rankVW=-42;
-      SUBR(orthog)(V_,Q_,R1_,R2_,nsteps,&rankVW,&iflag_);
-      if (iflag_!=+2)
-      {
-        ASSERT_EQ(0,iflag_);
-      }
-      ASSERT_EQ(m_+k_,rankVW);
-      
-      // check orthonormality of Q
-      ASSERT_NEAR(mt::one(),WTest::ColsAreNormalized(Q_vp_,nloc_,ldaQ_,stride_,mpi_comm_),tolW);
-      ASSERT_NEAR(mt::one(),WTest::ColsAreOrthogonal(Q_vp_,nloc_,ldaQ_,stride_,mpi_comm_),tolW);
-      
-      // check the decomposition: Q*R1 = W - V*R2 (compute W2=Q*R1+V*R2-W and compare with 0)
-      if (!useSVQB)
-      {
-        SUBR(mvec_times_sdMat)(st::one(),Q_,R1_,st::zero(),W2_,&iflag_);
-        ASSERT_EQ(0,iflag_);
-        SUBR(mvec_times_sdMat)(st::one(),V_,R2_,st::one(),W2_,&iflag_);
-        ASSERT_EQ(0,iflag_);
-        SUBR(mvec_add_mvec)(-st::one(),W_,st::one(),W2_,&iflag_);
-        ASSERT_EQ(0,iflag_);
-        ASSERT_NEAR(mt::one(),ArrayEqual(W2_vp_,nloc_,k_,ldaW2_,stride_,st::zero(),vflag_),tolW);
-      }
-      // check the location of the resulting R parts
-      _ST_ *R_vp;
-      lidx_t ldaR;
-
-      SUBR(sdMat_extract_view)(R,&R_vp,&ldaR,&this->iflag_);
-      ASSERT_EQ(0,this->iflag_);
-
-#ifdef PHIST_SDMATS_ROW_MAJOR
-#warning "test not implemented for row-major sdMats"
-#endif
-
-        _MT_ errR2=mt::one();
-        for (lidx_t i=0; i<m_; i++)
-        {
-          for (lidx_t j=0; j<k_; j++)
-          {
-            lidx_t jj = m_+j;
-            _ST_ a = R_vp[jj*ldaR+i];
-            _ST_ b = R2_vp_[j*ldaR2_+i];
-            _MT_ denom=st::abs(a+b);
-            if (denom==mt::zero()) denom=mt::one();
-            errR2+= st::abs(a-b)/denom;
-          }
-        }
-        _MT_ errR1=mt::one();
-        for (lidx_t i=0; i<k_; i++)
-        {
-          for (lidx_t j=0; j<k_; j++)
-          {
-            lidx_t ii = m_+i;
-            lidx_t jj = m_+j;
-            _ST_ a = R_vp[jj*ldaR+ii];
-            _ST_ b = R1_vp_[j*ldaR1_+i];
-            _MT_ denom=st::abs(a+b);
-            if (denom==mt::zero()) denom=mt::one();
-            errR1+= st::abs(a-b)/denom;
-          }
-        }
-#if PHIST_OUTLEV>=PHIST_DEBUG
-      PHIST_DEB("matrix H (last block-col should contain orthog-coefficients)");
-      SUBR(sdMat_print)(R,&iflag_);
-#endif    
-      ASSERT_REAL_EQ(mt::one(),errR1);
-      ASSERT_REAL_EQ(mt::one(),errR2);
     }
   }
 
   // check if we can orthogonalize W against V where V and W are views into Z=[V W].
-  TEST_F(CLASSNAME, DISABLED_one_vectors_viewing_same_block)
+  TEST_F(CLASSNAME, one_vectors_viewing_same_block)
   {
     if (typeImplemented_)
     {
-      // fill V and W with random numbers
-      SUBR(mvec_put_value)(V_,st::one(),&iflag_);
+      TYPE(mvec_ptr) V_big=NULL, V=NULL, W=NULL;
+      int ncols = m_+k_+13;// some extra padding to make the test more interesting
+      SUBR(mvec_create)(&V_big,map_,ncols,&iflag_);
       ASSERT_EQ(0,iflag_);
-      SUBR(mvec_put_value)(W_,st::one(),&iflag_);
+
+      // fill entire block with ones
+      SUBR(mvec_put_value)(V_big,st::one(),&iflag_);
       ASSERT_EQ(0,iflag_);
       
-      MT tolV=(MT)10.*VTest::releps(V_);
-      MT tolW=(MT)10.*WTest::releps(W_);
-
-      // copy Q=W because orthog() works in-place
-      SUBR(mvec_add_mvec)(st::one(),W_,st::zero(),Q_,&iflag_);
+      // create views
+      SUBR(mvec_view_block)(V_big,&V,1,m_,&iflag_);
       ASSERT_EQ(0,iflag_);
-      lidx_t nR=_M_+_K_;
-      TYPE(sdMat_ptr) R=NULL;
-      SUBR(sdMat_create)(&R,nR,nR,comm_,&iflag_);
-      ASSERT_EQ(0,iflag_);
-      SUBR(sdMat_delete)(R0_,&iflag_);
-      ASSERT_EQ(0,iflag_);
-      R0_=NULL;
-      SUBR(sdMat_delete)(R1_,&iflag_);
-      ASSERT_EQ(0,iflag_);
-      R1_=NULL;
-      SUBR(sdMat_delete)(R2_,&iflag_);
-      ASSERT_EQ(0,iflag_);
-      R2_=NULL;
-      SUBR(sdMat_view_block)(R,&R0_,0,_M_-1,0,_M_-1,&iflag_);
-      ASSERT_EQ(0,iflag_);
-      // R1 is the k x k diagonal block:
-      SUBR(sdMat_view_block)(R,&R1_,_M_,_M_+_K_-1,_M_,_M_+_K_-1,&iflag_);
-      ASSERT_EQ(0,iflag_);
-      // R2 is the m'th block col with k cols and m rows
-      SUBR(sdMat_view_block)(R,&R2_,0,_M_-1,_M_,_M_+_K_-1,&iflag_);
+      SUBR(mvec_view_block)(V_big,&W,m_+1,m_+k_,&iflag_);
       ASSERT_EQ(0,iflag_);
 
-      // renew raw views
-      SUBR(sdMat_extract_view)(R0_,&R0_vp_,&this->ldaR0_,&this->iflag_);
-      ASSERT_EQ(0,this->iflag_);
-      SUBR(sdMat_extract_view)(R1_,&R1_vp_,&this->ldaR1_,&this->iflag_);
-      ASSERT_EQ(0,this->iflag_);
-      SUBR(sdMat_extract_view)(R2_,&R2_vp_,&this->ldaR2_,&this->iflag_);
-      ASSERT_EQ(0,this->iflag_);
-
-
-      // orthogonalize the m columns of V
-      int rankVW=-42;
-      bool useSVQB=false;
-      SUBR(orthog)(NULL,V_,R0_,NULL,1,&rankVW,&iflag_);
-      if (iflag_!=+2)
-      {
-        ASSERT_EQ(_M_>1? 1:0,iflag_);
-      }
-      else
-      {
-        useSVQB=true;
-      }
-      ASSERT_EQ(1,rankVW);
-#if PHIST_OUTLEV>=PHIST_DEBUG
-      PHIST_DEB("coeffs for QR of V:\n");
-      SUBR(sdMat_print)(R0_,&iflag_);
-      PHIST_DEB("in correct position?\n");
-      SUBR(sdMat_print)(R,&iflag_);
-#endif      
-      // check wether this worked out
-      ASSERT_NEAR(mt::one(),VTest::ColsAreNormalized(V_vp_,nloc_,ldaV_,stride_,mpi_comm_),tolV);
-      ASSERT_NEAR(mt::one(),VTest::ColsAreOrthogonal(V_vp_,nloc_,ldaV_,stride_,mpi_comm_),tolV);
-      
-      int nsteps=2;
-
-      // now orthogonalize W against V. The result should be such that Q*R1=W-V*R2, Q'*Q=I,V'*Q=0
-      rankVW=-42;
-      SUBR(orthog)(V_,Q_,R1_,R2_,nsteps,&rankVW,&iflag_);
-      ASSERT_EQ(_M_,rankVW);
-      
-      // check orthonormality of Q
-      ASSERT_NEAR(mt::one(),WTest::ColsAreNormalized(Q_vp_,nloc_,ldaQ_,stride_,mpi_comm_),tolW);
-      ASSERT_NEAR(mt::one(),WTest::ColsAreOrthogonal(Q_vp_,nloc_,ldaQ_,stride_,mpi_comm_),tolW);
-      
-      // check the decomposition: Q*R1 = W - V*R2 (compute W2=Q*R1+V*R2-W and compare with 0)
-      if (!useSVQB)
-      {
-        SUBR(mvec_times_sdMat)(st::one(),Q_,R1_,st::zero(),W2_,&iflag_);
-        ASSERT_EQ(0,iflag_);
-        SUBR(mvec_times_sdMat)(st::one(),V_,R2_,st::one(),W2_,&iflag_);
-        ASSERT_EQ(0,iflag_);
-        SUBR(mvec_add_mvec)(-st::one(),W_,st::one(),W2_,&iflag_);
-        ASSERT_EQ(0,iflag_);
-        ASSERT_NEAR(mt::one(),ArrayEqual(W2_vp_,nloc_,k_,ldaW2_,stride_,st::zero(),vflag_),tolW);
-      }
-      // check the location of the resulting R parts
-      _ST_ *R_vp;
-      lidx_t ldaR;
-
-      SUBR(sdMat_extract_view)(R,&R_vp,&ldaR,&this->iflag_);
-      ASSERT_EQ(0,this->iflag_);
-
-#ifdef PHIST_SDMATS_ROW_MAJOR
-#warning "test not implemented for row-major sdMats"
-#endif
-
-        _MT_ errR2=mt::one();
-        for (lidx_t i=0; i<m_; i++)
-        {
-          for (lidx_t j=0; j<k_; j++)
-          {
-            lidx_t jj = m_+j;
-            _ST_ a = R_vp[jj*ldaR+i];
-            _ST_ b = R2_vp_[j*ldaR2_+i];
-            _MT_ denom=st::abs(a+b);
-            if (denom==mt::zero()) denom=mt::one();
-            errR2+= st::abs(a-b)/denom;
-          }
-        }
-        _MT_ errR1=mt::one();
-        for (lidx_t i=0; i<k_; i++)
-        {
-          for (lidx_t j=0; j<k_; j++)
-          {
-            lidx_t ii = m_+i;
-            lidx_t jj = m_+j;
-            _ST_ a = R_vp[jj*ldaR+ii];
-            _ST_ b = R1_vp_[j*ldaR1_+i];
-            _MT_ denom=st::abs(a+b);
-            if (denom==mt::zero()) denom=mt::one();
-            errR1+= st::abs(a-b)/denom;
-          }
-        }
-#if PHIST_OUTLEV>=PHIST_DEBUG
-      PHIST_DEB("matrix H (last block-col should contain orthog-coefficients)");
-      SUBR(sdMat_print)(R,&iflag_);
-#endif    
-      ASSERT_REAL_EQ(mt::one(),errR1);
-      ASSERT_REAL_EQ(mt::one(),errR2);
+      // test orthog routine, expect V and [V, W] to have rank 1
+      doOrthogTests(V, W, Q_, R0_, R1_, R2_, 
+        m_>1? 1:0, 1, 1, m_);
     }
   }
