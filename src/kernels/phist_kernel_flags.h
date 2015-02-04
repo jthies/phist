d455a5621bd49789e464b12758e964bec0facf84
Jonas Thies <jonas.thies@dlr.de>
2015-02-04 15:35:55 +0100

    added a header with flags that could be passed to the kernel functions to influence their behavior. So far, no flags are used by any kernel libs.

---
+++ src/kernels/phist_kernel_flags.h
@@ -0,0 +1,25 @@
+#ifndef PHIST_KERNEL_FLAGS_H
+#define PHIST_KERNEL_FLAGS_H
+
+/* These flags allow the PHIST programmer to give the kernel lib
+   "hints", but the kernel lib is not required to actually implement the feature.
+   For instance, when creating a sparse matrix, the user can ask for a communication-
+   reducing partitioning, but if the kernel lib does not support the feature, it may
+   just ignore the flag.
+   
+   In the documentation of the kernel functions, it is stated which flags are possible
+   inputs to the particular function (combined by bitwise ors, '|').
+   
+   If no flags are documented for a function in in phist_kernels_decl.h, none are used by 
+   any kernel lib.
+*/
+#define PHIST_IFLAG_DEFAULT 0
+
+/* sparse matrix preprocessing */
+#define PHIST_SPARSEMAT_REPARTITION 1
+#define PHIST_SPARSEMAT_DIST2_COLOR 2
+
+/* accuracy vs. speed */
+#define PHIST_ROBUST_REDUCTIONS 1
+
+#endif
