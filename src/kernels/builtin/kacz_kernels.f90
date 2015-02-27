006175e5c461ba4b1a6b57f903ba1818eabe2968
Jonas Thies <jonas.thies@dlr.de>
2015-02-27 16:41:36 +0100

    removed an obsolete warning

--- src/kernels/builtin/kacz_kernels.f90
+++ src/kernels/builtin/kacz_kernels.f90
@@ -186,11 +186,6 @@ subroutine dkacz_selector(nvec, nlocal, nhalo, ncols, nnz, &
 
 end subroutine dkacz_selector
 
-! TODO - is this still an issue?
-#if defined(__INTEL_COMPILER)&&defined(TESTING)&&defined(PHIST_HAVE_COLPACK)
-#warning "certain ifort debugging flags ('-check all', I think) break the real-only kacz kernel with coloring"
-#endif
-
 !! general implementation of forward or backward Kaczmarz sweep for a single shift
 !! and possibly multiple vector columns in X and B
 subroutine dkacz_selector_real(nvec, nlocal, nhalo, ncols, nnz, &
