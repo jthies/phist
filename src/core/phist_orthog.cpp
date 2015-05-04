jthies/phist

commit 9e143493b58b91cddd41d27cc9a7b17d1d87bb64

Add support for having a B matrix in the orthog and jdqr routines
This currently only works if you use different orthog and
jadaCorrectionSolver routines than the default ones in phist.

src/core/phist_orthog.cpp
@@ -26,7 +26,11 @@
 # ifdef PHIST_KERNEL_LIB_TPETRA
 #  include "BelosTpetraAdapter.hpp"
 #  include "phist_tpetra_typedefs.hpp"
-# else
+# elif defined(PHIST_KERNEL_LIB_EPETRA)
+#  include "Epetra_MultiVector.h"
+#  include "BelosEpetraAdapter.hpp"
+#  include "epetra_helpers.h"
+# else 
 #  error "not implemented"
 # endif
 
@@ -38,6 +42,13 @@
 # include "BelosIMGSOrthoManager.hpp"
 
 #endif /* USE_TRILINOS_ORTHO_MANAGER */
+
+#include "epetra_helpers.cpp"
+
+#include "Epetra_SerialDenseMatrix.h"
+#include "Epetra_LocalMap.h"
+#include "Epetra_SerialComm.h"
+#include "Epetra_CrsMatrix.h"
 
 #ifdef PHIST_HAVE_SP
 # include "phist_gen_s.h"

python/phist_core.py
@@ -72,8 +72,8 @@ for _varT in ('S', 'D', 'C', 'Z'):
     _declare(None, _prefix+'op_identity', (_op_ptr, c_int_p), skip_if_missing=True)
 
     # from phist_orthog_decl.h
-    #void SUBR(orthog)(TYPE(const_mvec_ptr) V, TYPE(mvec_ptr) W, TYPE(sdMat_ptr) R1, TYPE(sdMat_ptr) R2, int numSweeps, int* rankVW, int* iflag);
-    _declare(None, _prefix+'orthog', (_mvec_ptr, _mvec_ptr, _sdMat_ptr, _sdMat_ptr, c_int, c_int_p, c_int_p), skip_if_missing=True)
+    #void SUBR(orthog)(TYPE(const_mvec_ptr) V, TYPE(mvec_ptr) W, TYPE(const_op_ptr) B, TYPE(sdMat_ptr) R1, TYPE(sdMat_ptr) R2, int numSweeps, int* rankVW, int* iflag);
+    _declare(None, _prefix+'orthog', (_mvec_ptr, _mvec_ptr, _op_ptr, _sdMat_ptr, _sdMat_ptr, c_int, c_int_p, c_int_p), skip_if_missing=True)
 
 
 #--------------------------------------------------------------------------------
@@ -117,11 +117,11 @@ if __name__ == '__main__':
 
     # QR-orthogonalize vec1
     rnkV = ctypes.c_int()
-    PYST_CHK_NEG_IERR(phist_Dorthog, None, vec1, mat1, None, 1, rnkV)
+    PYST_CHK_NEG_IERR(phist_Dorthog, None, vec1, None, mat1, None, 1, rnkV)
 
     # orthogonalize vec2 wrt. vec1
     rnkVW = ctypes.c_int()
-    PYST_CHK_NEG_IERR(phist_Dorthog, vec1, vec2, mat2, mat3, 3, rnkVW)
+    PYST_CHK_NEG_IERR(phist_Dorthog, vec1, vec2, None, mat2, mat3, 3, rnkVW)
 
     # delete sdMats
     PYST_CHK_IERR(phist_DsdMat_delete, mat3)

src/core/phist_orthog.h
@@ -11,6 +11,8 @@
 #endif
 #include "phist_kernels.h"
 
+#include "phist_operator.h"
+
 #endif //DOXYGEN
 
 #ifdef __cplusplus

src/core/phist_orthog_decl.h
@@ -6,6 +6,11 @@
 //! (Q overwrites W here).                                         
 //! The matrices R1 and R2 must be pre-allocated by the caller.    
 //!                                                                
+//! There is an option to supply an operator for B, in which case  
+//! V should be a B-orthogonal basis and we B-orthogonalize W with 
+//! respect to V, such that instead Q'BQ=I. This is currectly not  
+//! implemented.                                                   
+//!                                                                
 //! The algorithm used is up to numSweeps steps of classical       
 //! block Gram-Schmidt, alternated with QR factorizations to       
 //! normalize W. The method stops if the reduction in norm of W    
@@ -16,7 +21,7 @@
 //! mutually orthogonal and orthogonal against V. On exit, the     
 //! variable rankWV will contain the original rank of the matrix   
 //! [V,W] before the orthogonalization/randomization of W. If [V,W]
-//! did not have full rank, *iflag=+1 is returned.                  
+//! did not have full rank, *iflag=+1 is returned.                 
 //!                                                                
 //! The implementation makes use of the kernel function mvec_QR    
 //! for the inner orthogonalization of W. If this function is not  
@@ -24,21 +29,23 @@
 //! fallback variant based on the SVQB algorithm. In this case,    
 //! the relation Q*R1 = W-V*R2 does not hold, but Q is orthonormal 
 //! and orthogonal against V anyway with the nullspace replaced    
-//! by random vectors. To indicate the invalid R's, iflag=+2 is     
+//! by random vectors. To indicate the invalid R's, iflag=+2 is    
 //! returned.                                                      
 //!                                                                
 //! If no random orthogonal vectors can be generated (after some   
-//! tries) iflag=-8 is returned. This may indicate a problem with   
+//! tries) iflag=-8 is returned. This may indicate a problem with  
 //! the random vector generator.                                   
 //!                                                                
 //! If the decrease in norm in one of the columns                  
 //! in the last CGS sweep indicates that the algorithm has not     
-//! yet converged, we return iflag=-9 to indicate that more steps   
+//! yet converged, we return iflag=-9 to indicate that more steps  
 //! may be advisable. This should not happen in practice if        
 //! numSweeps>=2 ('twice is enough').                              
 //!                                                                
+
 void SUBR(orthog)(TYPE(const_mvec_ptr) V,
                      TYPE(mvec_ptr) W,
+                     TYPE(const_op_ptr) B,
                      TYPE(sdMat_ptr) R1,
                      TYPE(sdMat_ptr) R2,
                      int numSweeps, int* rankVW, int* iflag);

src/core/phist_orthog_def.hpp
@@ -5,6 +5,7 @@ void SUBR(mvec_QB)(TYPE(mvec_ptr) V, TYPE(sdMat_ptr) B, _MT_* nrmsV, int *iflag)
 //! orthogonalize an mvec against an already orthogonal one.
 void SUBR(orthog)(TYPE(const_mvec_ptr) V,
                      TYPE(mvec_ptr) W,
+                     TYPE(const_op_ptr) B,
                      TYPE(sdMat_ptr) R1,
                      TYPE(sdMat_ptr) R2,
                      int numSweeps,
@@ -23,6 +24,11 @@ void SUBR(orthog)(TYPE(const_mvec_ptr) V,
   st::sdMat_t *R1p,*R2p,*R1pp;
 
   const_comm_ptr_t comm=NULL;
+
+  if (B!=NULL)
+  {
+    PHIST_SOUT(PHIST_WARNING,"case B!=I not implemented (file %s, line %d)\n",__FILE__,__LINE__);
+  }
 
   bool useSVQB=false;
   *iflag=0;

src/core/trili_orthog_def.hpp
@@ -2,11 +2,14 @@
 //! by one that uses an ortho manager from Belos. This is
 //! mostly for testing our own code but can also be used to
 //! quickly incorporate new developments in their code.
+
 void SUBR(orthog)(TYPE(const_mvec_ptr) V,
                      TYPE(mvec_ptr) W,
+                     TYPE(const_op_ptr) B,
                      TYPE(sdMat_ptr) R1,
                      TYPE(sdMat_ptr) R2,
                      int numSweeps,
+                     int* rankVW,
                      int* iflag)
   {
   PHIST_ENTER_FCN(__FUNCTION__);
@@ -72,9 +75,15 @@ typedef phist::tpetra::Traits< ST >::mvec_t BelosMV;
 typedef phist::tpetra::Traits< ST >::mvec_t MAT;
 typedef phist::tpetra::Traits< ST >::mvec_t BelosMAT;
 typedef phist::tpetra::Traits< ST >::Teuchos_sdMat_t TeuchosMAT;
+#elif defined(PHIST_KERNEL_LIB_EPETRA)
+typedef Epetra_MultiVector MV;
+typedef MV MAT;
+typedef Epetra_MultiVector BelosMV;
+typedef BelosMV BelosMAT;
+typedef Teuchos::SerialDenseMatrix<int,ST > TeuchosMAT;
 #elif defined(PHIST_KERNEL_LIB_GHOST)
 typedef ghost_vec_t MV;
-typedef MV MAT
+typedef MV MAT;
 typedef phist::GhostMV BelosMV;
 typedef BelosMV BelosMAT;
 typedef Teuchos::SerialDenseMatrix<int,ST > TeuchosMAT;
@@ -91,6 +100,9 @@ Teuchos::RCP<TeuchosMAT> R1_2 =
         phist::tpetra::Traits< ST >::CreateTeuchosViewNonConst(R1_1,iflag);
 Teuchos::RCP<TeuchosMAT> R2_2 = 
         phist::tpetra::Traits< ST >::CreateTeuchosViewNonConst(R2_1,iflag);
+#elif defined(PHIST_KERNEL_LIB_EPETRA)
+        Teuchos::RCP<TeuchosMAT> R1_2 = CreateTeuchosViewNonConst(R1_1,iflag);
+        Teuchos::RCP<TeuchosMAT> R2_2 = CreateTeuchosViewNonConst(R2_1,iflag);
 #else
 #error "not implemented"
 #endif
@@ -127,6 +139,9 @@ ortho = tsqr;
 
 typedef Belos::IMGSOrthoManager<ST,MV,TYPE(op)> orthoMan_t;
 Teuchos::RCP<orthoMan_t> imgs = Teuchos::rcp(new orthoMan_t("hist/orthog/imgs"));
+if (B != NULL) {
+  imgs = Teuchos::rcp(new orthoMan_t("hist/orthog/imgs", Teuchos::rcp(B, false)));
+}
 ortho = imgs;
 
   default_params = imgs->getValidParameters();
@@ -142,7 +157,36 @@ ortho = imgs;
 
   Teuchos::ArrayView< Teuchos::RCP<const BelosMV > > V_array( &V_2, 1 );
   Teuchos::Array< Teuchos::RCP<TeuchosMAT > > R2_array( 1, R2_2 );
-  rankW=ortho->projectAndNormalize( *W_2, R2_array, R1_2, V_array );
+  *rankVW=ortho->projectAndNormalize( *W_2, R2_array, R1_2, V_array );
 
+#if PHIST_OUTLEV>=PHIST_DEBUG
+  if (B != NULL)
+  {
+    const Epetra_MultiVector *Vview = (const Epetra_MultiVector *)V;
+    Epetra_MultiVector *Wview = (Epetra_MultiVector *)V;
+
+    int m=Vview->NumVectors();
+    int k=Wview->NumVectors();
+
+    PHIST_CAST_PTR_FROM_VOID(const Epetra_CrsMatrix,B_ptr,B->A,*iflag);
+
+    Epetra_MultiVector tmp(Wview->Map(), k);
+
+    B_ptr->Multiply(false, *Wview, tmp);
+    Epetra_SerialDenseMatrix C(m,k);
+    Epetra_SerialComm comm;
+    Epetra_LocalMap tinyMap(m,0,comm);
+    Epetra_MultiVector Cview(View,tinyMap,C.A(),C.LDA(),k);
+    Cview.Multiply('T','N',1.0,*Vview,tmp,0.0);
+
+    PHIST_DEB("V'BV matrix\n");
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < k; j++) {
+        PHIST_DEB("%f ", C[j][i]);
+      }
+      PHIST_DEB("\n");;
+    }
+  }
+#endif
   *iflag = 0; //ncols-rankW;// return positive number if rank not full.
   }

src/jada/phist_jdqr_def.hpp
@@ -33,10 +33,10 @@
 //!
 
 // First a forward decleration of a helper function to compute the residual
-void SUBR(computeResidual)(TYPE(const_op_ptr) B_op,
-        TYPE(mvec_ptr) r_ptr, TYPE(mvec_ptr) Au_ptr, TYPE(mvec_ptr) u_ptr,
-        TYPE(mvec_ptr) rtil_ptr, TYPE(mvec_ptr) Qv, TYPE(sdMat_ptr) Theta,
-        TYPE(sdMat_ptr) atil, TYPE(sdMat_ptr) atilv, _MT_ *resid,
+void SUBR(computeResidual)(TYPE(const_op_ptr) B_op, TYPE(mvec_ptr) r_ptr,
+        TYPE(mvec_ptr) Au_ptr, TYPE(mvec_ptr) u_ptr, TYPE(mvec_ptr) rtil_ptr,
+        TYPE(mvec_ptr) Qv, TYPE(mvec_ptr) tmp, TYPE(sdMat_ptr) Theta,
+        TYPE(sdMat_ptr) atil, TYPE(sdMat_ptr) *atilv, _MT_ *resid,
         int nv, int nconv, int* iflag);
 
 // Now the actual main function
@@ -72,7 +72,7 @@ void SUBR(jdqr)(TYPE(const_op_ptr) A_op, TYPE(const_op_ptr) B_op,
   mvec_ptr_t    u_r=NULL, Au_r=NULL, r_r=NULL, rtil_r=NULL, t_r=NULL;
 #endif
   // these point either to u or u_r etc. to make live simpler further down
-  mvec_ptr_t u_ptr=NULL, Au_ptr=NULL, r_ptr=NULL, rtil_ptr=NULL,t_ptr=NULL;
+  mvec_ptr_t u_ptr=NULL, Au_ptr=NULL, r_ptr=NULL, rtil_ptr=NULL, t_ptr=NULL;
   // Q*s (temporary vector)
   sdMat_ptr_t atil=NULL, atilv=NULL;
   // matrix <V,AV>
@@ -161,7 +161,7 @@ void SUBR(jdqr)(TYPE(const_op_ptr) A_op, TYPE(const_op_ptr) B_op,
   // orthogonalizing against [V Q], which is of dimension up to n x (maxBas+numEigs)
   PHIST_CHK_IERR(SUBR(sdMat_create)(&S,maxVecs+numEigs,maxVecs+numEigs,comm,iflag),*iflag);
   PHIST_CHK_IERR(SUBR(sdMat_create)(&T,maxVecs+numEigs,maxVecs+numEigs,comm,iflag),*iflag);
-  PHIST_CHK_IERR(SUBR(sdMat_create)(&R,numEigs,numEigs,comm,iflag),*iflag);
+  PHIST_CHK_IERR(SUBR(sdMat_create)(&R,numEigs+1,numEigs+1,comm,iflag),*iflag);
   PHIST_CHK_IERR(SUBR(sdMat_put_value)(R,st::zero(),iflag),*iflag);
 
   PHIST_CHK_IERR(SUBR(sdMat_create)(&atil,maxVecs,nv_max,comm,iflag),*iflag);
@@ -305,7 +305,7 @@ void SUBR(jdqr)(TYPE(const_op_ptr) A_op, TYPE(const_op_ptr) B_op,
           PHIST_CHK_IERR(SUBR(mvec_set_block)(Vv,Qv,m0,ncVQ-1,iflag),*iflag);
           }
         int rankV;
-        PHIST_CHK_NEG_IERR(SUBR(orthog)(Vv,t_ptr,Tv,Sv,3,&rankV,iflag),*iflag);
+        PHIST_CHK_NEG_IERR(SUBR(orthog)(Vv,t_ptr,B_op,Tv,Sv,3,&rankV,iflag),*iflag);
         // reset the view of V without the Q.
         PHIST_CHK_IERR(SUBR(mvec_view_block)(V,&Vv,0,m0-1,iflag),*iflag);
       }
@@ -453,7 +453,7 @@ void SUBR(jdqr)(TYPE(const_op_ptr) A_op, TYPE(const_op_ptr) B_op,
     prev_nrm=res_nrm;
 
     PHIST_CHK_IERR(SUBR(computeResidual)(B_op, r_ptr, Au_ptr, u_ptr,
-        rtil_ptr, Qv, Theta, atil, atilv, &res_nrm, nv, nconv, iflag),*iflag);
+        rtil_ptr, Qv, Vtmp, Theta, atil, &atilv, &res_nrm, nv, nconv, iflag),*iflag);
 
     PHIST_SOUT(PHIST_INFO,"JDQR Iter %d\tdim(V)=%d\ttheta=%8.4g%+8.4gi\t\tr_est=%8.4e\n",it,m,ct::real(theta),ct::imag(theta),res_nrm);
   // deflate converged eigenpairs
@@ -504,11 +504,11 @@ void SUBR(jdqr)(TYPE(const_op_ptr) A_op, TYPE(const_op_ptr) B_op,
     mvec_ptr_t v_tmp=NULL;
     PHIST_CHK_IERR(SUBR(mvec_view_block)(Vtmp,&v_tmp,0,m-1,iflag),*iflag);
     PHIST_CHK_IERR(SUBR(mvec_times_sdMat)(st::one(),Vv,Sv,st::zero(),v_tmp,iflag),*iflag);
-    PHIST_CHK_IERR(SUBR(mvec_set_block)(Vv,v_tmp,0,m-1,iflag),*iflag);
+    PHIST_CHK_IERR(SUBR(mvec_set_block)(V,v_tmp,0,m-1,iflag),*iflag);
     PHIST_CHK_IERR(SUBR(mvec_view_block)(V,&Vv,0,m-1,iflag),*iflag);
     //AV=AV*S;
     PHIST_CHK_IERR(SUBR(mvec_times_sdMat)(st::one(),AVv,Sv,st::zero(),v_tmp,iflag),*iflag);
-    PHIST_CHK_IERR(SUBR(mvec_set_block)(AVv,v_tmp,0,m-1,iflag),*iflag);
+    PHIST_CHK_IERR(SUBR(mvec_set_block)(AV,v_tmp,0,m-1,iflag),*iflag);
     PHIST_CHK_IERR(SUBR(mvec_view_block)(AV,&AVv,0,m-1,iflag),*iflag);
     
     // delete the temporary view (not the data, of course)
@@ -545,11 +545,12 @@ void SUBR(jdqr)(TYPE(const_op_ptr) A_op, TYPE(const_op_ptr) B_op,
       t_ptr=t_r;
     }
 #endif
+
     // get the diagonal block (1x1 or 2x2) corresponding to theta
     PHIST_CHK_IERR(SUBR(sdMat_view_block)(T,&Theta,ev_pos,ev_pos+nv-1,ev_pos,ev_pos+nv-1,iflag),*iflag);
 
     PHIST_CHK_IERR(SUBR(computeResidual)(B_op, r_ptr, Au_ptr, u_ptr,
-        rtil_ptr, Qv, Theta, atil, atilv, &res_nrm, nv, nconv, iflag),*iflag);
+        rtil_ptr, Qv, Vtmp, Theta, atil, &atilv, &res_nrm, nv, nconv, iflag),*iflag);
 
     // again, sort the largest Ritz value to the top
 //    nselect = m==maxBas-1? minBas: 1;
@@ -587,10 +588,10 @@ void SUBR(jdqr)(TYPE(const_op_ptr) A_op, TYPE(const_op_ptr) B_op,
     mvec_ptr_t v_tmp=NULL;
     PHIST_CHK_IERR(SUBR(mvec_view_block)(Vtmp,&v_tmp,0,m-1,iflag),*iflag);
     PHIST_CHK_IERR(SUBR(mvec_times_sdMat)(st::one(),Vv,Sv,st::zero(),v_tmp,iflag),*iflag);
-    PHIST_CHK_IERR(SUBR(mvec_set_block)(Vv,v_tmp,0,m-1,iflag),*iflag);
+    PHIST_CHK_IERR(SUBR(mvec_set_block)(V,v_tmp,0,m-1,iflag),*iflag);
     //AV=AV*S;
     PHIST_CHK_IERR(SUBR(mvec_times_sdMat)(st::one(),AVv,Sv,st::zero(),v_tmp,iflag),*iflag);
-    PHIST_CHK_IERR(SUBR(mvec_set_block)(AVv,v_tmp,0,m-1,iflag),*iflag);
+    PHIST_CHK_IERR(SUBR(mvec_set_block)(AV,v_tmp,0,m-1,iflag),*iflag);
     PHIST_CHK_IERR(SUBR(mvec_delete)(v_tmp,iflag),*iflag);
     //V=V(:,1:mmin);
     PHIST_CHK_IERR(SUBR(mvec_view_block)(V,&Vv,0,m-1,iflag),*iflag);
@@ -633,10 +634,6 @@ void SUBR(jdqr)(TYPE(const_op_ptr) A_op, TYPE(const_op_ptr) B_op,
 
     // the block case is not implemented here:
     // (I-uu')A(I-uu')*t - (I-uu')t*Theta=-r
-    if (B_op!=NULL)
-    {
-      PHIST_SOUT(PHIST_WARNING,"case B!=I not implemented (file %s, line %d)\n",__FILE__,__LINE__);
-    }
 
     // 1/2^mm, but at most the outer tol as conv tol for GMRES
     MT innerTol[2];
@@ -646,7 +643,7 @@ void SUBR(jdqr)(TYPE(const_op_ptr) A_op, TYPE(const_op_ptr) B_op,
 
     // allow at most 25 iterations
     int nIt=25;
-      PHIST_CHK_NEG_IERR(SUBR(jadaCorrectionSolver_run)(innerSolv, A_op, NULL, Qtil, NULL, 
+      PHIST_CHK_NEG_IERR(SUBR(jadaCorrectionSolver_run)(innerSolv, A_op, B_op, Qtil, NULL, 
                                                       sigma, rtil_ptr, NULL, 
                                                       innerTol, nIt, t_ptr, true, false, iflag), *iflag);
     expand=nv;
@@ -788,10 +785,10 @@ void SUBR(jdqr)(TYPE(const_op_ptr) A_op, TYPE(const_op_ptr) B_op,
   return;
   }
 
-void SUBR(computeResidual)(TYPE(const_op_ptr) B_op,
-        TYPE(mvec_ptr) r_ptr, TYPE(mvec_ptr) Au_ptr, TYPE(mvec_ptr) u_ptr,
-        TYPE(mvec_ptr) rtil_ptr, TYPE(mvec_ptr) Qv, TYPE(sdMat_ptr) Theta,
-        TYPE(sdMat_ptr) atil, TYPE(sdMat_ptr) atilv, _MT_ *resid,
+void SUBR(computeResidual)(TYPE(const_op_ptr) B_op, TYPE(mvec_ptr) r_ptr,
+        TYPE(mvec_ptr) Au_ptr, TYPE(mvec_ptr) u_ptr, TYPE(mvec_ptr) rtil_ptr,
+        TYPE(mvec_ptr) Qv, TYPE(mvec_ptr) tmp, TYPE(sdMat_ptr) Theta,
+        TYPE(sdMat_ptr) atil, TYPE(sdMat_ptr) *atilv, _MT_ *resid,
         int nv, int nconv, int* iflag)
 {
   PHIST_ENTER_FCN(__FUNCTION__);
@@ -801,11 +798,25 @@ void SUBR(computeResidual)(TYPE(const_op_ptr) B_op,
 
   MT nrm[2];
 
+  // pointer to temporary storage for B*Q*atil if B is defined
+  mvec_ptr_t tmp_ptr=NULL;
+
   // first: r=Au
   PHIST_CHK_IERR(SUBR(mvec_add_mvec)(st::one(),Au_ptr,st::zero(),r_ptr,iflag),*iflag);
 
-  // update r = r - U*Theta
-  PHIST_CHK_IERR(SUBR(mvec_times_sdMat)(-st::one(),u_ptr,Theta,st::one(),r_ptr,iflag),*iflag);
+  if (B_op != NULL)
+  {
+    // rt = B * u
+    PHIST_CHK_IERR(B_op->apply(st::one(),B_op->A,u_ptr,st::zero(),rtil_ptr,iflag),*iflag);
+
+    // update r = r - rt*Theta
+    PHIST_CHK_IERR(SUBR(mvec_times_sdMat)(-st::one(),rtil_ptr,Theta,st::one(),r_ptr,iflag),*iflag);
+  }
+  else
+  {
+    // update r = r - u*Theta
+    PHIST_CHK_IERR(SUBR(mvec_times_sdMat)(-st::one(),u_ptr,Theta,st::one(),r_ptr,iflag),*iflag);
+  }
 
   // set rtil=r
   PHIST_CHK_IERR(SUBR(mvec_add_mvec)(st::one(),r_ptr,st::zero(),rtil_ptr,iflag),*iflag);
@@ -815,13 +826,27 @@ void SUBR(computeResidual)(TYPE(const_op_ptr) B_op,
   if (nconv>0)
   {
     // view next ~a, a temporary vector to compute ~a=Q'*r
-    PHIST_CHK_IERR(SUBR(sdMat_view_block)(atil,&atilv,0,nconv-1,0,nv-1,iflag),*iflag);
+    PHIST_CHK_IERR(SUBR(sdMat_view_block)(atil,atilv,0,nconv-1,0,nv-1,iflag),*iflag);
 
     //atil = Q'*r;
-    PHIST_CHK_IERR(SUBR(mvecT_times_mvec)(st::one(),Qv,r_ptr,st::zero(),atilv,iflag),*iflag);
+    PHIST_CHK_IERR(SUBR(mvecT_times_mvec)(st::one(),Qv,r_ptr,st::zero(),*atilv,iflag),*iflag);
 
-    //rtil = r-Q*atil;
-    PHIST_CHK_IERR(SUBR(mvec_times_sdMat)(-st::one(),Qv,atilv,st::one(),rtil_ptr,iflag),*iflag);
+    if (B_op != NULL)
+    {
+      PHIST_CHK_IERR(SUBR(mvec_view_block)(tmp,&tmp_ptr,0,nv-1,iflag),*iflag);
+      // tmp = Q*atil
+      PHIST_CHK_IERR(SUBR(mvec_times_sdMat)(st::one(),Qv,*atilv,st::zero(),tmp_ptr,iflag),*iflag);
+
+      //rtil = r-B*Q*atil;
+      PHIST_CHK_IERR(B_op->apply(-st::one(),B_op->A,tmp_ptr,st::one(),rtil_ptr,iflag),*iflag);
+
+      PHIST_CHK_IERR(SUBR(mvec_delete)(tmp_ptr,iflag),*iflag);
+    }
+    else
+    {
+      //rtil = r-Q*atil;
+      PHIST_CHK_IERR(SUBR(mvec_times_sdMat)(-st::one(),Qv,*atilv,st::one(),rtil_ptr,iflag),*iflag);
+    }
   }
 
   //nrm=norm(rtil);

src/jada/phist_subspacejada_def.hpp
@@ -806,7 +806,7 @@ PHIST_SOUT(PHIST_INFO,"\n");
       PHIST_CHK_IERR(SUBR( sdMat_view_block ) (R_H_,&R_H, 0,     nV-1,      0,     k-1,       iflag), *iflag);
       PHIST_CHK_IERR(SUBR( sdMat_view_block ) (R_H_,&Rr_H,nV,    nV+k-1,    nV,    nV+k-1,    iflag), *iflag);
       int rankV;
-      PHIST_CHK_NEG_IERR(SUBR( orthog ) (Vful, Vv, Rr_H, R_H, 5, &rankV, iflag), *iflag);
+      PHIST_CHK_NEG_IERR(SUBR( orthog ) (Vful, Vv, NULL, Rr_H, R_H, 5, &rankV, iflag), *iflag);
       // TODO: only take non-random vector if *iflag > 0
       // calculate AVv, BVv
       PHIST_CHK_IERR( A_op->apply(st::one(), A_op->A, Vv, st::zero(), AVv, iflag), *iflag);

commit 1e39c01135acd05715b69d8ed9d97f319a4a8bb5

Fix compilation without Trilinos
Regression from 9e143493b58b91cddd41d27cc9a7b17d

src/core/phist_orthog.cpp
@@ -29,7 +29,14 @@
 # elif defined(PHIST_KERNEL_LIB_EPETRA)
 #  include "Epetra_MultiVector.h"
 #  include "BelosEpetraAdapter.hpp"
-#  include "epetra_helpers.h"
+
+// we include the cpp file here because epetra_helpers is not
+// compiled into a library anywhere??
+#  include "epetra_helpers.cpp"
+
+#  include "Epetra_SerialComm.h"
+#  include "Epetra_SerialDenseMatrix.h"
+#  include "Epetra_CrsMatrix.h"
 # else 
 #  error "not implemented"
 # endif
@@ -43,13 +50,6 @@
 
 #endif /* USE_TRILINOS_ORTHO_MANAGER */
 
-#include "epetra_helpers.cpp"
-
-#include "Epetra_SerialDenseMatrix.h"
-#include "Epetra_LocalMap.h"
-#include "Epetra_SerialComm.h"
-#include "Epetra_CrsMatrix.h"
-
 #ifdef PHIST_HAVE_SP
 # include "phist_gen_s.h"
 # include "phist_orthog_def.hpp"
@@ -58,6 +58,10 @@
 #endif
 
 #include "phist_gen_d.h"
-#include "phist_orthog_def.hpp"
+#ifdef USE_TRILINOS_ORTHO_MANAGER
+# include "trili_orthog_def.hpp"
+#else
+# include "phist_orthog_def.hpp"
+#endif
 #include "phist_gen_z.h"
 #include "phist_orthog_def.hpp"