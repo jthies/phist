8623bdb546dbc3cadfb61629d92a8c73b77339a3
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2015-05-11 18:13:25 +0200

    Just some debug output

--- test/task/MainTaskTest.cpp
+++ test/task/MainTaskTest.cpp
@@ -75,6 +75,7 @@ TEST_F(MainTaskTest, DISABLED_task_has_threads)
 #endif
 {
   ASSERT_TRUE(curTask_ != NULL);
+  PHIST_OUT(PHIST_DEBUG, " has nThreads = %d\n", curTask_->nThreads);
 #ifdef PHIST_KERNEL_LIB_GHOST
   ASSERT_GT(curTask_->nThreads, 0);
 #else
