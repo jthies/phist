48b4446a4e757911e0fbf6eb7d0f5af685214280
Jonas Thies <jonas.thies@dlr.de>
2015-08-04 13:54:59 +0200

    workaround for issue #131 in driver mvec_RR_accuracy

--- examples/core/Dmvec_RR_accuracy.cpp
+++ examples/core/Dmvec_RR_accuracy.cpp
@@ -26,6 +26,35 @@
 typedef phist::ScalarTraits<ST> st;
 typedef phist::ScalarTraits<MT> mt;
 
+// workaround for #131 (put_func fails with Intel 15.0up02)
+void SUBR(mvec_put_func_substitute)(TYPE(mvec_ptr) V, int (*func)(gidx_t,lidx_t,void*),int* ierr)
+{
+  _ST_ *val;
+  lidx_t nloc, lda, ncols;
+  gidx_t ilower, iupper;
+  const_map_ptr_t map;
+  PHIST_CHK_IERR(SUBR(mvec_get_map)(V,&map,ierr),*ierr);
+  PHIST_CHK_IERR(SUBR(mvec_num_vectors)(V,&ncols,ierr),*ierr);
+  PHIST_CHK_IERR(SUBR(mvec_my_length)(V,&nloc,ierr),*ierr);
+  PHIST_CHK_IERR(phist_map_get_ilower(map,&ilower,ierr),*ierr);
+  PHIST_CHK_IERR(phist_map_get_iupper(map,&iupper,ierr),*ierr);
+  PHIST_CHK_IERR(SUBR(mvec_from_device)(V,ierr),*ierr);
+  PHIST_CHK_IERR(SUBR(mvec_extract_view)(V,&val,&lda,ierr),*ierr);
+  for (lidx_t i=0; i<nloc; i++)
+  {
+    gidx_t ii=i+ilower;
+    for (lidx_t jj=0; jj<ncols; jj++)
+    {
+#ifdef PHIST_MVECS_ROW_MAJOR
+      func(ii,jj,&val[i*lda+jj]);
+#else
+      func(ii,jj,&val[jj*lda+i]);
+#endif
+    }
+  }
+  PHIST_CHK_IERR(SUBR(mvec_to_device)(V,ierr),*ierr);
+}
+
 // prototypes and mvec initialization functions
 
 // function that performs numerical tests
@@ -122,7 +151,8 @@ PHIST_MAIN_TASK_BEGIN
   m=20;
   PHIST_ICHK_IERR(phist_map_create(&map,comm,n,&ierr),ierr);
   PHIST_ICHK_IERR(SUBR(mvec_create)(&X,map,m,&ierr),ierr);
-  PHIST_ICHK_IERR(SUBR(mvec_put_func)(X,&hilbert,&ierr),ierr);
+  //PHIST_ICHK_IERR(SUBR(mvec_put_func)(X,&hilbert,&ierr),ierr);
+  PHIST_ICHK_IERR(SUBR(mvec_put_func_substitute)(X,&hilbert,&ierr),ierr);
     PHIST_SOUT(PHIST_INFO,"PROBLEM: Hilbert%dx%d matrix, standard\n",(int)n,(int)m);
     run_tests1(X,false);
     PHIST_SOUT(PHIST_INFO,"PROBLEM: Hilbert%dx%d matrix, high_prec\n",(int)n,(int)m);
@@ -134,7 +164,8 @@ PHIST_MAIN_TASK_BEGIN
   m=20;
   PHIST_ICHK_IERR(phist_map_create(&map,comm,n,&ierr),ierr);
   PHIST_ICHK_IERR(SUBR(mvec_create)(&X,map,m,&ierr),ierr);
-  PHIST_ICHK_IERR(SUBR(mvec_put_func)(X,&synth1,&ierr),ierr);
+  PHIST_ICHK_IERR(SUBR(mvec_put_func_substitute)(X,&synth1,&ierr),ierr);
+  //PHIST_ICHK_IERR(SUBR(mvec_put_func)(X,&synth1,&ierr),ierr);
     PHIST_SOUT(PHIST_INFO,"PROBLEM: Synth%dx%d matrix, standard\n",(int)n,(int)m);
     run_tests1(X,false);
     PHIST_SOUT(PHIST_INFO,"PROBLEM: Synth%dx%d matrix, high_prec\n",(int)n,(int)m);
