e8aa1d6c833de1a3ac3d00b491cee08d80f3d6fc
Ubuntu Linux (2) <jonas.thies@dlr.de>
2015-08-19 12:17:02 -0700

    make sure that thread_local is somehow defined before used in header file phist_fcmtrace.h This may cause problems otherwise if phist is installed and used from another application which is compiled with a compiler and/or flags that disable the thread_local keyword.

--- src/tools/phist_fcntrace.hpp
+++ src/tools/phist_fcntrace.hpp
@@ -16,6 +16,34 @@
 #include <likwid.h>
 #endif
 
+/* we have to make sure that thread_local is available even if the 
+ * macro PHIST_HAVE_CXX11_THREADLOCAL is set because an external user
+ * may include this header with a different compiler or different flags.
+ *
+ * This hack was taken from 
+ *
+ * http://stackoverflow.com/questions/18298280/how-to-declare-a-variable-as-thread-local-portably
+ *
+ */
+#ifndef thread_local
+# if __STDC_VERSION__ >= 201112 && !defined __STDC_NO_THREADS__
+#  define thread_local _Thread_local
+# elif defined _WIN32 && ( \
+       defined _MSC_VER || \
+       defined __ICL || \
+       defined __DMC__ || \
+       defined __BORLANDC__ )
+#  define thread_local __declspec(thread) 
+/* note that ICC (linux) and Clang are covered by __GNUC__ */
+# elif defined __GNUC__ || \
+       defined __SUNPRO_C || \
+       defined __xlC__
+#  define thread_local __thread
+# elif defined(PHIST_HAVE_CXX11_THREADLOCAL
+#  warning "Cannot define thread_local, please check compiler and flags used to compile phist."
+# endif
+#endif
+
 #endif /* DOXYGEN */
 
 // this is an object that prints a message when created and when destroyed.
