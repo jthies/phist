470420ef0d72e5fc0346d6f7e5250b512251d48b
Jonas Thies <jonas.thies@dlr.de>
2015-09-04 16:36:30 +0200

    added a second carp_sweep kernel which should operate on
    A-sI Q
    Q'   0
    
    (not implemented by any kernel lib, yet)

--- src/kernels/phist_kernels_carp_decl.h
+++ src/kernels/phist_kernels_carp_decl.h
@@ -2,9 +2,13 @@
 extern "C" {
 #endif
 
-//! kernels to implement CARP for the matrix sigma[j]I-A.
+//! kernels to implement CARP for the matrix A-sigma[j]I, or
+//! (A-sigma[j]I, Q; Q', 0], with Q orthonormal. The parallelization
+//! and implementation of the `bordering' with Q are left to the
+//! kernel lib, it may use e.g. CARP, multi-coloring etc., project out
+//! all Q columns at once or one-by-one.
 
-//! create data structures needed for subsequent calls to carp_sweep: 
+//! create data structures needed for subsequent calls to carp_sweep:
 //!
 //! input: 
 //!
@@ -35,6 +39,24 @@ void SUBR(carp_sweep)(TYPE(const_sparseMat_ptr) A,
         _MT_ const sigma_r[], _MT_ const sigma_i[],
         TYPE(const_mvec_ptr) Rhs,
         TYPE(mvec_ptr) X_r, TYPE(mvec_ptr) X_i,
+        void* const work,
+        _MT_ const * omega, int* iflag);
+
+//!                                                                     
+//! perform KACZ forward/backward sweep on the `augmented' system       
+//!                                                                     
+//! | A-sigma[j]I  Q ||X|   |Rhs|                                       
+//! | Q'           0 ||q| = |0  |                                       
+//!                                                                     
+//! where Q'Q=I should be orthonormal. For the moment we only allow a   
+//! real-valued Q in real arithmetic (this may change in the future).   
+//!                                                                     
+void SUBR(carp_sweep_aug)(TYPE(const_sparseMat_ptr) A,
+        _MT_ const sigma_r[], _MT_ const sigma_i[],
+        TYPE(const_mvec_ptr) Q,
+        TYPE(const_mvec_ptr) Rhs,
+        TYPE(mvec_ptr) X_r, TYPE(mvec_ptr) X_i,
+        TYPE(sdMat_ptr) q_r, TYPE(sdMat_ptr) q_i,
         void* const work,
         _MT_ const * omega, int* iflag);
 
