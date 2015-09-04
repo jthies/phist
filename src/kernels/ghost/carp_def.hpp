124c19a73b5f9706d11fe9d612d775a0dbe5b235
Jonas Thies <jonas.thies@dlr.de>
2015-09-04 16:36:30 +0200

    added a second carp_sweep kernel which should operate on
    A-sI Q
    Q'   0
    
    (not implemented by any kernel lib, yet)

--- src/kernels/ghost/carp_def.hpp
+++ src/kernels/ghost/carp_def.hpp
@@ -41,16 +41,24 @@ return;
 #endif
 }
 
+void SUBR(carp_sweep_aug)(TYPE(const_sparseMat_ptr) A,
+        _MT_ const sigma_r[], _MT_ const sigma_i[],
+        TYPE(const_mvec_ptr) Q,
+        TYPE(const_mvec_ptr) Rhs,
+        TYPE(mvec_ptr) X_r, TYPE(mvec_ptr) X_i,
+        TYPE(sdMat_ptr) q_r, TYPE(sdMat_ptr) q_i,
+        void* const work,
+        _MT_ const * omega, int* iflag)
+{
+  *iflag=PHIST_NOT_IMPLEMENTED;
+  return;
+}
+
 void SUBR(carp_destroy)(TYPE(const_sparseMat_ptr) A,
 void* work, int *iflag)
 {
   *iflag=0;
   return;
 }
-
-
-
-
-
 
 } // extern "C"
