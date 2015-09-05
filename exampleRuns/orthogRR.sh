206b3ec6ecd5af2b85f0fceca36dcd55e1b64c46
Melven Roehrig-Zoellner <Melven.Roehrig-Zoellner@DLR.de>
2015-09-05 23:57:33 +0200

    More/better examples for checking the performance of standard/high-prec kernels

--- exampleRuns/orthogRR.sh
+++ exampleRuns/orthogRR.sh
@@ -3,7 +3,7 @@
 
 # accuracy tests
 for acc in 0 1 2 3 5 7; do
-  for nb in 1 2 4 8; do
+  for nb in 1 2 4; do
     for dev1 in 1. 1.e-3 1.e-6 1.e-9 1.e-12; do
       for dev2 in 1. 1.e-3 1.e-6 1.e-9 1.e-12; do
         echo "Run orthog_fusedRR_nb${nb}_acc${acc}_dev_${dev1}_${dev2}.log"
