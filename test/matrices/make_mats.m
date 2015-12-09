9f993b30e7aa9194c0ec6dc9fe6c6ff09bbf73e0
Jonas Thies <jonas.thies@dlr.de>
2015-12-09 14:55:46 +0100

    updated larger test matrices to have fewer random elements and include the spshift matrix

--- test/matrices/make_mats.m
+++ test/matrices/make_mats.m
@@ -1,4 +1,5 @@
-n=25;
+n=388;
+fill=0.05;
 key='no key';
 
 % all zero matrix
@@ -27,7 +28,7 @@ write_all_formats(name,description,A,A);
 name=['sprandn_nodiag',int2str(n)];
 description='random matrix with row sum 1 and some zeros on diagonal';
 
-A=sprandn(n,n,0.25);
+A=sprandn(n,n,fill);
 A(4,4)=randn; 
 C=sprandn(A)+sprandn(A)*i;
 
@@ -47,7 +48,7 @@ write_all_formats(name,description,A,C);
 name=['sprandn',int2str(n)];
 description='random matrix with row sum 1 and nonzero diagonal';
 
-A=sprandn(n,n,0.3);
+A=sprandn(n,n,fill);
 A=sprandn(A+speye(n));
 
 C=sprandn(A)+sprandn(A)*i;
@@ -60,5 +61,17 @@ rs=sum(C,2);
 D=spdiags(1./rs,0,n,n);
 C = D*C;
 
+
+write_all_formats(name,description,A,C);
+
+% spshift matrix, just one superdiagonal 1, else 0
+
+name=['spshift',int2str(n)];
+description='matrix just one constant super-diagonal';
+
+A=spdiags(ones(n,1),1,n,n);
+A(n,1)=1;
+
+C=sparse(A);
 
 write_all_formats(name,description,A,C);
