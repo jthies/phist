6ab6a615d6cb787cec62a4a311c7d94597482c1b
Jonas Thies <jonas.thies@dlr.de>
2016-02-02 11:41:55 +0100

    builtin: do not call C kernels that use SSE if PHIST_HAVE_SSE is not set

--- src/kernels/builtin/crsmat_module.f90
+++ src/kernels/builtin/crsmat_module.f90
@@ -1557,6 +1557,7 @@ end subroutine permute_local_matrix
 
     recvBuffSize = A%comm_buff%recvInd(A%comm_buff%nRecvProcs+1)-1
     handled = .false.
+#ifdef PHIST_HAVE_SSE
     !try to use NT stores if possible
     if( beta .eq. 0 .and. x_is_aligned .and. y_is_aligned ) then
       if( nvec .eq. 1 ) then
@@ -1601,7 +1602,7 @@ end subroutine permute_local_matrix
         handled = .true.
       end if
     end if
-
+#endif
     if( .not. handled ) then
       if( nvec .eq. 1 ) then
         if( strided ) then
