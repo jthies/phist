8ec6137c49f1d9c6b5df5f28c056d8c9254d4dad
Jonas Thies <jonas.thies@dlr.de>
2016-02-02 23:01:44 +0100

    preliminary definition of preconditioning interface

---
+++ src/precon/phist_precon.cpp
@@ -0,0 +1,21 @@
+#include "phist_config.h"
+/* needs to be included before system headers for some intel compilers+mpi */
+#ifdef PHIST_HAVE_MPI
+#include <mpi.h>
+#endif
+#include "phist_operator.h"
+#include "phist_precon.h"
+#include "phist_macros.h"
+#include "phist_ScalarTraits.hpp"
+#include <stdlib.h>
+
+#ifdef PHIST_HAVE_SP
+#include "phist_gen_s.h"
+#include "phist_precon_def.hpp"
+#include "phist_gen_c.h"
+#include "phist_precon_def.hpp"
+#endif
+#include "phist_gen_d.h"
+#include "phist_precon_def.hpp"
+#include "phist_gen_z.h"
+#include "phist_precon_def.hpp"
---
+++ src/precon/phist_precon.h
@@ -0,0 +1,37 @@
+#ifndef PHIST_PRECON_H
+#define PHIST_PRECON_H
+
+#include "phist_operator.h"
+
+#include "phist_config.h"
+
+#ifndef DOXYGEN
+
+/* needs to be included before system headers for some intel compilers+mpi */
+#ifdef PHIST_HAVE_MPI
+#include <mpi.h>
+#endif
+#include "phist_kernels.h"
+
+#endif //DOXYGEN
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#ifdef PHIST_HAVE_SP
+#include "phist_gen_s.h"
+#include "phist_precon_decl.h"
+#include "phist_gen_c.h"
+#include "phist_precon_decl.h"
+#endif
+#include "phist_gen_d.h"
+#include "phist_precon_decl.h"
+#include "phist_gen_z.h"
+#include "phist_precon_decl.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
