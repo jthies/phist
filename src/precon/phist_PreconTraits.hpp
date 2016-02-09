d100ddcb9d88794bc578ada0e2fbb22e24d1bcfa
Jonas Thies <jonas.thies@dlr.de>
2016-02-09 21:54:48 +0100

    implemented PreconTraits for "NO_PRECON" option and fixed the "missing implementation" case

--- src/precon/phist_PreconTraits.hpp
+++ src/precon/phist_PreconTraits.hpp
@@ -33,7 +33,7 @@ public:
 
   static void NotImplemented(int* iflag)
   {
-    PHIST_SOUT(PHIST_ERROR,"class PreconTraits is missing a specialization for data type %s and preconditioner type %s\n",
+    PHIST_SOUT(PHIST_ERROR,"class PreconTraits is missing a specialization for data type %c and preconditioner type %s\n",
                         st::type_char(), precon2str(PT));
     *iflag=PHIST_NOT_IMPLEMENTED;
   }
@@ -65,6 +65,61 @@ public:
           mvec_t const* X, ST beta,  mvec_t* Y, int* iflag)
   {
     NotImplemented(iflag);
+    return;
+  }
+};
+
+
+template<typename ST>
+class PreconTraits<ST,NO_PRECON>
+{
+
+  typedef ScalarTraits<ST> st;
+  typedef void mvec_t;
+
+public:
+
+
+  static void Usage()
+  {
+    PHIST_SOUT(PHIST_INFO,"NO_PRECON is equivalent to P=I, this 'preconditioner' does not\n"
+                          "need any options\n");
+  }
+
+  static void Create(void** P, 
+        const void* A, ST sigma, const void* B, 
+        const void* Vkern, const void* BVkern,
+        const char* options, int* iflag)
+  {
+    *P=NULL;
+    return;
+  }
+
+  static void Delete(void* P, int* iflag)
+  {
+    *iflag=0;
+    return;
+  }
+  
+  static void Apply(ST alpha, void const* P, mvec_t const* X, ST beta, mvec_t* Y, int* iflag)
+  {
+    *iflag=0;
+    PHIST_CHK_IERR(SUBR(mvec_add_mvec)(alpha,X,beta,Y,iflag),*iflag);
+    return;
+  } 
+
+  static void ApplyT(ST alpha, void const* P, mvec_t const* X, ST beta, mvec_t* Y, int* iflag)
+  {
+    Apply(alpha,P,X,beta,Y,iflag);
+  }
+  static void ApplyShifted(ST alpha, const void* P, ST const * sigma,
+          mvec_t const* X, ST beta,  mvec_t* Y, int* iflag)
+  {
+    int nvec;
+    PHIST_CHK_IERR(SUBR(mvec_num_vectors)(X,&nvec,iflag),*iflag);
+    _ST_ alphas[nvec];
+    for (int i=0; i<nvec;i++) alphas[i]=alpha*(st::one()-sigma[i]);
+    PHIST_CHK_IERR(SUBR(mvec_vadd_mvec)(alphas,X,beta,Y,iflag),*iflag);
     return;
   }
 };
