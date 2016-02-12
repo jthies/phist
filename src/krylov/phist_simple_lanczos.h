1d51d818d3ae2d9772fee835338d77186fe23f0f
Jonas Thies <jonas.thies@dlr.de>
2016-02-12 16:19:32 +0100

    added a simple Lanczos method + driver which will be the testbed for fault-tolerance capabilities in PHIST

---
+++ src/krylov/phist_simple_lanczos.h
@@ -0,0 +1,35 @@
+#ifndef PHIST_SIMPLE_LANCZOS_H
+#define PHIST_SIMPLE_LANCZOS_H
+
+#include "phist_config.h"
+
+#ifndef DOXYGEN
+
+/* needs to be included before system headers for some intel compilers+mpi */
+#ifdef PHIST_HAVE_MPI
+#include <mpi.h>
+#endif
+#include "phist_operator.h"
+#include "phist_enums.h"
+
+#endif //DOXYGEN
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#ifdef PHIST_HAVE_SP
+#include "phist_gen_s.h"
+#include "phist_simple_lanczos_decl.h"
+#include "phist_gen_c.h"
+#include "phist_simple_lanczos_decl.h"
+#endif
+#include "phist_gen_d.h"
+#include "phist_simple_lanczos_decl.h"
+#include "phist_gen_z.h"
+#include "phist_simple_lanczos_decl.h"
+#include "phist_gen_clean.h"
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/krylov/phist_simple_lanczos_decl.h
@@ -0,0 +1,20 @@
+//! a simple Lanczos process to compute the largest and smallest eigenvalue 
+//! of a linear operator. This code is mainly intended as a testbed 
+//! for fault tolerance (FT) capabilities in PHIST right now. Only block size 1 
+//! is implemented.
+//!
+//! input:
+//!
+//! A: linearOp whose largest eigenpair is sought
+//! *numIter, maximum number of iterations allowed
+//!
+//! output:
+//!
+//! *lambda_min, lambda_max: approximations to the largest and smallest eigenvalue
+//! *numIter, number of iterations performed
+//!
+void SUBR(simple_lanczos)(TYPE(const_linearOp_ptr) A_op,
+        _MT_* lambda_min, _MT_* lambda_max, int *numIter, int* iflag);
+
+
+
