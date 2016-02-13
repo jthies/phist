b41a67f87045ba3053065592569c811358eb64e5
Jonas Thies <jonas.thies@dlr.de>
2016-02-13 11:27:42 +0100

    Revert "there are now two variants of the augmented spMVM, the single-shift one being implemented by calling the 'v' version"
    This reverts commit 51f83aab752b6e532bd2cbb3647df0b0d68aca51.
    
    Conflicts:
    	src/kernels/common/kernels_common_impl_def.h
    	src/kernels/phist_kernels_decl.h

--- src/kernels/ghost/kernels_def.hpp
+++ src/kernels/ghost/kernels_def.hpp
@@ -1445,25 +1445,6 @@ PHIST_TASK_END(iflag);
   }
 }
 
-// augmented spMVM kernel available in GHOST
-
-// like sparseMat_times_mvec_add_mvec, followed by z=a*y+b*z. if z!=NULL.
-// if dot_xx!=NULL, it will contain mvec_dot_mvec(x,x) on output
-// and similarly for dot_xy and dot_yy (final y being used)
-//
-// Kernel libraries that do not offer this can include 
-// common/kernels_no_fused.cpp for a fallback variant
-void SUBR(sparseMat_times_mvec_vaug)(_ST_ alpha, TYPE(const_sparseMat_ptr) A,
-        _ST_ const shifts[], TYPE(const_mvec_ptr) x, _ST_ beta, TYPE(mvec_ptr) y, 
-        _ST_ a, _ST_ b, TYPE(mvec_ptr) z,
-        _ST_* dot_xx, _ST_* dotxy, _ST_* dotyy, 
-        int* iflag)
-{
-  PHIST_ENTER_FCN(__FUNCTION__);
-  *iflag=-99;
-  return;//TODO
-}
-
 //! dot product of vectors v_i and w_i, i=1..numvecs
 extern "C" void SUBR(mvec_dot_mvec)(TYPE(const_mvec_ptr) vV, TYPE(const_mvec_ptr) vW, _ST_* s, int* iflag)
 {
