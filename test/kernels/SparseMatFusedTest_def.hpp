ab929e21fe6ca7bdefafa2d25a1b58da26ea671d
Jonas Thies <jonas.thies@dlr.de>
2016-03-30 16:23:39 +0200

    adjusted a test tolerance

--- test/kernels/SparseMatFusedTest_def.hpp
+++ test/kernels/SparseMatFusedTest_def.hpp
@@ -140,7 +140,7 @@ TEST_F(CLASSNAME,fused_spmv_mvTmv)
     // check xTy == x^T * y
     SUBR(mvecT_times_mvec)(st::one(),vec1_,vec3_,st::zero(),mat3_,&iflag_);
     ASSERT_EQ(0,iflag_);
-    ASSERT_NEAR(mt::one(), SdMatsEqual(mat2_,mat3_), 10000*(VTest::releps(vec1_)+VTest::releps(vec2_)));
+    ASSERT_NEAR(mt::one(), SdMatsEqual(mat2_,mat3_), mt::sqrt(mt::eps()));
 
     // check yTy == y^T * y
     SUBR(mvecT_times_mvec)(st::one(),vec3_,vec3_,st::zero(),mat3_,&iflag_);
