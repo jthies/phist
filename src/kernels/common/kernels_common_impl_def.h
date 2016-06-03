jthies/phist

commit 2a884fd57eb3a0ae543eb269cd4d7bdc313b7527

adjustemnts in phist_config.h and builtin RNG
Re #124, do not warn when building with Epetra kernels and newer Trilinos, this seems to work just fine
(the remaining issues are with Tpetra only)

Re #193, it seems to me that epetra+isorropia/zoltan works just fine,
but the builtin random number generator ran into problems with the redistributed
map. It should now also work with maps that aren't "linear" by skipping over
a lot more numbers if such a map is encountered.

I will leave this issue open until both Epetra and GHOST work stably with Zoltan

src/kernels/common/kernels_common_impl_def.h
@@ -82,17 +82,25 @@ return 0;
 extern "C" void SUBR(mvec_random)(TYPE(mvec_ptr) V, int* iflag)
 {
   PHIST_ENTER_KERNEL_FCN(__FUNCTION__);
+  const_comm_ptr comm;
   phist_gidx gnrows,ilower,iupper,pre_skip,post_skip;
   phist_const_map_ptr map=NULL;
   phist_lidx lnrows,nvec;
+  
+  bool is_linear_map;
   
   PHIST_CHK_IERR(SUBR(mvec_num_vectors)(V,&nvec,iflag),*iflag);
 
   PHIST_CHK_IERR(SUBR(mvec_get_map)(V,&map,iflag),*iflag);
   PHIST_CHK_IERR(phist_map_get_local_length(map,&lnrows,iflag),*iflag);
   PHIST_CHK_IERR(phist_map_get_global_length(map,&gnrows,iflag),*iflag);
-  PHIST_CHK_IERR(phist_map_get_ilower(map,&ilower,iflag),*iflag);
-  PHIST_CHK_IERR(phist_map_get_iupper(map,&iupper,iflag),*iflag);
+  // these may return iflag=1 if the map is not a linear map. In that
+  // case we can still create a reproducible sequence by skipping a little 
+  // further in the random number stream (see next if statement)
+  PHIST_CHK_NEG_IERR(phist_map_get_ilower(map,&ilower,iflag),*iflag);
+  is_linear_map=(iflag==0);
+  PHIST_CHK_NEG_IERR(phist_map_get_iupper(map,&iupper,iflag),*iflag);
+  is_linear_map&=(iflag==0);
   
 #ifdef IS_COMPLEX
   const int nelem=2;
@@ -100,8 +108,22 @@ extern "C" void SUBR(mvec_random)(TYPE(mvec_ptr) V, int* iflag)
   const int nelem=1;
 #endif  
 
-  pre_skip = ilower*nvec*nelem;
-  post_skip= (gnrows-iupper)*nvec*nelem;
+  // deal with non-standard (linear) maps
+  if (is_linear_map==false)
+  {
+    int rank, size;
+    PHIST_CHK_IERR(phist_map_get_comm(map,&comm,iflag),*iflag);
+    PHIST_CHK_IERR(phist_comm_get_rank(comm,&rank,iflag),*iflag);
+    PHIST_CHK_IERR(phist_comm_get_size(comm,&size,iflag),*iflag);
+    pre_skip  = rank            * gnrows*nvec*nelem;
+    post_skip = (size-(rank+1)) * gnrows*nvec*nelem;
+  }
+  else
+  {
+    pre_skip = ilower*nvec*nelem;
+    post_skip= (gnrows-iupper+1)*nvec*nelem;
+  }
+  
     
   // we use the most robust way of implementing this, which should work for
   // any situation (row/col major, GPU/CPU etc.): generate row-major clone data

src/phist_config.h.in
@@ -86,11 +86,9 @@ kernel libs) ?
 #cmakedefine PHIST_HAVE_IFPACK2
 #cmakedefine PHIST_HAVE_MUELU
 #cmakedefine PHIST_HAVE_AMESOS2
-#if 0
-/* disabled due to issue #193 */
 #cmakedefine PHIST_HAVE_ISORROPIA
 #cmakedefine PHIST_HAVE_ZOLTAN
-#endif
+
 /* other TPLs */
 #cmakedefine PHIST_HAVE_MKL
 #cmakedefine PHIST_HAVE_PARMETIS
@@ -130,7 +128,9 @@ kernel libs) ?
 # ifdef PHIST_HAVE_KOKKOS
 # include "Trilinos_version.h"
 #  if ((TRILINOS_MAJOR_VERSION>11)||((TRILINOS_MAJOR_VERSION==11)&&(TRILINOS_MINOR_VERSION>1112)))
-#  warning "Trilinos version >11.12 is not fully supported by PHIST, yet (cf. issue #124)"
+#   if PHIST_KERNEL_LIB_TPETRA
+#    warning "Trilinos version >11.12 is not fully supported by PHIST, yet (cf. issue #124)"
+#   endif
 #  else
 #  define OLD_TRILINOS
 #  endif

commit 8a06aba577e0979efcfeb60952ce0328359a955a

hopefully fixed indexing issue in builtin RNG introduced in 2a884fd5

src/kernels/common/kernels_common_impl_def.h
@@ -121,7 +121,7 @@ extern "C" void SUBR(mvec_random)(TYPE(mvec_ptr) V, int* iflag)
   else
   {
     pre_skip = ilower*nvec*nelem;
-    post_skip= (gnrows-iupper+1)*nvec*nelem;
+    post_skip= (gnrows-iupper)*nvec*nelem;
   }
   
     