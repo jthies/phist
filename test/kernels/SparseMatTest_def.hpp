4811218633066113f0890c0ccd702e82f24be724
Jonas Thies <jonas.thies@dlr.de>
2016-07-27 17:20:56 +0200

    missing vector initialization in test

--- test/kernels/SparseMatTest_def.hpp
+++ test/kernels/SparseMatTest_def.hpp
@@ -507,6 +507,10 @@ protected:
     SUBR(sparseMat_create_fromRowFunc)(&A,comm_,_N_,_N_,1,&PHIST_TG_PREFIX(idfunc),NULL,&iflag_);
     ASSERT_EQ(0,iflag_);
     // check that AX=X
+    SUBR(mvec_random)(vec1_,&iflag_);
+    ASSERT_EQ(0,iflag_);
+    SUBR(mvec_put_value)(vec2_,(_ST_)99.9,&iflag_);
+    ASSERT_EQ(0,iflag_);
     SUBR(sparseMat_times_mvec)(st::one(),A,vec1_,st::zero(),vec2_,&iflag_);
     ASSERT_EQ(0,iflag_);
     ASSERT_NEAR(1.0,MvecsEqual(vec1_,vec2_),100*mt::eps());
