6b4483489274b8e5655aa2624a0a36f73e11cb37
Jonas Thies <jonas.thies@dlr.de>
2016-11-15 14:17:41 +0100

    bug fix in default context

--- src/kernels/common/default_context_def.hpp
+++ src/kernels/common/default_context_def.hpp
@@ -8,11 +8,13 @@ extern "C" void SUBR(sparseMat_get_context)(TYPE(const_sparseMat_ptr) A, phist_c
   PHIST_ENTER_FCN(__FUNCTION__);
   *iflag=0;
   phist::internal::default_context* ctx=phist::internal::contextCollection[A];
-  if (ctx!=NULL) return; // context has been created before -> return it again
-  ctx=new phist::internal::default_context;
-  PHIST_CHK_IERR(SUBR(sparseMat_get_row_map)(A,&ctx->row_map,iflag),*iflag);
-  PHIST_CHK_IERR(SUBR(sparseMat_get_domain_map)(A,&ctx->domain_map,iflag),*iflag);
-  PHIST_CHK_IERR(SUBR(sparseMat_get_range_map)(A,&ctx->range_map,iflag),*iflag);
-  phist::internal::contextCollection[A]=ctx;
+  if (ctx==NULL)
+  {
+    ctx=new phist::internal::default_context;
+    PHIST_CHK_IERR(SUBR(sparseMat_get_row_map)(A,&ctx->row_map,iflag),*iflag);
+    PHIST_CHK_IERR(SUBR(sparseMat_get_domain_map)(A,&ctx->domain_map,iflag),*iflag);
+    PHIST_CHK_IERR(SUBR(sparseMat_get_range_map)(A,&ctx->range_map,iflag),*iflag);
+    phist::internal::contextCollection[A]=ctx;
+  }
   *vctx=(phist_const_context_ptr)ctx;
 }
