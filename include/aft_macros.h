2fe13530f90b08dc908dd10bb70bd354f6d6d0ea
Faisal Shahzad <faisal.shahzad@fau.de>
2016-12-12 16:40:37 +0100

    aft_macros slightly updated

--- include/aft_macros.h
+++ include/aft_macros.h
@@ -1,22 +1,75 @@
 #define AFT_BEGIN(comm_working, myrank, argv) \
 { \
+	int AftFailed = false; \
+  do \
+  { \
+	  try{ \
+      MPI_Comm parent; \
+	    MPI_Errhandler errh; \
+	    MPI_Comm_create_errhandler(&errhandler_respawn, &errh); \
+	    MPI_Comm_get_parent( &parent ); \
+	    if( parent == MPI_COMM_NULL && AftFailed == false){ \
+		    MPI_Comm_dup (MPI_COMM_WORLD, &comm_working); \
+		    MPI_Comm_rank (comm_working, myrank); \
+		    if(*myrank==0) printf("FIRST RUNNN!!!!\n"); \
+		    MPI_Comm_set_errhandler(comm_working, errh); \
+	    } \
+      if ( parent != MPI_COMM_NULL || AftFailed == true)	{ \
+		    if( parent != MPI_COMM_NULL){ \
+			    if(*myrank==0) printf("%d_SPAWNED RANK \n", *myrank); \
+			    comm_working = MPI_COMM_NULL; \
+			    AftFailed = true;	\
+		    } \
+		    if(*myrank==0) printf("%d: calling app_needs_repair \n", *myrank); \
+		    app_needs_repair(&comm_working, argv); \
+		    MPI_Comm_set_errhandler(comm_working, errh); \
+		    MPI_Comm_rank(comm_working, myrank); \
+		    if(*myrank==0) printf("%d i am back from repair\n", *myrank); \
+      } 
+
+
+#define AFT_END() \
+    AftFailed = false; \
+	  }catch(int exception_val){ \
+		  sleep(1); \
+		  printf("exception value is %d \n", exception_val); \
+		  AftFailed = true;	\
+ 	  } \
+  } \
+  while(AftFailed == true);\
+} 
+
+/*
+#define AFT_RECOVER() \
+  AftFailed 
+
+#define AFT_RECOVERED() \
+  AftFailed = false;
+
+*/
+
+
+/*
+#define AFT_BEGIN(comm_working, myrank, argv) \
+{ \
+  int AftFailed = false; \
 	while(success != true) \
 	try{ \
      MPI_Comm parent; \
 	MPI_Errhandler errh; \
 	MPI_Comm_create_errhandler(&errhandler_respawn, &errh); \
 	MPI_Comm_get_parent( &parent ); \
-	if( parent == MPI_COMM_NULL && failed == false){ \
+	if( parent == MPI_COMM_NULL && AftFailed == false){ \
 		printf("FIRST RUNNN!!!!\n"); \
 		MPI_Comm_dup (MPI_COMM_WORLD, &comm_working); \
 		MPI_Comm_rank (comm_working, myrank); \
 		MPI_Comm_set_errhandler(comm_working, errh); \
 	} \
-    if ( parent != MPI_COMM_NULL || failed == true)	{ \
+    if ( parent != MPI_COMM_NULL || AftFailed == true)	{ \
 		if( parent != MPI_COMM_NULL){ \
 			printf("%d_SPAWNED RANK \n", *myrank); \
 			comm_working = MPI_COMM_NULL; \
-			failed = true;	\
+			AftFailed = true;	\
 		} \
 		printf("%d: calling app_needs_repair \n", *myrank); \
 		app_needs_repair(&comm_working, argv); \
@@ -25,13 +78,14 @@
 		printf("%d i am back from repair\n", *myrank); \
 	} 
 
-#define AFT_END(comm_working) \
+#define AFT_END() \
 	}catch(int exception_val){ \
 		sleep(1); \
-		printf("%d: exception value is %d \n", myrank, exception_val); \
-		failed = true;	\
+		printf("exception value is %d \n", exception_val); \
+		AftFailed = true;	\
  	} \
 } 
+*/
 
 /*
 	while(success != true)
@@ -42,7 +96,7 @@
 	MPI_Comm_get_parent( &parent ); 
 	if( parent == MPI_COMM_NULL && failed == false){		// performed only in the first run 
 		printf("FIRST RUNNN!!!!\n");
-		MPI_Comm_dup (MPI_COMM_WORLD, &comm_working); 
+		MPI_Comm_dup (MPI_COMM_WORLD, &comm_working);     // for multiplezones this may need to be changed
 		MPI_Comm_size (comm_working, &numprocs); 
 		MPI_Comm_rank (comm_working, &myrank); 
 		MPI_Comm_set_errhandler(comm_working, errh); 
