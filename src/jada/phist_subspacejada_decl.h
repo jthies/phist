dd8800c29ed0ab88b6371d3e3783b496df02526f
Jonas Thies <Jonas.Thies@DLR.de>
2016-12-13 13:45:19 +0100

    Re #201, allow starting subspacejada with either 0, 1 or minBas vectors in opts.v0. If 1<k<minBas are given, use 1 and issue a warning. If >minBas are given, use minBas and issue a warning.
    Also allow returningg up to opts.minBas vectors in Q (and a larger R)
    
    None of this is properly tested right now!

--- src/jada/phist_subspacejada_decl.h
+++ src/jada/phist_subspacejada_decl.h
@@ -7,32 +7,42 @@
 
 //! Subspace Jacobi-Davidson for extreme eigenvalues
 
-//! Tries to compute a partial schur form $(Q,R)$ of dimension opts.nEigs
-//! of the stencil $A*x-\lambda*B*x$ with a general linear operator $A$ and a
-//! hermitian positive definite (hpd.) linear operator $B$ using a
-//! block-Jacobi-Davidson QR method. <br>
-//! The generalized eigenvalues $\lambda_i$ are the diagonal entries of the
-//! partial schur form $A*Q = B*Q*R$ returned. <br>
-//!
-//! solver options are passed in via a jadaOpts_t struct, default parameters can
-//! be set using jadaOpts_setDefaults().
-//!
-//! Input arguments:
-//!
-//! A_op:             pointer to the operator A
-//! B_op:             pointer to the hpd. operator B (if B==NULL, B=I is assumed)
-//!
-//! for documentation of the other input parameters, see file phist_jadaOpts.h
-//! 
-//! Output arguments:
-//!
-//! nConv:     number of converged eigenpairs (e.g. dimension of (Q,R))
-//! Q:        orthogonal vectors of the partial schur form (Q,R)
-//! R:        small upper triangular matrix of the partial schur form (Q,R)
-//! nIter:    number of iterations performed
-//! resNorm:  norm of the residua of the schur form $A*q_i-Q*r_i, i=1,nEig$
-//! iflag:     return code of the solver (0 on success, negative on error, positive on warning)
-//!
+//! Tries to compute a partial Schur form $(Q,R)$ of dimension opts.nEigs                       
+//! of the stencil $A*x-\lambda*B*x$ with a general linear operator $A$ and a                   
+//! hermitian positive definite (hpd.) linear operator $B$ using a                              
+//! block-Jacobi-Davidson QR method. <br>                                                       
+//! The generalized eigenvalues $\lambda_i$ are the diagonal entries of the                     
+//! partial Schur form $A*Q = B*Q*R$ returned. <br>                                             
+//!                                                                                             
+//! solver options are passed in via a jadaOpts_t struct, default parameters can                
+//! be set using jadaOpts_setDefaults().                                                        
+//!                                                                                             
+//! Input arguments:                                                                            
+//!                                                                                             
+//! A_op:             pointer to the operator A                                                 
+//! B_op:             pointer to the hpd. operator B (if B==NULL, B=I is assumed)               
+//!                                                                                             
+//! for documentation of the other input parameters, see file phist_jadaOpts.h                  
+//!                                                                                             
+//! Output arguments:                                                                           
+//!                                                                                             
+//! nConv:    number of converged eigenpairs (e.g. dimension of (Q,R))                          
+//! Q:        orthogonal vectors of the partial Schur form (Q,R). Should be allocated           
+//!           with at least m=opts.numEigs+opts.blockSize-1 and at most opts.minBas vectors.    
+//!           On output, the first nConv columns are the converged invariant subspace, but      
+//!           more useful vectors (approximating yet unconverged eigenvectors) may be           
+//!           contained if more vectors were allocated. A space Q of dimension opts.minBas      
+//!           can be used as a starting basis for a subsequent call to subspacejada which       
+//!           then does not require Arnoldi steps for starting up (set opts.v0=Q for this).     
+//! R:        small upper triangular matrix of the partial Schur form (Q,R), should have the    
+//!           same number of rows and columns as the number of columns in Q on input.           
+//!           The upper left nConv x nConv block of R contains the Schur-form for the converged 
+//!           eigenpairs, unconverged eigenvalues may be contained in the complete R if more    
+//!           space is available.                                                               
+//! nIter:    number of iterations performed                                                    
+//! resNorm:  norm of the residuals of the Schur form $A*q_i-Q*r_i, i=1,nEig$                   
+//! iflag:     return code of the solver (0 on success, negative on error, positive on warning) 
+//!                                                                                             
 void SUBR(subspacejada)( TYPE(const_linearOp_ptr) A_op,  TYPE(const_linearOp_ptr) B_op,
                          phist_jadaOpts opts,
                          TYPE(mvec_ptr) Q,         TYPE(sdMat_ptr) R,
