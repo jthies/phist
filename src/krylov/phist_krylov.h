57cd64e43fa4bd2b3f786005c8d68158ec725261
Jonas Thies <jonas.thies@dlr.de>
2017-11-08 09:33:16 +0100

    missing includes for TPLs Belos and Anasazi

--- src/krylov/phist_krylov.h
+++ src/krylov/phist_krylov.h
@@ -1,6 +1,8 @@
 #ifndef PHIST_KRYLOV_H
 #define PHIST_KRYLOV_H
 
+#include "phist_config.h"
+
 /* blocked Krylov solvers used as correction solvers in jada */
 #include "phist_blockedbicgstab.h"
 #include "phist_blockedgmres.h"
@@ -8,6 +10,14 @@
 #include "phist_blockedqmr.h"
 
 #include "phist_carp_cg.h"
+
+#ifdef PHIST_HAVE_ANASAZI
+#include "phist_anasazi.h"
+#endif
+
+#ifdef PHIST_HAVE_BELOS
+#include "phist_belos.h"
+#endif
 
 /* simple iteration schemes to start the Jacobi-Davidson method */
 #include "phist_simple_arnoldi.h"
