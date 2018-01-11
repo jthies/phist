f3d896e71e76b15c624e38c5f2fd52f2b106c801
Jonas Thies <jonas.thies@dlr.de>
2018-01-11 21:33:16 +0100

    builtin/colpack: use bipartite graph coloring to include the off-processor (ghost nodes) in the coloring, that way we should get consistent updates in kaczmarz/carp also with MPI+OpenMP. Fixes #116.

--- src/kernels/builtin/colpack.cpp
+++ src/kernels/builtin/colpack.cpp
@@ -16,7 +16,8 @@
 #include "ColPackHeaders.h"
 #endif
 // interface to ColPack's graph coloring algorithms. 
-// Input: local CRS arrays as used in the builtin kernels (only local edges are considered)
+// Input: CRS arrays as used in the builtin kernels (off-processor elements are renumbered and bipartite row-coloring 
+//        is used so that all local rows of a color have a distance at least of 'dist' taking ghost nodes into account)
 //        dist: only distance 2 supported for now
 //        *ncolors: will contain the number of colurs
 //        colors: dimension nrows, preallocated by user, contains 0-based color values.
@@ -41,7 +42,7 @@ extern "C" void colpack_v1_0_8(int nrows, int64_t* row_ptr, int64_t* nonlocal_pt
   //              ...
   // ... all the way, so we just need a single object to represent the graph and the ordering 
   // and everything
-  ColPack::GraphColoring *GC=new ColPack::GraphColoring();
+  ColPack::BipartiteGraphPartialColoring *GC=new ColPack::BipartiteGraphPartialColoring();
 
   // the input format this package accepts is called "ADOL-C compressed row". I have
   // looked in an obscure paper, ADOL-C is something about automatic differentiation
@@ -51,9 +52,13 @@ extern "C" void colpack_v1_0_8(int nrows, int64_t* row_ptr, int64_t* nonlocal_pt
                           // entry 0 is the length of row i, so we need to
                           // re-allocate and copy instead of passing in CRS
 
- // first we need to count the local nonzeros (TODO - or are they readily 
- // available in crsmat_module?)
+ // first we need to count the local nonzeros and determine the number of columns
+ // in the local part of the matrix. If there are nrows local rows and the largest
+ // off-processor column entry is c_max, ncols=nrows+c_max. Since this coloring is
+ // only local to the MPI process, we do not need a consistent numbering across
+ // MPI processes.
  int64_t nzloc=0;
+ int ncols=nrows;
  int nthreads=1;
 #pragma omp parallel
 {
@@ -61,15 +66,15 @@ extern "C" void colpack_v1_0_8(int nrows, int64_t* row_ptr, int64_t* nonlocal_pt
  nthreads=omp_get_num_threads();
 }
 
- std::cout << "nthreads="<<nthreads<<std::endl; 
-
  int64_t thread_nrows[nthreads];
  int64_t thread_nzloc[nthreads];
+ int64_t thread_c_max[nthreads];
  
  for (int i=0; i<nthreads;i++)
  {
    thread_nrows[i]=0;
    thread_nzloc[i]=0;
+   thread_c_max[i]=0;
  }
  
 #pragma omp parallel
@@ -79,14 +84,18 @@ extern "C" void colpack_v1_0_8(int nrows, int64_t* row_ptr, int64_t* nonlocal_pt
   for (int i=0;i<nrows;i++)
   {
     thread_nrows[thread_id]++;
-    thread_nzloc[thread_id]+=nonlocal_ptr[i]-row_ptr[i];
+    thread_nzloc[thread_id]+=row_ptr[i+1]-row_ptr[i];
+    for (int j=nonlocal_ptr[i]; j<row_ptr[i+1]; j++)
+    {
+      thread_c_max[thread_id] = std::max(thread_c_max[thread_id], (int64_t)col_idx[j-idx_base]);
+    }
   }
 }
 
 for (int i=0;i<nthreads;i++)
 {
-  std::cout << i << ": "<<thread_nrows[i]<<" "<<thread_nzloc[i]<<std::endl;
   nzloc+=thread_nzloc[i];
+  ncols+=thread_c_max[i];
 }
 
 
@@ -105,16 +114,21 @@ for (int i=0;i<nthreads;i++)
   {
     adolc[i]=&(adolc_data[pos]);
     // first entry in row is the local row length
-    adolc_data[pos++]=nonlocal_ptr[i]-row_ptr[i];
+    adolc_data[pos++]=row_ptr[i+1]-row_ptr[i];
     for (int j=row_ptr[i];j<nonlocal_ptr[i];j++)
     {
       // subtract idx_base to account for 1-based input
       adolc_data[pos++]=col_idx[j-idx_base]-idx_base;
     }
+    for (int j=nonlocal_ptr[i];j<row_ptr[i+1];j++)
+    {
+      // add nrows to assign unique node IDs to the off-processor column indices.
+      adolc_data[pos++]=col_idx[j-idx_base]+nrows;
+    }
   }
 }
 
-  int i_HighestDegree = GC->BuildGraphFromRowCompressedFormat(adolc, nrows);
+  int i_HighestDegree = GC->BuildBPGraphFromRowCompressedFormat(adolc, nrows,ncols);
   
   delete [] adolc_data;
   delete [] adolc;
@@ -126,19 +140,21 @@ for (int i=0;i<nthreads;i++)
   if (dist==2)
   {
     // this function returns 1 on success
-    PHIST_CHK_IERR(*iflag=GC->DistanceTwoColoring()-1,*iflag);
+    PHIST_CHK_IERR(*iflag=GC->PartialDistanceTwoRowColoring()-1,*iflag);
 #if PHIST_OUTLEV>=PHIST_VERBOSE
 int verbose=2;
 #else
 int verbose=1;
 #endif
-#ifdef PHIST_TESTING
+//#ifdef PHIST_TESTING
     PHIST_SOUT(PHIST_VERBOSE,"thoroughly test local dist-2 coloring\n");
-    PHIST_CHK_IERR(*iflag=GC->CheckDistanceTwoColoring(verbose),*iflag);
+    PHIST_CHK_IERR(*iflag=GC->CheckPartialDistanceTwoColoring(),*iflag);
+/*
 #else
     PHIST_SOUT(PHIST_VERBOSE,"quickly test local dist-2 coloring\n");
     PHIST_CHK_IERR(*iflag=GC->CheckQuickDistanceTwoColoring(verbose),*iflag);
 #endif
+*/
   }
   else
   {
@@ -146,7 +162,8 @@ int verbose=1;
   }
 
   *ncolors = GC->GetVertexColorCount();
-  std::vector<int>& colorVec=*(GC->GetVertexColorsPtr());
+  std::vector<int> colorVec;
+  GC->GetVertexPartialColors(colorVec);
 
   PHIST_CHK_IERR(*iflag=nrows-colorVec.size(),*iflag);
   
