34adcfb66047e36005041dff54ff004bb760ef8f
Jonas Thies <jonas.thies@dlr.de>
2018-01-18 14:12:50 +0100

    Revert "Revert "minor fix in iter_op (use phist_gen_all rather than separate includes)""
    This reverts commit 037ea74567df8794ad6be7a6f62aea8983697939.

--- src/krylov/phist_iter_op.h
+++ src/krylov/phist_iter_op.h
@@ -13,18 +13,7 @@
 #include "phist_operator.h"
 #include "phist_enums.h"
 
-#ifdef PHIST_HAVE_SP
-#include "phist_gen_s.h"
-#include "phist_iter_op_decl.h"
-#include "phist_gen_c.h"
-#include "phist_iter_op_decl.h"
-#endif
-
-#include "phist_gen_d.h"
-#include "phist_iter_op_decl.h"
-#include "phist_gen_z.h"
-#include "phist_iter_op_decl.h"
-
-#include "phist_gen_clean.h"
+#define PHIST_CLASSFILE_DEF "phist_iter_op_decl.h"
+#include "phist_gen_all.h"
 
 #endif
