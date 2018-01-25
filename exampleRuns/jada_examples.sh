c6c642c9647d7bf614494be6335a5c0458c862f1
Jonas Thies <jonas.thies@dlr.de>
2018-01-25 15:11:17 +0100

    added a new exampleRun and updated jada_examples.sh script

--- exampleRuns/jada_examples.sh
+++ exampleRuns/jada_examples.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-# compute the largest few eigenvalues of a 3D laplacian
-mpirun -np 2 ./phist_Dsubspacejada BENCH3D-32-A0 I opts-herm-LM.txt
+# compute the smallest few eigenvalues of a 3D laplacian
+mpirun -np 2 ./phist_Dsubspacejada BENCH3D-32-A0 I opts-herm-SM.txt
+
+# same problem with IFPACK preconditioner (will only work with Epetra or Tpetra).
+# ifpack_options.xml can be found in exampleRuns/precon/
+mpirun -np 2 ./phist_Dsubspacejada BENCH3D-32-A0 I opts-herm-SM-ifpack.txt
+
+# solve a generalized EVP
+mpirun -np 2 ./phist_Dsubspacejada elast_A.mm elast_B.mm opts-elast.txt
