b7c696fa299e3b7f7a40d451772a77f6cdd1cb79
Resa Hennig <Rebekka-Sarah.Hennig@dlr.de>
2018-01-25 13:36:50 +0100

    Re #213: changed jadaOp_create, jdPrec_create and jadaOp_set_leftPrecond so that they work with jadaOp_create_variable insteat of the many diffrent jadaOp_apply functions

--- src/jada/phist_jadaOp_def.hpp
+++ src/jada/phist_jadaOp_def.hpp
@@ -74,9 +74,14 @@ class TYPE(jadaOp_data)
   TYPE(const_linearOp_ptr)    AB_op;   // operator of the general matrix A
   TYPE(const_linearOp_ptr)    B_op;   // operator of the hpd. matrix B, assumed I when NULL
   TYPE(const_linearOp_ptr)    leftPrecon_op;   // left preconditioning operator
+  TYPE(const_linearOp_ptr)    Proj_op;   // projection operator 
+  TYPE(const_linearOp_ptr)    Prec_op;   // projection operator 
+  TYPE(const_linearOp_ptr)    Skew_op;   // projection operator 
+  TYPE(linearOp_ptr)    k_op; //jadaOp with k_wrapper
   TYPE(const_mvec_ptr)  V;      // B-orthonormal basis
   TYPE(const_mvec_ptr)  BV;     // B*V
   int num_shifts;               // number of shifts given to constructor
+  int projType;
   const _ST_*           sigma;  // array of NEGATIVE shifts, assumed to have correct size; TODO: what about 'complex' shifts for real JDQR?
   TYPE(mvec_ptr)        X_proj; // temporary storage for (I-VV'B)X, only used for B!= NULL
   MvecOwner<_ST_> _X_proj, _V_prec; // these objects make sure that some temporary storage is freed when the object is deleted
@@ -94,6 +99,30 @@ class TYPE(projOp_data)
   MvecOwner<_ST_> _V, _W; // these objects make sure that some temporary storage is freed when the object is deleted 
 };
 
+//! apply for jadaOp
+void SUBR(jadaOp_apply)(_ST_ alpha, const void* op, TYPE(const_mvec_ptr) X,
+    _ST_ beta, TYPE(mvec_ptr) Y, int* iflag)
+{
+#include "phist_std_typedefs.hpp"
+  PHIST_ENTER_FCN(__FUNCTION__);
+  PHIST_CAST_PTR_FROM_VOID(const TYPE(jadaOp_data), jadaOp, op, *iflag);
+  
+  TYPE(const_linearOp_ptr) k_Op = jadaOp->k_op;
+
+  if(k_Op == NULL)
+  {
+    PHIST_CHK_IERR(*iflag=PHIST_INVALID_INPUT,*iflag);
+    return;
+  }
+  if( alpha == st::zero() )
+  {
+    PHIST_CHK_IERR( SUBR( mvec_scale ) (Y, beta, iflag), *iflag);
+  }
+  else
+  {
+    k_Op->apply(alpha,k_Op->A,X,beta,Y,iflag);
+  }
+}
 
 //! simply apply original operator shifted, no pre- or postprojection
 void SUBR(jadaOp_apply_project_none)(_ST_ alpha, const void* op, TYPE(const_mvec_ptr) X,
@@ -354,90 +383,6 @@ void SUBR(jadaOp_apply_project_pre_post)(_ST_ alpha, const void* op, TYPE(const_
   }
 }
 
-// allocate and initialize the jadaOp struct
-extern "C" void SUBR(jadaOp_create)(TYPE(const_linearOp_ptr)    AB_op,
-                         TYPE(const_linearOp_ptr)     B_op,
-                         TYPE(const_mvec_ptr)  V,       TYPE(const_mvec_ptr)  BV,
-                         const _ST_            sigma[], int                   nvec,
-                         TYPE(linearOp_ptr)          jdOp,    int*                  iflag)
-{
-#include "phist_std_typedefs.hpp"
-  PHIST_ENTER_FCN(__FUNCTION__);
-  *iflag = 0;
-
-  int i;
-  // allocate jadaOp struct
-  TYPE(jadaOp_data) *myOp = new TYPE(jadaOp_data);
-  
-  if (AB_op->apply_shifted==NULL)
-  {
-    PHIST_SOUT(PHIST_ERROR, "operator passed to %s does not support apply_shifted\n"
-                            "(file %s, line %d)\n",__FUNCTION__,__FILE__,__LINE__);
-  *iflag=-1;
-  return;
-  }
-
-  // setup jadaOp members
-  myOp->AB_op   = AB_op;
-  myOp->B_op   = B_op;
-  myOp->leftPrecon_op = NULL;
-  myOp->V      = V;
-  myOp->BV     = (BV != NULL ? BV     : V);
-  myOp->num_shifts = nvec;
-  myOp->sigma  = sigma;
-  // allocate necessary temporary arrays
-  int nvecp=0;
-  phist_const_comm_ptr comm;
-  PHIST_CHK_IERR(phist_map_get_comm(AB_op->domain_map, &comm, iflag), *iflag);
-  if (V!=NULL)
-  {
-    PHIST_CHK_IERR(SUBR(mvec_num_vectors)(V, &nvecp, iflag), *iflag);
-  }
-  // this vector is created for a fixed number of vectors nvec to which the operator
-  // is applied. if another blocksize occurs, temporary storage is used in the apply
-  // function, which may mean some overhead!
-  myOp->X_proj = NULL;
-
-  // setup op_ptr function pointers. For standard EVP, just post-project. For generalized EVP,
-  // pre- and postproject. X_proj is only needed if B!=NULL, otherwise we don't do any pre-
-  // projection.
-  jdOp->A     = (const void*)myOp;
-
-  if (nvecp==0)
-  {
-    jdOp->apply = SUBR(jadaOp_apply_project_none);
-  }
-  else
-  {
-    if (B_op!=NULL)
-    {
-      PHIST_CHK_IERR(SUBR(mvec_create)(&myOp->X_proj,AB_op->domain_map,nvec,iflag),*iflag);
-      // make sure X_proj gets deleted automatically when myOp is deleted
-      myOp->_X_proj.set(myOp->X_proj);
-      // if the user passes in a B operator and projection vectors V, he also has to provide BV
-      PHIST_CHK_IERR(*iflag= (V!=NULL && BV==V)? PHIST_INVALID_INPUT: 0, *iflag);
-      jdOp->apply = SUBR(jadaOp_apply_project_pre_post);
-    }
-    else
-    {
-      jdOp->apply = SUBR(jadaOp_apply_project_post);
-    }
-  }
-  jdOp->applyT= NULL; // not needed, I think, but it's trivial to implement
-  jdOp->apply_shifted=NULL;// does not make sense, it would mean calling apply_shifted in a 
-                           // nested way.
-  jdOp->destroy = SUBR(jadaOp_delete);
-
-  jdOp->range_map=AB_op->range_map;
-  jdOp->domain_map=AB_op->domain_map;
-  
-  // print some useful data
-  PHIST_SOUT(PHIST_DEBUG, "Created jadaOp with %d projection vectors and %d shifts\n%s\n",   nvecp,nvec,
-              B_op==NULL? "                    B=I and postprojection\n":
-                          "                    B-inner product and pre-/postprojection\n");
-}
-
-
 // deallocate jadaOp struct
 extern "C" void SUBR(jadaOp_delete)(TYPE(linearOp_ptr) jdOp, int *iflag)
 {
@@ -454,115 +399,6 @@ extern "C" void SUBR(jadaOp_delete)(TYPE(linearOp_ptr) jdOp, int *iflag)
 
   // delete jadaOp
   delete jadaOp;
-}
-
-// create a preconditioner for the inner solve in Jacobi-Davidson.
-//
-// Given a linear operator that is a preconditioner for A-sigma_j*B, this function will
-// wrap it up to use apply_shifted and pre-/post-apply adequate (skew-) projections
-// when apply() is called. We need this because our implementations     
-// of blockedGMRES and MINRES are not aware of the shifts so they can only call apply in the precon-    
-// ditioning operator. Obviously not all preconditioners are able to handle varying shifts without      
-// recomputing, this is not taken into account by this function:in that case the input P_op must be     
-// updated beforehand, or the existing preconditioner for e.g. A or A-tau*B is applied.
-//
-// If V is given, the preconditioner application will include a skew-projection
-//
-// Y <- (I - P_op\V (BV'P_op\V)^{-1} (BV)') P_op\X
-//
-// If BV==NULL, BV=V is assumed.
-extern "C" void SUBR(jadaPrec_create)(TYPE(const_linearOp_ptr) P_op,
-        TYPE(const_mvec_ptr) V, TYPE(const_mvec_ptr) BV,
-        const _ST_ sigma[], int nvec,
-        TYPE(linearOp_ptr) jdPrec, int projType, int* iflag)
-
-{
-#include "phist_std_typedefs.hpp"
-  if (V==NULL||projType==0)
-  {
-    // simply apply the preconditioner "as is" or apply given projection vectors after applying P
-    PHIST_CHK_IERR(SUBR(jadaOp_create)(P_op,NULL,V,BV,sigma, nvec, jdPrec,iflag),*iflag);
-    // use the version without the projections:
-    jdPrec->apply=SUBR(jadaOp_apply_project_none);
-  }
-  else
-  {
-    // we always require BV!=NULL, but it may point to the same memory or object in memory
-    if (BV==NULL)
-    {
-      PHIST_SOUT(PHIST_ERROR,"jadaPrec_create requires BV if V is given, but it may be the same object or a view \n"
-                             " of the same memory as V\n");
-      *iflag=PHIST_INVALID_INPUT;
-      return;
-    }
-    // construct a skew-projected operator, first we need to construct P\V*(BV'P\V)^{-1}
-    int nproj;
-    PHIST_CHK_IERR(SUBR(mvec_num_vectors)(V,&nproj,iflag),*iflag);
-    if (nproj==0) PHIST_CHK_IERR(*iflag=PHIST_INVALID_INPUT,*iflag);
-    
-    TYPE(mvec_ptr) PV=NULL;
-    PHIST_CHK_IERR(SUBR(mvec_create)(&PV,P_op->domain_map,nproj,iflag),*iflag);
-    PHIST_CHK_IERR(P_op->apply(st::one(),P_op->A,V,st::zero(),PV,iflag),*iflag);
-    TYPE(sdMat_ptr) BVtPV=NULL, M=NULL;
-    phist_const_comm_ptr comm=NULL;
-
-    PHIST_CHK_IERR(phist_map_get_comm(P_op->domain_map,&comm,iflag),*iflag);
-    PHIST_CHK_IERR(SUBR(sdMat_create)(&BVtPV, nproj,nproj,comm,iflag),*iflag);
-    PHIST_CHK_IERR(SUBR(sdMat_create)(&M, nproj,nproj,comm,iflag),*iflag);
-    SdMatOwner<_ST_> _BVtPV(BVtPV), _M(M);
-    
-    PHIST_CHK_IERR(SUBR(mvecT_times_mvec)(st::one(),BV,PV,st::zero(),BVtPV,iflag),*iflag);
-
-    // compute the *transposed* pseudo-inverse of (BV)'P\V in place
-    PHIST_CHK_IERR(SUBR(sdMat_from_device)(BVtPV,iflag),*iflag);    
-    int rank;
-    _MT_ rankTol=mt::eps();
-    PHIST_CHK_IERR(SUBR(sdMat_pseudo_inverse)(BVtPV,&rank,rankTol,iflag),*iflag);
-    PHIST_CHK_IERR(SUBR(sdMat_to_device)(BVtPV,iflag),*iflag);
-    // explicitly transpose the result
-    PHIST_CHK_IERR(SUBR(sdMatT_add_sdMat)(st::one(),BVtPV,st::zero(),M,iflag),*iflag);
-
-    // in-place PV*((BV)'P\V)^+
-    PHIST_CHK_IERR(SUBR(mvec_times_sdMat_inplace)(PV,M,iflag),*iflag);
-
-    // use the version with only post-projection by giving B_op==NULL:
-    PHIST_CHK_IERR(SUBR(jadaOp_create)(P_op,NULL,BV,PV,sigma, nvec, jdPrec,iflag),*iflag);
-    jdPrec->apply=SUBR(jadaOp_apply_project_post);
-    // add the PV block vector to be deleted automatically when the operator is deleted.
-    TYPE(jadaOp_data)* jdDat=(TYPE(jadaOp_data)*)jdPrec->A;
-    jdDat->_V_prec.set(PV);
-  }
-}
-
-//! add a left preconditioner created by jadaPrec_create to a jadaOp.
-
-//! The effect of the apply function will afterwards by Y <- alpha*(jadaPrec*jadaOp*X) + beta*Y,
-//! the projections used are determined by the AB_op and jadaPrec operators. If jadaPrec==NULL, 
-//! the operator is reset to it's original effect.
-extern "C" void SUBR(jadaOp_set_leftPrecond)(TYPE(linearOp_ptr) jdOp, TYPE(const_linearOp_ptr) jadaPrec, int* iflag)
-{
-  PHIST_CAST_PTR_FROM_VOID(TYPE(jadaOp_data), jdDat, jdOp->A, *iflag);
-  jdDat->leftPrecon_op = jadaPrec;
-  if (jadaPrec==NULL)
-  {
-    // recover original behavior: post-project for B=I, pre/post for B!=I
-    if (jdDat->B_op==NULL)
-    {
-      jdOp->apply=SUBR(jadaOp_apply_project_post);
-    }
-    else
-    {
-      jdOp->apply=SUBR(jadaOp_apply_project_pre_post);
-    }
-  }
-  else
-  { 
-    PHIST_CAST_PTR_FROM_VOID(const TYPE(jadaOp_data), jdPrec, jadaPrec->A, *iflag);
-    // We pre-project with (I-(BV)V') and
-    // postproject with (I-Z(Z'BQ)^{-1}BQ^T), Z=P\Q. The post-projection
-    // is handled by the preconditioner wrapper (see jadaPrec_create)
-    jdOp->apply=SUBR(jadaOp_apply_project_pre_post);
-  }
 }
 
 // apply function for projection Operator
@@ -909,5 +745,177 @@ extern "C" void SUBR(JadaOp_create_variable)(TYPE(const_linearOp_ptr)    AB_op,
   
   PHIST_CHK_IERR(SUBR(linearOp_wrap_linearOp_product_k)(jdOp, k, k_ops,which_apply, sigma, nvec, iflag),*iflag);
   
+}
+
+// allocate and initialize the jadaOp struct
+extern "C" void SUBR(jadaOp_create)(TYPE(const_linearOp_ptr)    AB_op,
+                         TYPE(const_linearOp_ptr)     B_op,
+                         TYPE(const_mvec_ptr)  V,       TYPE(const_mvec_ptr)  BV,
+                         const _ST_            sigma[], int                   nvec,
+                         TYPE(linearOp_ptr)          jdOp,    int*                  iflag)
+{
+#include "phist_std_typedefs.hpp"
+  PHIST_ENTER_FCN(__FUNCTION__);
+  *iflag = 0;
+
+  int i;
+  // allocate jadaOp struct
+  TYPE(jadaOp_data) *myOp = new TYPE(jadaOp_data);
+  
+  if (AB_op->apply_shifted==NULL)
+  {
+    PHIST_SOUT(PHIST_ERROR, "operator passed to %s does not support apply_shifted\n"
+                            "(file %s, line %d)\n",__FUNCTION__,__FILE__,__LINE__);
+  *iflag=-1;
+  return;
+  }
+
+  // setup jadaOp members
+  myOp->AB_op   = AB_op;
+  myOp->B_op   = B_op;
+  myOp->leftPrecon_op = NULL;
+  myOp->V      = V;
+  myOp->BV     = (BV != NULL ? BV     : V);
+  myOp->num_shifts = nvec;
+  myOp->sigma  = sigma;
+  // allocate necessary temporary arrays
+  int nvecp=0;
+  phist_const_comm_ptr comm;
+  PHIST_CHK_IERR(phist_map_get_comm(AB_op->domain_map, &comm, iflag), *iflag);
+  if (V!=NULL)
+  {
+    PHIST_CHK_IERR(SUBR(mvec_num_vectors)(V, &nvecp, iflag), *iflag);
+  }
+  // this vector is created for a fixed number of vectors nvec to which the operator
+  // is applied. if another blocksize occurs, temporary storage is used in the apply
+  // function, which may mean some overhead!
+  myOp->X_proj = NULL;
+  
+  TYPE(linearOp_ptr) proj_Op = new TYPE(linearOp);
+  PHIST_CHK_IERR(SUBR(projection_Op_create)(myOp->V,myOp->BV,proj_Op,iflag),*iflag);
+  myOp->Proj_op = proj_Op;
+  
+  const char* method;
+  if (B_op!=NULL)
+  {
+    method= "PRE_POST";
+  }
+  else{
+    method= "POST";
+  }
+
+  TYPE(linearOp_ptr) k_Op = new TYPE(linearOp);
+  PHIST_CHK_IERR(SUBR(JadaOp_create_variable)(myOp->AB_op,myOp->Proj_op,NULL,NULL,myOp->V,myOp->BV,myOp->sigma,myOp->num_shifts,k_Op,method,0,iflag),*iflag);
+
+  myOp->k_op = k_Op;
+
+  // setup op_ptr function pointers. For standard EVP, just post-project. For generalized EVP,
+  // pre- and postproject. X_proj is only needed if B!=NULL, otherwise we don't do any pre-
+  // projection.
+  jdOp->A     = (const void*)myOp;
+
+  jdOp->apply = SUBR(jadaOp_apply);
+  jdOp->applyT= NULL; // not needed, I think, but it's trivial to implement
+  jdOp->apply_shifted=NULL;// does not make sense, it would mean calling apply_shifted in a 
+                           // nested way.
+  jdOp->destroy = SUBR(jadaOp_delete);
+
+  jdOp->range_map=k_Op->range_map;
+  jdOp->domain_map=k_Op->domain_map;
+  
+  // print some useful data
+  PHIST_SOUT(PHIST_DEBUG, "Created jadaOp with %d projection vectors and %d shifts\n%s\n",   nvecp,nvec,
+              B_op==NULL? "                    B=I and postprojection\n":
+                          "                    B-inner product and pre-/postprojection\n");
+                          
+}
+
+// create a preconditioner for the inner solve in Jacobi-Davidson.
+//
+// Given a linear operator that is a preconditioner for A-sigma_j*B, this function will
+// wrap it up to use apply_shifted and pre-/post-apply adequate (skew-) projections
+// when apply() is called. We need this because our implementations     
+// of blockedGMRES and MINRES are not aware of the shifts so they can only call apply in the precon-    
+// ditioning operator. Obviously not all preconditioners are able to handle varying shifts without      
+// recomputing, this is not taken into account by this function:in that case the input P_op must be     
+// updated beforehand, or the existing preconditioner for e.g. A or A-tau*B is applied.
+//
+// If V is given, the preconditioner application will include a skew-projection
+//
+// Y <- (I - P_op\V (BV'P_op\V)^{-1} (BV)') P_op\X
+//
+// If BV==NULL, BV=V is assumed.
+extern "C" void SUBR(jadaPrec_create)(TYPE(const_linearOp_ptr) P_op,
+        TYPE(const_mvec_ptr) V, TYPE(const_mvec_ptr) BV,
+        const _ST_ sigma[], int nvec,
+        TYPE(linearOp_ptr) jdPrec, int projType, int* iflag)
+{
+#include "phist_std_typedefs.hpp"
+  if (V==NULL||projType==0)
+  {
+    // simply apply the preconditioner "as is" or apply given projection vectors after applying P
+    PHIST_CHK_IERR(SUBR(jadaOp_create)(P_op,NULL,V,BV,sigma, nvec, jdPrec,iflag),*iflag);
+    PHIST_CAST_PTR_FROM_VOID(TYPE(jadaOp_data), jdPr, jdPrec->A, *iflag);
+    jdPr->projType=projType;
+    jdPr->Prec_op = P_op;
+    jdPr->Skew_op = NULL;
+    
+    // need methode NONE with p_op as ab_op
+    jdPr->k_op->destroy(jdPr->k_op,iflag);
+    const char* method = "NONE";
+    PHIST_CHK_IERR(SUBR(JadaOp_create_variable)(jdPr->Prec_op,NULL,NULL,NULL,jdPr->V,jdPr->BV,jdPr->sigma,jdPr->num_shifts,jdPr->k_op,method,0,iflag),*iflag);
+    jdPrec->apply = SUBR(jadaOp_apply);
+  }
+  else
+  {
+    // we always require BV!=NULL, but it may point to the same memory or object in memory
+    if (BV==NULL)
+    {
+      PHIST_SOUT(PHIST_ERROR,"jadaPrec_create requires BV if V is given, but it may be the same object or a view \n"
+                             " of the same memory as V\n");
+      *iflag=PHIST_INVALID_INPUT;
+      return;
+    }
+    
+    TYPE(linearOp_ptr) skew_Op = new TYPE(linearOp);
+    PHIST_CHK_IERR(SUBR(skew_projection_Op_create)(P_op,V,BV,skew_Op,iflag),*iflag);
+    PHIST_CHK_IERR(SUBR(jadaOp_create)(P_op,NULL,V,BV,sigma, nvec, jdPrec,iflag),*iflag);
+    PHIST_CAST_PTR_FROM_VOID(TYPE(jadaOp_data), jdPr, jdPrec->A, *iflag);
+    jdPr->projType=projType;
+    jdPr->Prec_op = P_op;
+    jdPr->Skew_op = skew_Op;
+    
+    // need method POST with skew_op as proj_op and p_op as ab_op
+    jdPr->k_op->destroy(jdPr->k_op,iflag);
+    const char* method = "POST";
+    PHIST_CHK_IERR(SUBR(JadaOp_create_variable)(jdPr->Prec_op,jdPr->Skew_op,NULL,NULL,jdPr->V,jdPr->BV,jdPr->sigma,jdPr->num_shifts,jdPr->k_op,method,0,iflag),*iflag);
+    jdPrec->apply = SUBR(jadaOp_apply);
+  }
+}
+
+//! add a left preconditioner created by jadaPrec_create to a jadaOp.
+
+//! The effect of the apply function will afterwards by Y <- alpha*(jadaPrec*jadaOp(with PRE_POST)*X) + beta*Y,
+//! the projections used are determined by the AB_op and jadaPrec operators. If jadaPrec==NULL, 
+//! the operator is reset to it's original effect.
+extern "C" void SUBR(jadaOp_set_leftPrecond)(TYPE(linearOp_ptr) jdOp, TYPE(const_linearOp_ptr) jadaPrec, int* iflag)
+{
+  PHIST_CAST_PTR_FROM_VOID(TYPE(jadaOp_data), jdDat, jdOp->A, *iflag);
+  jdDat->leftPrecon_op = jadaPrec;
+  if (jadaPrec==NULL)
+  {
+    return;
+  }
+  else
+  { 
+    PHIST_CAST_PTR_FROM_VOID(TYPE(jadaOp_data), jdPrec, jadaPrec->A, *iflag);
+    jdDat->Prec_op = jdPrec->Prec_op;
+    jdDat->Skew_op = jdPrec->Skew_op;
+    jdDat->projType = jdPrec->projType;
+    jdDat->k_op->destroy(jdDat->k_op,iflag);
+    const char* method= "PRE_POST";
+    PHIST_CHK_IERR(SUBR(JadaOp_create_variable)(jdDat->AB_op,jdDat->Proj_op,jdDat->Prec_op,jdDat->Skew_op,jdDat->V,jdDat->BV,jdDat->sigma,jdDat->num_shifts,jdDat->k_op,method,1-(jdDat->projType),iflag),*iflag);
+    jdOp->apply = SUBR(jadaOp_apply);
+  }
 }
 
