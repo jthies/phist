540198fbe110f02d7e443dbf4eff2b78db513fe7
Jonas Thies <Jonas.Thies@DLR.de>
2018-04-11 06:08:34 -0700

    fix typo and tests for ghost spmv modes (they require a special flag when creating the matrix now, re #232)

--- src/kernels/ghost/kernels.cpp
+++ src/kernels/ghost/kernels.cpp
@@ -191,7 +191,7 @@ int get_perm_flag(int iflag, int outlev)
        (PHIST_DEFAULT_SPMV_MODE==0) )
   {
     // save memory by not duplicating the matrix (otherwise it is stored as a whole and as local/non-local part)
-    oflag|=GHOST_SPARSEMAT_NOT_STORE_SPLIT);
+    oflag|=GHOST_SPARSEMAT_NOT_STORE_SPLIT;
   }
 
   return oflag;
