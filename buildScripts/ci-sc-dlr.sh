65f7504eaf8783fad12fdd45ec811d35de7d164f
Jonas Thies <Jonas.Thies@DLR.de>
2018-04-13 14:43:08 +0200

    rename kernel library "Eigen" to "eigen" to conoform with the others

--- buildScripts/ci-sc-dlr.sh
+++ buildScripts/ci-sc-dlr.sh
@@ -25,7 +25,7 @@ SANITIZER="address"
 
 
 ## parse command line arguments
-usage() { echo "Usage: $0 [-k <builtin|ghost|epetra|tpetra|petsc|Eigen>] [-e <PrgEnv/module-string>] [-f <optional-libs>]"; \
+usage() { echo "Usage: $0 [-k <builtin|ghost|epetra|tpetra|petsc|eigen>] [-e <PrgEnv/module-string>] [-f <optional-libs>]"; \
           echo "       [-c <cmake flags to be added>] [-v <SSE|AVX|AVX2|CUDA>] [-t <trilinos version>] [-w <workspace-dir>]" 1>&2; exit 1; }
 
 function update_error { 
@@ -38,7 +38,7 @@ while getopts "k:e:f:c:v:w:t:h" o; do
     case "${o}" in
         k)
             KERNELS=${OPTARG}
-            ((KERNELS == "builtin" || KERNELS == "ghost" || KERNELS == "tpetra" || KERNELS == "epetra" || KERNELS == "petsc" || KERNELS == "Eigen")) || usage
+            ((KERNELS == "builtin" || KERNELS == "ghost" || KERNELS == "tpetra" || KERNELS == "epetra" || KERNELS == "petsc" || KERNELS == "eigen")) || usage
             ;;
         e)
             PRGENV=${OPTARG}
@@ -76,7 +76,7 @@ MODULES_KERNELS=(
   ["epetra"]="lapack trilinos/trilinos-${TRILINOS_VERSION}"
   ["tpetra"]="lapack trilinos/trilinos-${TRILINOS_VERSION}" 
   ["petsc"]="petsc" 
-  ["Eigen"]="Eigen" )
+  ["eigen"]="Eigen" )
 
 declare -A MODULES_KERNELS_OPTIONAL
 MODULES_KERNELS_OPTIONAL=(
@@ -85,7 +85,7 @@ MODULES_KERNELS_OPTIONAL=(
   ["epetra"]=""
   ["tpetra"]=""
   ["petsc"]="" 
-  ["Eigen"]="" )
+  ["eigen"]="" )
 
 
 ## prepare system for compilation
