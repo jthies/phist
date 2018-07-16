2fac8a6210639750479ac96425a746c8ef016b77
Jonas Thies <Jonas.Thies@DLR.de>
2018-07-16 04:06:36 -0700

    SVQB test: null space now at end of vector blocks

--- test/core/SVQB_Test_def.hpp
+++ test/core/SVQB_Test_def.hpp
@@ -77,14 +77,14 @@ public:
     {
     if (typeImplemented_ && !problemTooSmall_)
       {
-     //PrintVector(*cout,"SVQB_Test V",vec2_vp_,nloc_,lda_,stride_,mpi_comm_);
+     //PrintVector(PHIST_DEBUG,"SVQB_Test V",vec2_vp_,nloc_,lda_,stride_,mpi_comm_);
       SUBR(svqb)(vec2_,mat1_,nrms_,&iflag_);
       ASSERT_EQ(0,iflag_);
 
       // doing a SVQB decomp must not relocate data:
       ASSERT_EQ(true,MTest::pointerUnchanged(mat1_,mat1_vp_,m_lda_));
       ASSERT_EQ(true,VTest::pointerUnchanged(vec2_,vec2_vp_,lda_));
-      //PrintVector(*cout,"SVQB_Test Q",vec2_vp_,nloc_,lda_,stride_,mpi_comm_);
+      //PrintVector(PHIST_DEBUG,"SVQB_Test Q",vec2_vp_,nloc_,lda_,stride_,mpi_comm_);
       /*
       PHIST_SOUT(PHIST_DEBUG,"B matrix:\n");
       SUBR(sdMat_print)(mat1_,&iflag_);
@@ -111,31 +111,31 @@ public:
     }
 
   TEST_F(CLASSNAME, with_rank_deficiency) 
-    {
+  {
     if (typeImplemented_ && !problemTooSmall_)
-      {
+    {
       if (nvec_==1)
-        {
+      {
         SUBR(mvec_put_value)(vec1_,st::zero(),&iflag_);
         ASSERT_EQ(0,iflag_);
-        }
+      }
       else
-        {
+      {
         SUBR(mvec_random)(vec1_,&iflag_);
         SUBR(mvec_from_device)(vec1_,&iflag_);
         ASSERT_EQ(0,iflag_);
         // set last two columns to same vector
         for (int i=0;i<stride_*nloc_;i+=stride_)
-          {
+        {
 #ifdef PHIST_MVECS_ROW_MAJOR
           vec1_vp_[(nvec_-1)+i*lda_] = vec1_vp_[(nvec_-2)+i*lda_];
 #else
           vec1_vp_[(nvec_-1)*lda_+i] = vec1_vp_[(nvec_-2)*lda_+i];
 #endif
-          }
+        }
         SUBR(mvec_to_device)(vec1_,&iflag_);
         ASSERT_EQ(0,iflag_);
-        }
+      }
       SUBR(mvec_add_mvec)(st::one(),vec1_,st::zero(),vec2_,&iflag_);
       ASSERT_EQ(0,iflag_);
 
@@ -153,22 +153,26 @@ public:
       }
       SUBR(mvec_from_device)(vec2_,&iflag_);
       ASSERT_EQ(0,iflag_);
-      
-      // SVQB will return a block with the first <dim0> columns zero,
+
+     //PrintVector(PHIST_DEBUG,"SVQB_Test V",vec1_vp_,nloc_,lda_,stride_,mpi_comm_);
+     //PrintVector(PHIST_DEBUG,"SVQB_Test Q",vec2_vp_,nloc_,lda_,stride_,mpi_comm_);
+     //PrintSdMat (PHIST_DEBUG,"SVQB_Test B",mat1_vp_,m_lda_,1,mpi_comm_);
+
+      // SVQB will return a block with the last <dim0> columns zero,
       // and the remaining orthonormal
-      _ST_* vec2_col1=NULL;
+      int dim0=1, offs=ncols_-dim0;
+      _ST_* vec2_col0=NULL;
 #ifdef PHIST_MVECS_ROW_MAJOR
-      vec2_col1 = vec2_vp_+1;
+      vec2_col0 = vec2_vp_+offs;
 #else
-      vec2_col1 = vec2_vp_+lda_;
+      vec2_col0 = vec2_vp_+offs*lda_;
 #endif
 
-      // the factor 2 in releps here is because otherwise fails the test by a fraction of releps
-      ASSERT_NEAR(mt::one(), ArrayEqual(vec2_vp_,nloc_,1,lda_,stride_,st::zero(),vflag_), 100*mt::eps());
+      ASSERT_NEAR(mt::one(), ArrayEqual(vec2_col0,nloc_,dim0,lda_,stride_,st::zero(),vflag_), 100*mt::eps());
       if (nvec_>1)
       {
-        ASSERT_NEAR(mt::one(),VTest_m_minus_1::ColsAreNormalized(vec2_col1,nloc_,lda_,stride_,mpi_comm_),(MT)100.0*releps(vec1_));
-        ASSERT_NEAR(mt::one(),VTest_m_minus_1::ColsAreOrthogonal(vec2_col1,nloc_,lda_,stride_,mpi_comm_),(MT)100.0*releps(vec1_));
+        ASSERT_NEAR(mt::one(),VTest_m_minus_1::ColsAreNormalized(vec2_vp_,nloc_,lda_,stride_,mpi_comm_),(MT)100.0*releps(vec1_));
+        ASSERT_NEAR(mt::one(),VTest_m_minus_1::ColsAreOrthogonal(vec2_vp_,nloc_,lda_,stride_,mpi_comm_),(MT)100.0*releps(vec1_));
       }
 
       // check Q=V*B
@@ -176,8 +180,8 @@ public:
       SUBR(mvec_from_device)(vec2_,&iflag_);
       ASSERT_EQ(0,iflag_);
       ASSERT_NEAR(mt::one(), ArrayEqual(vec2_vp_,nloc_,nvec_,lda_,stride_,st::zero(),vflag_),sqrt(mt::eps()));
-      }
     }
+  }
 
   TEST_F(CLASSNAME, with_one_vectors) 
   {
@@ -204,17 +208,18 @@ public:
 
       // SVQB will return a block with the first <dim0> columns zero,
       // and the remaining orthonormal
-      _ST_* vec2_col1=NULL;
+      int dim0=1;
+      _ST_* vec2_col0=NULL;
 #ifdef PHIST_MVECS_ROW_MAJOR
-      vec2_col1 = vec2_vp_+nvec_-1;
+      vec2_col0 = vec2_vp_+dim0;
 #else
-      vec2_col1 = vec2_vp_+(nvec_-1)*lda_;
+      vec2_col0 = vec2_vp_+dim0*lda_;
 #endif
       // the factor 2 in releps here is because otherwise fails the test by a fraction of releps
-      ASSERT_EQ(mt::one(), ArrayEqual(vec2_vp_,nloc_,nvec_-1,lda_,stride_,st::zero(),vflag_));
+      ASSERT_EQ(mt::one(), ArrayEqual(vec2_col0,nloc_,nvec_-1,lda_,stride_,st::zero(),vflag_));
       if (nvec_>1)
       {
-        ASSERT_NEAR(mt::one(),VTest_1::ColsAreNormalized(vec2_col1,nloc_,lda_,stride_,mpi_comm_),(MT)100.*releps(vec1_));
+        ASSERT_NEAR(mt::one(),VTest_1::ColsAreNormalized(vec2_vp_,nloc_,lda_,stride_,mpi_comm_),(MT)100.*releps(vec1_));
       }
 
       // check Q=V*B
