9f598050fe1ad0780855459f01e34a83a0d75cbe
Jonas Thies <jonas.thies@dlr.de>
2018-09-04 22:49:51 +0200

    exampleProjects: try to fix problems with pkg-config and cmake demo projects and 'make test_install', set compilers in test script etc.

--- buildScripts/test_install.sh
+++ buildScripts/test_install.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
 INSTALL_PREFIX=$1
+CC=$2
+CXX=$3
+FC=$4
 
 echo "Check installation in $INSTALL_PREFIX"
 
 echo "... with pkg-config project"
 mkdir jdqr_pkg_config; cd $_
-PKG_CONFIG_PATH=$INSTALL_PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH cmake ../../exampleProjects/jdqr_pkg_config || exit $LINENO
+PKG_CONFIG_PATH=$INSTALL_PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH
+cmake ../../exampleProjects/jdqr_pkg_config || exit $LINENO
 make || exit $LINENO
 ./Djdqr || exit $LINENO
 cd ..
 echo "... with CMake project"
 mkdir jdqr_cmake; cd $_
-CMAKE_PREFIX_PATH=$INSTALL_PREFIX:$CMAKE_PREFIX_PATH cmake ../../exampleProjects/jdqr_cmake || exit $LINENO
+CMAKE_PREFIX_PATH=$INSTALL_PREFIX:$CMAKE_PREFIX_PATH
+cmake ../../exampleProjects/jdqr_cmake || exit $LINENO
 make || exit $LINENO
 ./Djdqr || exit $LINENO
 cd ..
