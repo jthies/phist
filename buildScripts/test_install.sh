779e2663fbba89037e0b06dfd34b30d1dcb60414
Jonas Thies <jonas.thies@dlr.de>
2018-09-05 12:59:33 +0200

    'make test_install' target: get compilers using pkg-config

--- buildScripts/test_install.sh
+++ buildScripts/test_install.sh
@@ -1,25 +1,43 @@
 #!/bin/bash
 
 INSTALL_PREFIX=$1
-CC=$2
-CXX=$3
-FC=$4
+
+error=0
+
+function update_error { 
+if [[ "${error}" = "0" ]]; then
+  error=$1
+fi
+}
 
 echo "Check installation in $INSTALL_PREFIX"
 
 echo "... with pkg-config project"
-mkdir jdqr_pkg_config; cd $_
+rm -rf jdqr_pkg_config; mkdir jdqr_pkg_config; cd $_
 PKG_CONFIG_PATH=$INSTALL_PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH
-cmake ../../exampleProjects/jdqr_pkg_config || exit $LINENO
-make || exit $LINENO
-./Djdqr || exit $LINENO
-cd ..
-echo "... with CMake project"
-mkdir jdqr_cmake; cd $_
-CMAKE_PREFIX_PATH=$INSTALL_PREFIX:$CMAKE_PREFIX_PATH
-cmake ../../exampleProjects/jdqr_cmake || exit $LINENO
-make || exit $LINENO
-./Djdqr || exit $LINENO
+
+CC=`pkg-config --variable=cc phist`
+CXX=`pkg-config --variable=cxx phist`
+FC=`pkg-config --variable=fc phist`
+
+cmake -DCMAKE_CXX_COMPILER=$CXX \
+      -DCMAKE_C_COMPILER=$CC \
+      -DCMAKE_Fortran_COMPILER=$FC \
+      ../../exampleProjects/jdqr_pkg_config || update_error $LINENO
+make || update_error $LINENO
+./Djdqr || update_error $LINENO
 cd ..
 
-exit 0
+echo "... with CMake project"
+rm -rf jdqr_cmake; mkdir jdqr_cmake; cd $_
+# cmake in this project should automatically use the compilers specified in phistConfig.cmake
+unset CC
+unset CXX
+unset FC
+CMAKE_PREFIX_PATH=$INSTALL_PREFIX:$CMAKE_PREFIX_PATH
+cmake ../../exampleProjects/jdqr_cmake || update_error $LINENO
+make || update_error $LINENO
+./Djdqr || update_error $LINENO
+cd ..
+
+exit ${error}
