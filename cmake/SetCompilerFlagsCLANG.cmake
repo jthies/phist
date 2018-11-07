1a0d57d4ae8eac25215fc4d83ff5fffc2feafa89
Jonas Thies <Jonas.Thies@DLR.de>
2018-11-07 15:53:53 +0100

    cmake: allow combination clang/gfortran

--- cmake/SetCompilerFlagsCLANG.cmake
+++ cmake/SetCompilerFlagsCLANG.cmake
@@ -3,6 +3,11 @@
     set (CMAKE_CXX_FLAGS      "${CMAKE_CXX_FLAGS}     -DMKL_LP64")
   endif()
 
+  if (CMAKE_Fortran_COMPILER_ID STREQUAL "GNU")
+    # FLANG is still experimental and one may want to use gfortran instead
+    set (CMAKE_Fortran_FLAGS  "${CMAKE_Fortran_FLAGS} -ffree-line-length-none")
+  endif()
+
   # -ffast-math kills high precision stuff!
   set(FAST_MATH "-fno-math-errno -ffinite-math-only -fno-signed-zeros -fno-trapping-math")
   set (CMAKE_C_FLAGS_RELEASE        "-O3 ${FAST_MATH} -march=native")
@@ -15,4 +20,8 @@
 
   set (CMAKE_C_FLAGS_DEBUG       "-O0 -g -Wall")
   set (CMAKE_CXX_FLAGS_DEBUG     "-O0 -g -Wall")
-  set (CMAKE_Fortran_FLAGS_DEBUG "-O0 -g -Mbounds")
+  set (CMAKE_Fortran_FLAGS_DEBUG "-O0 -g -Wall")
+
+  if (CMAKE_Fortran_COMPILER_ID STREQUAL "Clang")
+    set (CMAKE_Fortran_FLAGS_DEBUG "${CMAKE_Fortran_FLAGS_Debug} -Mbounds")
+  endif()
