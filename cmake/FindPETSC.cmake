da34fbb0ebe9b81ff2f9ae417bd4c61913310024
Jonas Thies <jonas.thies@dlr.de>
2018-11-12 10:23:08 +0100

    petsc: determine supported data type using cmake

--- cmake/FindPETSC.cmake
+++ cmake/FindPETSC.cmake
@@ -2,3 +2,17 @@ include(LibFindMacros)
 
 libfind_pkg_check_modules(PETSC PETSc)
 libfind_process(PETSC)
+
+# Figure out which data type is supported by the PETSc installation found.
+# Note that a given PETSc installation supports exactly one data type (S/C/D/Z)
+# at a time.
+include(CheckSymbolExists)
+check_symbol_exists(PETSC_USE_REAL_DOUBLE "petscsys.h" PETSC_USE_REAL_DOUBLE)
+check_symbol_exists(PETSC_USE_REAL_SINGLE "petscsys.h" PETSC_USE_REAL_SINGLE)
+check_symbol_exists(PETSC_USE_COMPLEX "petscsys.h" PETSC_USE_COMPLEX)
+
+if (PETSC_USE_REAL_SINGLE AND PETSC_USE_REAL_DOUBLE)
+  message(WARNING "Found PETSc installation, but it unexpectedly defines both "
+                  "'PETSC_USE_REAL_SINGLE' and 'PETSC_USE_REAL_DOUBLE'."
+                  "Will continue with both data types enabled.")
+endif()
