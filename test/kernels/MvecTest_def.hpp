50b1e8563b902e561bc4270109c8303b3dc560ac
Jonas Thies <Jonas.Thies@DLR.de>
2018-11-14 21:43:33 +0100

    disable mvec_times_mvec_elemwise tests for GHOST (doesn't have the kernel up to now)

--- test/kernels/MvecTest_def.hpp
+++ test/kernels/MvecTest_def.hpp
@@ -711,7 +711,11 @@ public:
 
 
   // Y(i,j)=alpha*X(i,j)*Y(i,j)
+#ifdef PHIST_KERNEL_LIB_GHOST
+  TEST_F(CLASSNAME, DISABLED_times_mvec_elemwise_k_k)
+#else
   TEST_F(CLASSNAME, times_mvec_elemwise_k_k)
+#endif
   {
     if( typeImplemented_ && !problemTooSmall_ )
     {
@@ -727,7 +731,11 @@ public:
   }
 
   // Y(i,j)=alpha*X(i,1)*Y(i,j)
+#ifdef PHIST_KERNEL_LIB_GHOST
+  TEST_F(CLASSNAME, DISABLED_times_mvec_elemwise_1_k)
+#else
   TEST_F(CLASSNAME, times_mvec_elemwise_1_k)
+#endif
   {
     if( typeImplemented_ && !problemTooSmall_ )
     {
