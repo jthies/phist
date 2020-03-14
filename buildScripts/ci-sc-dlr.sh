4eedf72c9ef8d2d60a433939ef6c614f791999a3
Jonas Thies <Jonas.Thies@DLR.de>
2020-03-14 14:40:16 +0100

    ci script: correct ccache dir

--- buildScripts/ci-sc-dlr.sh
+++ buildScripts/ci-sc-dlr.sh
@@ -136,7 +136,7 @@ if [[ $PRGENV =~ gcc* ]]; then
   fi
   if [[ "${VECT_EXT}" != "CUDA" ]] && [[ "${PRGENV}" != "gcc-7.2.0-openmpi" ]]; then
     ADD_CMAKE_FLAGS+=" -DPHIST_USE_CCACHE=ON"
-    export CCACHE_DIR=/home_local/f_buildn/ESSEX_workspace/.ccache/
+    export CCACHE_DIR=/home_local/f_jkessx/.ccache/
   else
     ADD_CMAKE_FLAGS+=" -DPHIST_USE_CCACHE=OFF"
   fi
