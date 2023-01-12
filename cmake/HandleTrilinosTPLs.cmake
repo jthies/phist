ac8ae5d286d67c1dcf8824e6110fcd952a507e10
Jonas Thies <16190001+jthies@users.noreply.github.com>
2023-01-12 21:26:13 +0100

    cmake: outsource some stuff to declutter the main CMakeLists.txt file, change order to make sure include_directories is called before add_library for the Trilinos TPLs

---
+++ cmake/HandleTrilinosTPLs.cmake
@@ -0,0 +1,106 @@
+set(REQUIRE_TRILI_PKG,"")
+set(OPTIONAL_TRILI_PKG,"")
+
+if (PHIST_USE_SOLVER_TPLS)
+  list(APPEND OPTIONAL_TRILI_PKG "Teuchos")
+  list(APPEND OPTIONAL_TRILI_PKG "Anasazi")
+  list(APPEND OPTIONAL_TRILI_PKG "Belos")
+endif()
+
+if  (PHIST_KERNEL_LIB STREQUAL "epetra")
+  list(APPEND REQUIRE_TRILI_PKG "Epetra")
+  list(APPEND REQUIRE_TRILI_PKG "EpetraExt")
+  list(APPEND REQUIRE_TRILI_PKG "Teuchos")
+
+  list(APPEND OPTIONAL_TRILI_PKG "Kokkos")
+  if (PHIST_USE_GRAPH_TPLS)
+    list(APPEND OPTIONAL_TRILI_PKG "Isorropia")
+    list(APPEND OPTIONAL_TRILI_PKG "Zoltan")
+  endif()
+  if (PHIST_USE_PRECON_TPLS)
+    list(APPEND OPTIONAL_TRILI_PKG "Ifpack")
+    list(APPEND OPTIONAL_TRILI_PKG "ML")
+    list(APPEND OPTIONAL_TRILI_PKG "MueLU")
+  endif()
+elseif  (PHIST_KERNEL_LIB STREQUAL "tpetra")
+  list(APPEND REQUIRE_TRILI_PKG "Tpetra")
+  list(APPEND REQUIRE_TRILI_PKG "Kokkos")
+  list(APPEND REQUIRE_TRILI_PKG "Teuchos")
+  if (PHIST_USE_PRECON_TPLS)
+    list(APPEND OPTIONAL_TRILI_PKG "Ifpack2")
+    list(APPEND OPTIONAL_TRILI_PKG "Amesos2")
+    list(APPEND OPTIONAL_TRILI_PKG "MueLU")
+  endif()
+
+elseif  (PHIST_KERNEL_LIB STREQUAL "ghost")
+
+  if (PHIST_USE_GRAPH_TPLS)
+    list(APPEND OPTIONAL_TRILI_PKG "Zoltan")
+  endif()
+  list(APPEND OPTIONAL_TRILI_PKG "Kokkos")
+  list(APPEND OPTIONAL_TRILI_PKG "Teuchos")
+
+endif()
+
+
+###########################################################################################
+# check for required Trilinos libraries, note that you have to add the Trilinos lib/cmake #
+# dir to your CMAKE_PREFIX_PATH so that CMake finds them automatically.                   #
+###########################################################################################
+
+foreach(PKG ${REQUIRE_TRILI_PKG})
+  string(TOUPPER ${PKG} PKG_CAPS)
+  if (Trilinos_DIR)
+    set(${PKG}_DIR "${Trilinos_DIR}/../${PKG}")
+    find_package(${PKG} PATHS ${${PKG}_DIR} QUIET NO_DEFAULT_PATH REQUIRED)
+  else()
+    find_package(${PKG} REQUIRED)
+  endif()
+  message(STATUS "Look for required Trilinos package ${PKG}...")
+  if (${${PKG}_FOUND})
+    message(STATUS "TROET: include_directories(${${PKG}_INCLUDE_DIRS}")
+    include_directories(${${PKG}_INCLUDE_DIRS})
+    include_directories(${${PKG}_TPL_INCLUDE_DIRS})
+    link_directories(${${PKG}_LIBRARY_DIRS})
+    link_directories(${${PKG}_TPL_LIBRARY_DIRS})
+    set(PHIST_HAVE_${PKG_CAPS} "Enable required Trilinos package ${PKG}" ON)
+    mark_as_advanced(${PKG}_DIR)
+  else()
+    message(FATAL_ERROR "required package ${PKG} was not found")
+  endif()
+
+endforeach()
+
+#########################################################################################
+# check for and enable optional libs from Trilinos (preconditioners, Belos/Anasazi)     #
+#########################################################################################
+
+
+if (NOT PHIST_USE_TRILINOS_TPLS)
+  set(OPTIONAL_TRILI_PKG "")
+endif()
+
+foreach(PKG ${OPTIONAL_TRILI_PKG})
+  string(TOUPPER ${PKG} PKG_CAPS)
+  set(TPL_REPORT "optional Trilinos package ${PKG_CAPS}... ")
+  if (Trilinos_DIR)
+    set(${PKG}_DIR "${Trilinos_DIR}/../${PKG}")
+    find_package(${PKG} PATHS ${${PKG}_DIR} QUIET NO_DEFAULT_PATH QUIET)
+  else()
+    find_package(${PKG} QUIET)
+  endif()
+
+  if (${${PKG}_FOUND})
+    list(APPEND TPL_REPORT "found")
+    include_directories(${${PKG}_INCLUDE_DIRS})
+    include_directories(${${PKG}_TPL_INCLUDE_DIRS})
+    link_directories(${${PKG}_LIBRARY_DIRS})
+    link_directories(${${PKG}_TPL_LIBRARY_DIRS})
+    mark_as_advanced(${PKG}_DIR)
+    set(PHIST_HAVE_${PKG_CAPS} "Enable optional Trilinos package ${PKG}" ON)
+  else()
+    list(APPEND TPL_REPORT "not found")
+  endif()
+  message(STATUS ${TPL_REPORT})
+endforeach()
+
---
+++ cmake/KernelLibSettings.cmake
@@ -0,0 +1,42 @@
+string(TOUPPER ${PHIST_KERNEL_LIB} PHIST_KERNEL_LIB_CAPS)
+set(PHIST_KERNEL_LIB_${PHIST_KERNEL_LIB_CAPS} ON)
+
+if  (PHIST_KERNEL_LIB STREQUAL "epetra")
+
+elseif  (PHIST_KERNEL_LIB STREQUAL "tpetra")
+
+elseif (PHIST_KERNEL_LIB STREQUAL "ghost")
+  # we already link with ghost because of the queuing system  
+  set(PHIST_USE_GHOST ON)
+  set(PHIST_MVECS_ROW_MAJOR 1 CACHE STRING "wether multi-vectors should be stored in row-major ordering")
+  set(PHIST_USE_GHOST_TASKS 0 CACHE STRING "execute all kernels as separate GHOST tasks, required for using PHISTs tasking features.")
+
+  set(PHIST_SELL_C -1 CACHE STRING "chunk size C for the SELL-C-sigma format (-1: automatic choice)")
+  set(PHIST_SELL_SIGMA -1 CACHE STRING "sorting scope sigma for the SELL-C-sigma format (-1: automatic choice)")
+
+  set(PHIST_DEFAULT_SPMV_MODE 0 CACHE STRING "default GHOST spmv mode (0: VECTOR, 8: OVERLAP, 16: TASK)")
+
+elseif (PHIST_KERNEL_LIB STREQUAL "builtin")
+  set(PHIST_MVECS_ROW_MAJOR 1)
+  option(PHIST_BUILTIN_MEMALIGN "Specify desired alignment for builtin Fortran kernels (in byte). Setting it to 0 will use regular mallocs." 0)
+  if(PHIST_HAVE_AVX2)
+    option(PHIST_HIGH_PRECISION_KERNELS "Activate experimental high-precision builtin kernel routines" Off)
+    option(PHIST_HIGH_PRECISION_KERNELS_FORCE "Always force the usage of high precision reductions even if this requires copying data and is slow!" Off)
+  endif()
+
+  if( NOT PHIST_HAVE_MPI )
+    message(FATAL_ERROR "Builtin kernel lib requires MPI!")
+  endif()
+elseif (PHIST_KERNEL_LIB STREQUAL "magma")
+  set(PHIST_MVECS_ROW_MAJOR 0)
+elseif (PHIST_KERNEL_LIB STREQUAL "petsc")
+  set(PHIST_MVECS_ROW_MAJOR 0)
+elseif (PHIST_KERNEL_LIB STREQUAL "eigen")
+  set(PHIST_MVECS_ROW_MAJOR 0 CACHE STRING "wether multi-vectors should be stored in row-major ordering")
+  if( PHIST_HAVE_MPI )
+    message(FATAL_ERROR "The eigen kernel lib has no MPI support!")
+  endif()
+else ()
+  message( FATAL_ERROR "PHIST_KERNEL_LIB not set or not recognized" )
+endif ()
+
